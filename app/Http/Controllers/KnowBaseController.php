47716280835a02c5ffeaa6277ff249c2feca8c3f
abik50000 <abik50000@gmail.com>
2022-05-12 16:33:00 +0600

    fix

--- app/Http/Controllers/KnowBaseController.php
+++ app/Http/Controllers/KnowBaseController.php
@@ -21,7 +21,7 @@ class KnowBaseController extends Controller
 
     public function get(Request $request) {
         return [
-            'books' => KnowBase::whereNull('parent_id')->with('children')->orderBy('order')->get()->toArray(),
+            'books' => KnowBase::whereNull('parent_id')->orderBy('order')->get()->toArray(),
         ];
     }
 
