746315abb4073bbe83930f63174ad074eecfb990
abik50000 <abik50000@gmail.com>
2022-05-13 15:43:01 +0600

    fix

--- resources/assets/js/pages/CourseResults.vue
+++ resources/assets/js/pages/CourseResults.vue
@@ -1,14 +1,35 @@
 <template>
 <div class="course-results">
-    <p>Тут ничего нет</p>
-    <div>
-        {{ monthInfo }}
-        {{ currentGroup }}
+        <div class="d-flex">
+            <button class="btn btn-primary mr-2 rounded"  :class="{'btn-primary': type == BY_USER}" @click="type = BY_USER">
+                <span>По сотрудникам</span>
+            </button>
+            <button class="btn btn-grey mr-2 rounded" :class="{'btn-primary': type == BY_GROUP}" @click="type = BY_GROUP">
+                <span>По группам</span>
+            </button>
+        </div>  
+       
+    <div v-if="type == BY_USER" class="by_user">
+        <p>Тут ничего нет</p>
+        <div>
+            {{ monthInfo }}
+            {{ currentGroup }}
+        </div>
     </div>
+
+    <div v-else class="by_group">
+         <p>Тут тоже отвечаю</p>
+    </div>
+
+    
+    
 </div>
 </template>
 
 <script>
+const BY_USER = 1;
+const BY_GROUP = 2;
+
 export default {
     name: "CourseResults", 
     watch: {
@@ -30,6 +51,9 @@ export default {
     data() {
         return {
             data: [],
+            type: BY_USER,
+            BY_USER: BY_USER,
+            BY_GROUP: BY_GROUP,
         }
     },
     created() {
