c3eb34329f268a222bfa5fe0abe36f1bce3e6d4f
abik50000 <abik50000@gmail.com>
2022-05-13 16:58:27 +0600

    fix

--- resources/assets/js/pages/CourseResults.vue
+++ resources/assets/js/pages/CourseResults.vue
@@ -24,7 +24,7 @@
 
                 <tr v-for="(item, i) in users.items" :key="i">
                     <td v-for="(field, f) in users.fields" :key="f">
-                        <div>{{ item[f] }}</div>
+                        <div>{{ item[f.key] }}</div>
                     </td>
                 </tr>
 
