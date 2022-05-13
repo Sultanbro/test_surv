cf3058ff799868b8c5e8e69a61edfad2e11246c4
abik50000 <abik50000@gmail.com>
2022-05-13 14:09:10 +0600

    fix

---
+++ resources/assets/js/pages/CourseResults.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="course-results">
+    <p>Тут ничего нет</p>
+    <div>
+        {{ monthInfo }}
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "CourseResults", 
+    props: {
+        monthInfo: {
+            required: false
+        }
+    },
+    data() {
+        return {
+            data: [],
+        }
+    },
+    created() {
+
+        
+    },
+    methods: {
+
+    } 
+}
+</script>
