3725df44bdd92b537e4d2c82ccff2e4e69045141
abik50000 <abik50000@gmail.com>
2022-05-14 17:16:37 +0600

    fix

--- resources/assets/js/pages/booklist.vue
+++ resources/assets/js/pages/booklist.vue
@@ -24,7 +24,7 @@
           :auth_user_id="auth_user_id"
           :open="true"
           @showPage="showPage"
-          @appPage="appPage"
+          @addPage="addPage"
           :parent_id="parent_id"
         />
       </div>
