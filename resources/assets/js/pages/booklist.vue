8ee97537f7d5f38f6b85125227ebf9d20e9baf3b
abik50000 <abik50000@gmail.com>
2022-05-15 18:34:16 +0600

    fix

--- resources/assets/js/pages/booklist.vue
+++ resources/assets/js/pages/booklist.vue
@@ -1071,7 +1071,7 @@ export default {
       if (obj.id === targetId) {
         return obj
       }
-     
+     console.log(obj)
       for (let item of obj.children) {
         let check = this.deepSearchId(item, targetId)
         if (check) {
