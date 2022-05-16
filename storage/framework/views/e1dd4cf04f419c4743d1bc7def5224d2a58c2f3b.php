968c676b9667baf337d1d469e6293d81fd9b6141
okTraning <ali.akpanov@yandex.ru>
2022-05-16 12:01:48 +0600

    fix

--- storage/framework/views/e1dd4cf04f419c4743d1bc7def5224d2a58c2f3b.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', 'Курсы'); ?>
-<?php $__env->startSection('content'); ?>
-<page-courses />      
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.admin', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
