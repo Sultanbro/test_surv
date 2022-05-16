0bc8f71de30e92017fb3aa94f25f1b7510b648bc
abik50000 <abik50000@gmail.com>
2022-05-16 16:42:05 +0600

    fix

--- app/Models/CourseResult.php
+++ app/Models/CourseResult.php
@@ -156,7 +156,7 @@ class CourseResult extends Model
         
 
         return [
-            'items' => $users,
+            'items' => $groups,
             'fields' => self::getGroupFields()    
         ];
     }
