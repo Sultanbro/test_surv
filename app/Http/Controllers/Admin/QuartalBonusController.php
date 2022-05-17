9b6d3e29d64daef9f95bc4a38f163973017c9331
abik50000 <abik50000@gmail.com>
2022-05-17 12:52:46 +0600

    fix

--- app/Http/Controllers/Admin/QuartalBonusController.php
+++ app/Http/Controllers/Admin/QuartalBonusController.php
@@ -14,25 +14,32 @@ class QuartalBonusController extends Controller
     public function storePersonQuartal(Request $request)
     {
 
-        $qb = QuartalBonus::where('quartal', $request->quartal)
-            ->where('year', date('Y'))
-            ->where('user_id', $request->user_id)
-            ->first();
+        foreach ($request->items as $key => $item) {
+            if($item['checked']) {
+                    $qb = QuartalBonus::where('quartal', $item['quarter'])
+                        ->where('year', date('Y'))
+                        ->where('user_id', $request->user_id)
+                        ->first();
 
-        $arr = [
-            'user_id'=> $request->user_id,
-            'auth_id' => 0,
-            'quartal'=> $request->quartal,
-            'sum'=> $request->sum,
-            'year'=> date('Y'),
-            'text'=> $request->text,
-        ];
+                $arr = [
+                    'user_id'=> $request->user_id,
+                    'auth_id' => 0,
+                    'quartal'=> $item['quarter'],
+                    'sum'=> $item['sum'],
+                    'year'=> date('Y'),
+                    'text'=> $item['text'],
+                ];
 
-        if($qb) {
-            $qb->update($arr);
-        } else {
-            QuartalBonus::create($arr);
+                if($qb) {
+                    $qb->update($arr);
+                } else {
+                    QuartalBonus::create($arr);
+                }
+            }
+
+           
         }
+      
 
 
 
