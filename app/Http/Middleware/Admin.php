cf2628e49d8105b74015f05f04994a6c4990c600
okTraning <ali.akpanov@yandex.ru>
2022-05-22 14:15:20 +0600

    fix

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -28,7 +28,7 @@ class Admin {
 
         $admin = AdminModel::where('owner_id', 18)->first();
 
-        if(Auth::user() && (in_array(Auth::user()->ID,$admin->users) || Auth::user()->ID == $admin->owner_id)) {
+        if(Auth::user() && $admin && $admin->users != null && (in_array(Auth::user()->ID,$admin->users) || Auth::user()->ID == $admin->owner_id)) {
             Auth::user()->is_admin = true;
         } else {
             Auth::user()->is_admin = false;
---
+++ resources/views/layouts/right_menu.blade.php
@@ -0,0 +1,5 @@
+<aside class="right-menu">
+
+
+
+</aside>
