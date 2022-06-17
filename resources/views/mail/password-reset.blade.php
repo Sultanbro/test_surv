c8c26ac9edf06a41da037e91a13bb65b66efba56
tvoipups <tashmetov03@mail.ru>
2022-06-17 12:35:19 +0600

    change text

--- resources/views/mail/password-reset.blade.php
+++ resources/views/mail/password-reset.blade.php
@@ -4,7 +4,7 @@
   <meta name="x-apple-disable-message-reformatting"> 
   <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
   <meta content="telephone=no" name="format-detection"> 
-  <title>Новое письмо 2</title><!--[if (mso 16)]>
+  <title>Восстановление пароля</title><!--[if (mso 16)]>
     <style type="text/css">
     a {text-decoration: none;}
     </style>
