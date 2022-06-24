2f0264cbf0b98879ff49565428b6042b9ed259dd
Abik Khoja <abik50000@gmail.com>
2022-06-24 10:29:10 +0000

    fix

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1342,16 +1342,19 @@ class UserController extends Controller
         /*******  Приглашение на почту  */
         /*==============================================================*/
 
-        if (env('APP_ENV','local') == 'prod'){
-            try { // если письмо с прилашением отправилось
+      
+       
+        try { // если письмо с прилашением отправилось
 
-                \Mail::to($request['email'])->send(new \App\Mail\SendInvitation($data));
-
-            } catch (Swift_TransportException $e) { // Если письмо по каким то причинам не отправилось
-                return redirect()->to('/timetracking/create-person')->withInput()->withErrors('Возможно вы ввели не верный email или его не существует! <br><br> ' . $e->getMessage());
-            }
+            \App\Components\TelegramBot::send('start mail');
+            \Mail::to($request['email'])->send(new \App\Mail\SendInvitation($data));
+            \App\Components\TelegramBot::send('end mail');
+        } catch (Throwable $e) { // Если письмо по каким то причинам не отправилось
+            \App\Components\TelegramBot::send($e);
+            return redirect()->to('/timetracking/create-person')->withInput()->withErrors('Возможно вы ввели не верный email или его не существует! <br><br> ' . $e->getMessage());
         }
-
+    
+        \App\Components\TelegramBot::send('end');
 
         /*==============================================================*/
         /*******  Создание пользователя в U-marketing.org  */
