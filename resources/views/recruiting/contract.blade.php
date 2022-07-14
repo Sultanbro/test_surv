c3fdd341d9518130451e22d60948286af1191052
abik50000 <abik50000@gmail.com>
2022-07-14 09:32:40 +0600

    fix

--- resources/views/recruiting/contract.blade.php
+++ resources/views/recruiting/contract.blade.php
@@ -165,7 +165,32 @@
                     </button>
                 </form>
             </div>
-            
+
+
+        <div id="loader">
+            <div class="icon">
+            <div class="loader loader--style8" title="7">
+                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+                    <rect x="0" y="10" width="4" height="10" fill="#333" opacity="0.2">
+                    <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0s" dur="0.6s" repeatCount="indefinite" />
+                    <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />
+                    <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />
+                    </rect>
+                    <rect x="8" y="10" width="4" height="10" fill="#333"  opacity="0.2">
+                    <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+                    <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+                    <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
+                    </rect>
+                    <rect x="16" y="10" width="4" height="10" fill="#333"  opacity="0.2">
+                    <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+                    <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+                    <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
+                    </rect>
+                </svg>
+                </div>
+            </div>
+        </div>
         
 
         <script>
@@ -214,7 +239,30 @@
                     alert("Пожалуйста, напишите ФИО!");
                     return null;
                 }
+
+                document.getElementById("submit").setAttribute("disabled", "disabled");
+                document.getElementById("submit").innerText == 'Загружаются...'
+                document.getElementById("loader").classList.addClass('show');
             }
         </script>
+
+        <style>
+            #loader {
+                display: none;
+                position: fixed;
+                width: 100%;
+                height: 100%;
+                z-index: 99;
+                background: rgba(0,0,0,.5);
+            }
+            #loaders.show {
+                display: flex;
+                justify-content: center;
+                align-items:center;
+            }
+            #loader svg {
+                width: 70px;
+            }
+        </style>
     </body>
 </html>
