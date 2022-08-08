b172e1b90349314ee421185c73e7629a07fdbee9
abik50000 <abik50000@gmail.com>
2022-08-08 18:56:08 +0600

    fix

--- resources/js/pages/kpi/Kpi.vue
+++ resources/js/pages/kpi/Kpi.vue
@@ -30,7 +30,11 @@
 
             <template v-for="(item, i) in items">
                 <tr :key="i">
-                    <td  @click="item.expanded = !item.expanded">{{ i + 1 }}</td>
+                    <td  @click="item.expanded = !item.expanded" class="pointer">
+                        <span class="mr-2">{{ i + 1 }}</span>
+                        <i class="fa fa-minus" v-if="item.expanded"></i>
+                        <i class="fa fa-plus" v-else></i>
+                    </td>
                     <td  v-for="(field, f) in show_fields" :key="f">
 
                         <div v-if="field.key == 'stats'" :class="field.class">
@@ -55,27 +59,29 @@
                                 <table class="table table-inner">
                                     <thead>
                                         <tr>
-                                            <td>12312</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
-                                            <td>123123</td>
+                                            <th>Наименование активности</th>
+                                            <th>Вид плана</th>
+                                            <th>Показатели</th>
+                                            <th>Ед. изм.</th>
+                                            <th>Целевое значение на месяц</th>
+                                            <th>Удельный вес, %</th>
+                                            <th>Сумма премии при выполенении плана, KZT</th>
+                                            <th>Действия</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr class="jt-row j-hidden" :class="{'j-hidden': !item.expanded}" v-for="(element, j) in item.elements" :key="i + '-' + j">
-                                            <td >{{ j + 1 }}</td>
-                                            <td >asdasd</td>
-                                            <td >dsfsdr</td>
-                                            <td >rtyr</td>
-                                            <td >ghjg</td>
-                                            <td >;lk;k</td>
-                                            <td >bnm</td>
-                                            <td >haopi</td>
+                                            <td >{{ element.name }}</td>
+                                            <td >{{ element.plan_type }}</td>
+                                            <td >{{ element.activity }}</td>
+                                            <td >{{ element.plan_unit }}</td>
+                                            <td >{{ element.plan }}</td>
+                                            <td >{{ element.ud_ves }}</td>
+                                            <td >{{ element.sum }}</td>
+                                            <td>
+                                                <i class="fa fa-edit mr-1 btn btn-primary p-1" @click="editKpi"></i>
+                                                <i class="fa fa-trash btn btn-primary p-1" @click="deleteKpi"></i>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -113,11 +119,11 @@ export default {
                     lower_limit: 80,
                     upper_limit: 100,
                     stats: 1,
-                    created_at: new Date(),
-                    updated_at: new Date(),
+                    created_at: new Date().toISOString().substr(0, 19).replace('T',' '),
+                    updated_at: new Date().toISOString().substr(0, 19).replace('T',' '),
                     created: 'Ходжа Абулхаир',
                     updated: 'Али Акпанов',
-                    elements: [{}, {}], 
+                    elements: [], 
                     expanded: false
                 },
                 {
@@ -127,11 +133,11 @@ export default {
                     lower_limit: 80,
                     upper_limit: 100,
                     stats: 2,
-                    created_at: new Date(),
-                    updated_at: new Date(),
+                    created_at: new Date().toISOString().substr(0, 19).replace('T',' '),
+                    updated_at: new Date().toISOString().substr(0, 19).replace('T',' '),
                     created: 'Ходжа Абулхаир',
                     updated: 'Али Акпанов',
-                    elements: [{}, {}], 
+                    elements: [], 
                     expanded: false
                 },
             ]
@@ -142,6 +148,23 @@ export default {
        // this.fetchData()
        this.prepareFields(); 
        this.addStatusToItems(); 
+
+       this.items.forEach(el => {
+        let a = Math.floor(Math.random() * 3) + 1
+
+        for(let i=1;i<=a;i++) {
+            el.elements.push({
+                name: 'Активность',
+                plan_type: 'среднее',
+                activity: 'ячейка из сводной',
+                plan_unit: '%',
+                plan: '90%',
+                ud_ves: '40%',
+                sum: '20000',
+            });
+        }
+     
+       })
     },
     methods: {
 
@@ -261,8 +284,8 @@ export default {
                 lower_limit: 80,
                 upper_limit: 100,
                 stats: 3,
-                created_at: new Date(),
-                updated_at: new Date(),
+                created_at: new Date().toISOString().substr(0, 19).replace('T',' '),
+                updated_at: new Date().toISOString().substr(0, 19).replace('T',' '),
                 created: 'Али Акпанов',
                 updated: 'Али Акпанов',
                 elements: [{}, {}], 
