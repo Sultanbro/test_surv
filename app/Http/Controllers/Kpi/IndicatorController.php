46bba0090eb243dd838c8cfea43ce53e39a09c6a
abik50000 <abik50000@gmail.com>
2022-08-10 10:47:22 +0600

    fix

--- app/Http/Controllers/Kpi/IndicatorController.php
+++ app/Http/Controllers/Kpi/IndicatorController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Kpi;
 
 use App\Http\Controllers\Controller;
 use App\Models\Analytics\Activity;
---
+++ app/Http/Controllers/Kpi/QuartalPremiumController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Kpi;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Http\Request;
+use DB;
+use Carbon\Carbon;
+use App\User;
+use App\ProfileGroup;
+use App\Position;
+use App\Models\Analytics\Activity;
+// use App\Models\Kpi\QuartalPremium;  
+
+class QuartalPremiumController extends Controller
+{
+
+    public function index(Request $request)
+    {
+
+    }
+
+}
