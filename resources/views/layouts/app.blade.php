77ac114c4975f6ead97cb36046135ea1dc8cf22b
abik50000 <abik50000@gmail.com>
2022-10-19 17:46:18 +0600

    explode newprofile.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,21 @@
     <div class="right-panel-app">
 
         <div class="container header__container">
-            @include('layouts.header')
+            <profile-sidebar />
+
+            <header class="header">
+
+                <left-sidebar />
+                <right-sidebar />
+
+                <div class="header__arrow">
+                    <a href="#"><img src="images/dist/header-arrow.svg" alt="arrow icon"></a>
+                </div>
+                
+            </header>
         </div>
 
+        <!-- Main -->
         <div class="wrapper">
 
             <main class="main"> 
