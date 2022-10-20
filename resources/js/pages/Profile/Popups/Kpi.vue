36b4261cb6bdba67e42928db6155f45719ee8aca
abik50000 <abik50000@gmail.com>
2022-10-20 22:01:40 +0600

    fix

--- resources/js/pages/Profile/Popups/Kpi.vue
+++ resources/js/pages/Profile/Popups/Kpi.vue
@@ -200,7 +200,7 @@ export default {
                 
                 // items
                 this.items = response.data.items;
-                this.items = this.kpis.map(res=> ({...res, my_sum: 0}))
+                this.items = this.items.map(res=> ({...res, my_sum: 0}))
                 
                 this.activities = response.data.activities;
                 this.groups = response.data.groups;
