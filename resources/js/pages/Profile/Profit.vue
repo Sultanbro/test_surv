a2c487ed532367cf835391480bf6a410bb580d6a
abik50000 <abik50000@gmail.com>
2022-10-24 13:37:30 +0600

    add payemnts terms to profile

--- resources/js/pages/Profile/Profit.vue
+++ resources/js/pages/Profile/Profit.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="profit block _anim _anim-no-hide content _active" id="profit" v-if="slides.length > 0">
+<div class="profit block _anim _anim-no-hide content _active" id="profit" :class="{'hidden': slides.length > 0}">
     <div class="profit__title title mt-5">
         Как можно зарабатывать больше
     </div>
