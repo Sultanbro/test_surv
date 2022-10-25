1f7cfdc8d6c1322162a40eea104d38c9e0f07c89
abik50000 <abik50000@gmail.com>
2022-10-25 12:21:35 +0600

    fix

--- resources/js/pages/Layouts/RightSidebar.vue
+++ resources/js/pages/Layouts/RightSidebar.vue
@@ -1,22 +1,23 @@
 <template>
 <div class="header__right closedd">
     <div class="header__right-nav">
-        <a href="#" class="header__right-icon" @click="$emit('pop', 'faq')">
+        <a href="#" class="header__right-icon" v-b-popover.hover.right.html="'Вопросы и ответы - Этот функционал в разработке'">
             <img src="/images/dist/header-right-1.svg" alt="nav icon" class="header__icon-img">
         </a>
         <a href="#" class="header__right-icon bell red" @click="$emit('pop', 'notifications')">
             <img src="/images/dist/header-right-2.svg" alt="nav icon" class="header__icon-img">
         </a>
-        <a href="#" class="header__right-icon loop" @click="$emit('pop', 'search')">
+        <a href="#" class="header__right-icon loop" v-b-popover.hover.right.html="'Поиск по чату - Этот функционал в разработке'">
             <img src="/images/dist/header-right-3.svg" alt="nav icon" class="header__icon-img">
         </a>
-        <a href="#" class="header__right-icon" @click="$emit('pop', 'mail')">
+        <a href="#" class="header__right-icon" v-b-popover.hover.right.html="'Тикеты - Этот функционал в разработке'">
             <img src="/images/dist/header-right-5.svg" alt="nav icon" class="header__icon-img">
         </a>
         <a href="#" class="header__right-icon check" @click="$emit('pop', 'checklist')">
             <img src="/images/dist/header-right-6.svg" alt="nav icon" class="header__icon-img">
         </a>
     </div>
+
     <div class="header__right-messages">
         <a href="#" class="header__message-item new">
             <img src="/images/dist/header-right-avatar-1.png" alt="header avatar">
@@ -37,6 +38,7 @@
             <img src="/images/dist/header-right-avatar-6.png" alt="header avatar">
         </a>
     </div>
+
     <div class="header__right-arrow">
         <a href="#"><img src="/images/dist/header-arrow.svg" alt="arrow icon"></a>
     </div>
--- resources/js/pages/Widgets/ProfileInfo.vue
+++ resources/js/pages/Widgets/ProfileInfo.vue
@@ -1,20 +1,21 @@
 <template>
-<div class="profile__about" v-if="data.user !== undefined && data.user !== null">
+<div class="profile__about" :class="{'hidden': data.user === undefined || data.user === null }">
 
     <div class="profile__name">{{ data.user.last_name + ' ' + data.user.name }}</div>
     <div class="profile__job profile-border">{{ data.position.position }}</div>
-    <div class="profile__salary profile-border">ОКЛАД {{ data.salary }}</div>
+    <div class="profile__job profile-border">{{ data.groups }}</div>
+    <div class="profile__salary profile-border">ОКЛАД: {{ data.salary }}</div>
     <div class="profile__wrapper">
         <p class="profile-border">{{ data.workingDay }}</p>
         <p class="profile-border">{{ data.schedule }}</p>
         <p class="profile-border">{{ data.workingTime }}</p>
     </div>
 
-    <select class="select-css" v-model="data.currency">
+    <!-- <select class="select-css" v-model="data.currency">
         <option v-for="key in Object.keys(data.currencies)" :value="key">
           {{ key }} {{ data.currencies[key] }}
         </option>
-    </select>
+    </select> -->
 </div>
 </template>
 
