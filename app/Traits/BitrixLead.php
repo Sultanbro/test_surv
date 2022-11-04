4bb04e3bd7574ebb430f21b8d239aa1fff092555
Yerlan Sharofidinov <cool.krutoi@inbxo.ru>
2022-11-04 18:33:54 +0600

    fix: fix

--- app/Traits/BitrixLead.php
+++ app/Traits/BitrixLead.php
@@ -11,12 +11,15 @@ trait BitrixLead
     public function changeDeal(User $user): void
     {
         $lead = Lead::userLeadByDesc($user);
-        if($lead  && $lead->deal_id != 0) {
-            $bitrix = new Bitrix();
 
-            $bitrix->changeDeal($lead->deal_id, [
-                'STAGE_ID' => 'C4:WON' // не присутствовал на обучении
-            ]);
+        if (isset($lead)) {
+            if($lead  && $lead->deal_id != 0) {
+                $bitrix = new Bitrix();
+
+                $bitrix->changeDeal($lead->deal_id, [
+                    'STAGE_ID' => 'C4:WON' // не присутствовал на обучении
+                ]);
+            }
         }
     }
 }
