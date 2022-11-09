a2242a735a5f7bc6b53e0a3ba6d202e601e3301b
ysharofidinov <cool.krutoi@inbox.ru>
2022-11-09 15:54:57 +0600

    fix: test

--- app/Service/UpdatedUserStatService.php
+++ app/Service/UpdatedUserStatService.php
@@ -22,7 +22,6 @@ class UpdatedUserStatService
     public function calculateStat(User $user, Carbon $date): float|int
     {
         $statistics = $this->repository->retrieveLastRecordUpdatedStatisticsForEachKpi($user, $date)->get();
-        dd($statistics);
         $amount = 0;
         foreach ($statistics as $statistic) {
             if ($statistic != null) {
