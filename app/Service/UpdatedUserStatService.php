29a3137021b8895e0ebc8ed83b836960a78daf34
ysharofidinov <cool.krutoi@inbox.ru>
2022-11-09 22:38:04 +0600

    fix: фикс

--- app/Service/UpdatedUserStatService.php
+++ app/Service/UpdatedUserStatService.php
@@ -27,12 +27,11 @@ class UpdatedUserStatService
             if ($statistic != null) {
                 $kpiItem  = (new KpiItemRepository)->joinKpiItemsWithKpi($statistic->kpi_item_id);
                 if ($kpiItem != null) {
-                    $amount  = CalculateCarried::calculate($kpiItem, $statistic, $user);
-                    dump($amount);
+                    $amount  += CalculateCarried::calculate($kpiItem, $statistic, $user);
+
                 }
             }
         }
-        dd();
         return $amount;
 
     }
