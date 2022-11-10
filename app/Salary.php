8f9e43e8b5289b4e2efb52bcead0e6af588682db
ysharofidinov <cool.krutoi@inbox.ru>
2022-11-10 16:43:28 +0600

    fix: правка

--- app/Salary.php
+++ app/Salary.php
@@ -900,6 +900,7 @@ class Salary extends Model
             /**
              * Добавляем в KPI переменную значения с таблицы saved_kpi, updated_user_stats
              */
+            dd($user->kpi);
 //            $user->kpi += (new SavedKpiRepository)->getSavedKpiForMonth($user, $date)->sum('total');
             $user->kpi += (new UpdatedUserStatService)->calculateStat($user, $date);
 
