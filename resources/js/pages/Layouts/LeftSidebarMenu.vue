7cc96b69715cf44cda0b903377e5b04e1fb29378
takodachi2501 <vrnehot@yandex.kz>
2022-11-15 10:40:25 +0300

    SCRUM-24 подменю для иконок не влезающих в сайдбар

---
+++ resources/js/pages/Layouts/LeftSidebarMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header__menu">
+    <template v-for="item in items">
+      <a
+        :key="item.title"
+        v-if="!item.hide"
+        :href="item.href"
+        class="menu__item"
+      >
+        <img :src="item.img" :alt="item.alt">
+        <span class="menu__item-title">{{item.title}}</span>
+      </a>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftSidebarMenu',
+  props: ['items']
+} 
+</script>
+
+<style lang="scss">
+</style>
