973ef1247a229ab1a709adacdab5cba5e4d7feb8
ysharofidinov <cool.krutoi@inbox.ru>
2022-11-16 16:51:06 +0600

    fix: fix

--- app/Http/Controllers/Admin/AnalyticsController.php
+++ app/Http/Controllers/Admin/AnalyticsController.php
@@ -432,6 +432,7 @@ class AnalyticsController extends Controller
             if(!in_array($tt->user_id, $marked_users)) {
                 $old_value = is_numeric($old_value) ? (int) $old_value : 0;
                 $new_value = ($tt->total_hours + $value) - $old_value;
+                dump($new_value);
                 if($new_value < 0) $new_value = 0;
                 $tt->total_hours = $new_value; 
 
