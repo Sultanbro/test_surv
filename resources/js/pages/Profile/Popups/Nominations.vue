19585b6e7060abf82d712140dd0349941b515896
timur <armor.d.r.x@gmail.com>
2022-11-22 18:05:04 +0600

    awards 1 step

--- resources/js/pages/Profile/Popups/Nominations.vue
+++ resources/js/pages/Profile/Popups/Nominations.vue
@@ -37,74 +37,47 @@
                                 <div class="tab__content-item-include is-active" data-content="1">
                                     <div class="certificates__title">
                                         Сертификатов: <span class="current">{{myAwards.length}}</span> из <span
-                                            class="all">17</span>
+                                            class="all">{{availableAwards.length}}</span>
                                     </div>
 
-                                    <div class="certificates__wrapper" v-for="award in myAwards"
-                                         :key="award.created_at">
-                                        <div class="certificates__item">
-                                            <img :src="award.path" alt="certificate image">
-                                        </div>
-                                    </div>
+                                    <BRow>
+                                        <BCol cols="12" md="4" lg="3" v-for="award in myAwards"
+                                              :key="award.created_at">
+                                            <div class="certificates__item" @click="modalShow(award)">
+                                                <img :src="award.path" alt="certificate image">
+                                            </div>
+                                        </BCol>
+                                    </BRow>
 
                                 </div>
                                 <div class="tab__content-item-include" data-content="2">
+
                                     <div class="certificates__title">
-                                        Сертификатов: <span class="current">0</span> из <span class="all">80</span>
+                                        Сертификатов: <span class="current">{{availableAwards.length - myAwards.length}}</span> из <span
+                                            class="all">{{availableAwards.length}}</span>
                                     </div>
 
-                                    <div class="certificates__wrapper">
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-3.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-2.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-1.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-3.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-1.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-2.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-3.png" alt="certificate image">
-                                        </div>
-                                    </div>
+                                    <BRow>
+                                        <BCol cols="12" md="4" lg="3" v-for="award in availableAwards"
+                                              :key="award.created_at">
+                                            <div class="certificates__item" @click="modalShow(award)">
+                                                <img :src="award.path" alt="certificate image">
+                                            </div>
+                                        </BCol>
+                                    </BRow>
                                 </div>
                                 <div class="tab__content-item-include" data-content="3">
-                                    <div class="certificates__title">
-                                        Сертификатов: <span class="current">3</span> из <span class="all">80</span>
-                                    </div>
-
-                                    <div class="certificates__wrapper">
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-2.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-1.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-2.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-3.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-1.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-2.png" alt="certificate image">
-                                        </div>
-                                        <div class="certificates__item">
-                                            <img src="images/dist/certificate-3.png" alt="certificate image">
-                                        </div>
-                                    </div>
+<!--                                    <div class="certificates__title">-->
+<!--                                        Сертификатов: <span class="current">3</span> из <span class="all">80</span>-->
+<!--                                    </div>-->
+                                    <BRow>
+                                        <BCol cols="12" md="4" lg="3" v-for="award in otherAwards"
+                                              :key="award.created_at">
+                                            <div class="certificates__item" @click="modalShow(award)">
+                                                <img :src="award.path" alt="certificate image">
+                                            </div>
+                                        </BCol>
+                                    </BRow>
                                 </div>
                             </div>
                         </div>
@@ -300,8 +273,13 @@
                     </div>
                 </div>
             </div>
+            <b-modal v-if="itemModal" centered size="xl" v-model="modal" :title="itemModal.name">
+                <img :src="itemModal.path" alt="" class="img-fluid">
+                <template #modal-footer>
+                    <BButton variant="primary" @click="modal = !modal">Закрыть</BButton>
+                </template>
+            </b-modal>
         </div>
-
     </div>
 </template>
 
@@ -311,37 +289,92 @@
         props: {},
         data: function () {
             return {
+                modal: false,
+                itemModal: null,
                 fields: [],
                 awardTypes: null,
-                myAwards: []
+                myAwards: [],
+                availableAwards: [],
+                availableAwardsCurrent: [],
+                otherAwards: []
             };
         },
         mounted() {
-            this.axios
-                .get('/awards/my')
-                .then(response => {
-                    const data = response.data.data.awards.my;
-                    for (let i = 0; i < data.length; i++) {
-                        this.myAwards.push(data[i]);
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-
+            let loader = this.$loading.show();
             this.axios
                 .get('/awards/type?award_type_id=1')
                 .then(response => {
-                    console.log(response);
+                    const data = response.data.data;
+                    for (let i = 0; i < data.my.length; i++) {
+                        this.myAwards.push(data.my[i]);
+                    }
+                    for (let i = 0; i < data.available.length; i++) {
+                        this.availableAwards.push(data.available[i]);
+                    }
+                    for (let i = 0; i < data.other.length; i++) {
+                        this.otherAwards.push(data.other[i]);
+                    }
+                    this.availableAwards.forEach((item, index, array) => {
+
+                    });
+
+                    // let i = 0;
+                    // // надо исключить id того и того и вернуть остаток
+                    // while (i < this.availableAwards.length) {
+                    //     if(this.myAwards[i]){
+                    //         const myId = this.myAwards[i].id;
+                    //         for(let j = 0; j < this.availableAwards.length; j++){
+                    //             const awId = this.availableAwards[j].id;
+                    //             if(awId !== myId){
+                    //                 this.availableAwardsCurrent.push(this.availableAwards[j]);
+                    //                 j = 9999999;
+                    //                 i++;
+                    //             }
+                    //         }
+                    //     } else{
+                    //         i = 9999999;
+                    //     }
+                    // }
+                    // console.log(this.availableAwardsCurrent);
+                    loader.hide();
                 })
                 .catch(error => {
                     console.log(error);
+                    loader.hide();
                 });
+
         },
         methods: {
-            fetchData() {
-
-            }
+            modalShow(item){
+                this.itemModal = item;
+                this.modal = !this.modal;
+            },
         }
     };
 </script>
+
+
+<style lang="scss" scoped>
+    .nominations__wrapper{
+        width: 100%;
+        display: block;
+    }
+    .certificates__item {
+        height: 200px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        overflow: hidden;
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        box-shadow: rgb(50 50 93 / 25%) 0px 13px 27px -5px, rgb(0 0 0 / 30%) 0px 8px 16px -8px;
+
+        img {
+            width: auto;
+            height: 200px;
+            object-fit: cover;
+        }
+    }
+
+</style>
