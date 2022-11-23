17be89f4b310b1cddcb91604702c770951c40b92
Aruzhan Jankushukova <a.dzhankushukova@ticketon.kz>
2022-11-23 16:22:12 +0600

    seed value updated

--- database/seeds/AwardTypeSeeder.php
+++ database/seeds/AwardTypeSeeder.php
@@ -18,17 +18,24 @@ class AwardTypeSeeder extends Seeder
     {
         $types = [
             [
+                'name' => 'Номинаций',
+                'description' => 'Получает при выполнений плана.',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
                 'name' => 'Сертификаты',
                 'description' => 'Получает при успешной прохождений курсов.',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'name' => 'Номинаций',
-                'description' => 'Получает при выполнений плана.',
+                'name' => 'Начисления',
+                'description' => 'начисления',
                 'created_at' => now(),
                 'updated_at' => now()
-            ]
+            ],
+
         ];
 
         DB::table('award_types')->insert($types);
