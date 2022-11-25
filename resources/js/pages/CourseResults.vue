759888ad5868601481cd09022f711c1ecdde7a85
takodachi2501 <vrnehot@yandex.kz>
2022-11-25 10:06:43 +0300

    SCRUM-88 1 вывести в тех курсах другие разделы

--- resources/js/pages/CourseResults.vue
+++ resources/js/pages/CourseResults.vue
@@ -1,18 +1,18 @@
 <template>
 <div class="course-results">
     <div class="d-flex mb-2">
-        <button class="btn btn-grey mr-2 rounded"  :class="{'btn-primary': type == BY_USER}" @click="type = BY_USER">
+        <button class="btn btn-grey mr-2 rounded" :class="{'btn-primary': type == BY_USER}" @click="type = BY_USER">
             <span>По сотрудникам</span>
         </button>
         <button class="btn btn-grey mr-2 rounded" :class="{'btn-primary': type == BY_GROUP}" @click="type = BY_GROUP">
             <span>По отделам</span>
         </button>
-    </div>  
-       
+    </div>
+
     <div v-if="type == BY_USER" class="by_user">
 
         <div class="table-responsive" v-if="users.items.length > 0">
-           
+
             <table class="table b-table table-bordered table-sm">
 
                 <tr>
@@ -21,7 +21,7 @@
                     </th>
                 </tr>
 
-                
+
                 <template v-for="(item, i) in users.items">
                     <tr class="pointer" :class="{
                         'expanded-title': item.expanded
@@ -31,19 +31,19 @@
                                 <p class="mb-0 mr-1">{{ item[field.key] }}</p>
                                 <progress :value="item[field.key].slice(0, -1)" max="100"></progress>
                             </div>
-                            <div v-else>{{ item[field.key] }}</div>  
+                            <div v-else>{{ item[field.key] }}</div>
                         </td>
                     </tr>
 
                     <template v-for="(course, c) in item.courses">
                         <tr v-if="item.expanded" class="expanded">
-                            <td v-for="(field, f) in users.fields" :key="f" :class="field.class">
+                            <td v-for="(field, f) in users.fields" :key="f" :class="[field.class, {pointer: course.items && course.items.length > 1}]" @click="expandCourse(course, item)">
                                 <div v-if="field.key == 'progress'" class="d-flex jcc aic">
                                     <p class="mb-0 mr-1">{{ course[field.key] }}</p>
                                     <progress :value="course[field.key].slice(0, -1)" max="100"></progress>
                                 </div>
-                                <div v-else-if="field.key == 'name'" class="relative nullify-wrap">
-                                    
+                                <div v-else-if="field.key == 'name'" class="nullify-wrap relative">
+
                                     {{ course[field.key] }}
 
                                     <i class="absolute nullify fa fa-broom" title="Обнулить прогресс" @click="nullify(i, c)"></i>
@@ -52,10 +52,28 @@
                                 <div v-else>{{ course[field.key] }}</div>
                             </td>
                         </tr>
+
+                        <template v-if="course.items && course.items.length > 1 && course.expanded">
+                            <tr v-for="(coureItem, ci) in course.items" :key="ci" class="expanded-course-item">
+                                <td v-for="(field, f) in users.fields" :key="f" :class="field.class">
+                                    <div v-if="field.key == 'name'" class="nullify-wrap relative">
+                                        {{ coureItem[course2item[field.key]] || field.key }}
+                                        <i
+                                            class="absolute nullify fa fa-broom"
+                                            title="Обнулить прогресс"
+                                            @click="regress(item.user_id, course.course_id, coureItem)"
+                                        />
+                                    </div>
+                                    <template v-else>
+                                        {{ coureItem[course2item[field.key]] || field.key }}
+                                    </template>
+                                </td>
+                            </tr>
+                        </template>
                     </template>
 
                 </template>
-                
+
 
             </table>
 
@@ -64,7 +82,7 @@
 
     <div v-else class="by_group">
         <div class="table-responsive" v-if="groups.items.length > 0">
-           
+
             <table class="table b-table table-bordered table-sm">
 
                 <tr>
@@ -73,23 +91,23 @@
                     </th>
                 </tr>
 
-                
+
                 <template v-for="(item, i) in groups.items">
                     <tr>
                         <td v-for="(field, f) in groups.fields" :key="f" :class="field.class" @click="expandUser(item)">
-                            <div>{{ item[field.key] }}</div> 
+                            <div>{{ item[field.key] }}</div>
                         </td>
                     </tr>
                 </template>
-                
+
 
             </table>
 
         </div>
     </div>
 
-    
-    
+
+
 </div>
 </template>
 
@@ -98,7 +116,7 @@ const BY_USER = 1;
 const BY_GROUP = 2;
 
 export default {
-    name: "CourseResults", 
+    name: "CourseResults",
     watch: {
         monthInfo(val) {
             this.first = true;
@@ -108,7 +126,7 @@ export default {
             } else {
                 this.fetchData('users');
             }
-        }, 
+        },
         currentGroup() {
             this.first = true;
             if(this.type == this.BY_GROUP) {
@@ -148,6 +166,15 @@ export default {
                 items: [],
                 fields: [],
             },
+            course2item: {
+                name: 'title',
+                status: 'status',
+                points: 'points',
+                progress: 'progress',
+                progress_on_week: 'progress_on_week',
+                started_at: 'started_at',
+                ended_at: 'ended_at'
+            }
         }
     },
     created() {
@@ -165,26 +192,46 @@ export default {
                     group_id: this.currentGroup !== undefined ? this.currentGroup :  null,
                 })
                 .then((response) => {
-                    
+
                     if(type == 'users') {
                         this.users = response.data.items;
                     }
                     if(type == 'groups') {
                         this.groups = response.data.items;
                     }
-                    
-            
+
+
                     loader.hide();
                 });
         },
 
         expandUser(item) {
             let ex = item.expanded;
-            this.users.items.forEach(i => i.expanded = false);
+            this.users.items.forEach(i => {
+                i.expanded = false
+                if(i.courses) i.courses.forEach(c => this.$set(c, 'expanded', false))
+            });
             item.expanded = !ex;
-            
         },
-        
+
+        expandCourse(course, item) {
+            if(course.items && course.items.length > 1){
+                this.users.items.every(el => {
+                    console.log('el.user_id', el.user_id, item.user_id)
+                    if(el.user_id !== item.user_id) return true
+                    item.courses.forEach(c => {
+                        console.log('c.course_id', c.course_id, course.course_id)
+                        if(c.course_id === course.course_id){
+                            this.$set(c, 'expanded', !c.expanded)
+                        }
+                        else{
+                            this.$set(c, 'expanded', false)
+                        }
+                    })
+                })
+            }
+        },
+
         nullify(i, c) {
 
             if(!confirm('Вы уверены? Потом прогресс не восстановить')) {
@@ -229,15 +276,36 @@ export default {
                 .catch(e => console.log(e));
 
             loader.hide();
+        },
+
+        regress(user_id, course_id, coureItem){
+            if(!confirm('Вы уверены? Потом прогресс не восстановить')) return
+
+            const loader = this.$loading.show()
+
+            axios.post('/course/regress', {
+                type: 'item',
+                user_id,
+                course_id,
+                course_item_id: coureItem.id
+            }).then((response) => {
+                // coureItem.progress = '0%'
+                // coureItem.started_at = ''
+                // coureItem.ended_at = ''
+                // coureItem.status = 'Запланирован'
+                // coureItem.points = '0 / 0 / 0%'
+                // coureItem.progress_on_week = '0%'
+                this.$toast.success('Прогресс по разделу курса обнулен')
+            }).catch(e => console.log(e))
+
+            loader.hide()
         }
-        
-     
-    } 
+    }
 }
 </script>
 
 
-<style scoped>
+<style scoped lang="scss">
 .nullify {
     right: -6px;
     top: -2px;
@@ -247,8 +315,17 @@ export default {
     border-radius: 50px;
     display: none;
     cursor: pointer;
+    position: absolute;
+}
+.nullify-wrap{
+    padding: 5px 10px;
+    margin: -5px -10px;
 }
 .nullify-wrap:hover .nullify {
     display: block;
+}
+
+.expanded-course-item{
+    background: lighten(#c0def2, 5%);
 }
 </style>
