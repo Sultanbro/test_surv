792556762dc9813677876e1b60d3cd21d6e2591e
abik50000 <abik50000@gmail.com>
2022-12-01 14:33:31 +0600

    move logic from DTO to Service

--- app/Service/Courses/CourseProgressService.php
+++ app/Service/Courses/CourseProgressService.php
@@ -4,6 +4,9 @@ namespace App\Service\Courses;
 
 use App\DTO\CourseProgressDTO;
 use App\Exceptions\NotResultsException;
+use App\Models\CourseItem;
+use App\Models\CourseItemModel;
+use App\Models\TestBonus;
 use Illuminate\Support\Facades\Log;
 
 /**
@@ -22,16 +25,49 @@ class CourseProgressService
      * @throws NotResultsException
      */
     public function handle(): array
-    {
+    {   
         try {
             return [
                 'course'      => $this->dto->course,
-                'courseItems' => $this->dto->courseItems
+                'courseItems' => $this->getCourseItems()
             ];
         } catch (NotResultsException $exception) {
             Log::error($exception->getMessage());
 
             throw new NotResultsException($exception->getMessage());
         }
+    }
+
+    /**
+     * Получить этапы курса с прогрессом
+     * 
+     * @return \Illuminate\Support\Collection <CourseItem>
+     */
+    private function getCourseItems()
+    {
+        $userId = $this->dto->user->id;
+        $courseId = $this->dto->course->id;
+
+        return CourseItem::with('element')
+            ->where('course_id', $courseId)
+            ->get()
+            ->map(function($item) use ($userId) {
+                $item->element_id = $item->element ? $item->element->id : 0;
+        
+                $item->stages = $item->element ? count($item->element->getOrder()) : 0;
+                $item->passed_stages = $item->element
+                    ? (new CourseItemModel())->progress($userId, $item->id, $item->element)
+                    : [];
+
+                $item->bonuses = TestBonus::query()
+                                    ->selectRaw("SUM(amount) as sum")
+                                    ->where('course_item_id', $item->id)
+                                    ->where('user_id', $userId)
+                                    ->first()
+                                    ->sum;
+
+                unset($item->element);
+                return $item;
+            });
     }
 }
