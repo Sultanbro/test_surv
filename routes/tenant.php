2eeaf426b198d197ca38ea40e0fb89430abf685c
ysharofidinov <cool.krutoi@inbox.ru>
2022-12-14 11:29:14 +0600

    fix: refactor settings

--- routes/tenant.php
+++ routes/tenant.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\Admin\QualityController;
 use App\Http\Controllers\Admin\QuartalBonusController;
 use App\Http\Controllers\Admin\SalaryController;
 use App\Http\Controllers\Admin\TimeTrack\SettingController as TimeTrackSetting;
+use App\Http\Controllers\Admin\TimeTrack\TimeTrackSettingController;
 use App\Http\Controllers\Admin\TimetrackingController;
 use App\Http\Controllers\Admin\TopController;
 use App\Http\Controllers\Admin\TraineeController;
@@ -408,8 +409,14 @@ Route::middleware([
     Route::any('/timetracking/info', [TimetrackingController::class, 'info']);
     Route::any('/timetracking/set-day', [TimetrackingController::class, 'setDay']);
     Route::any('/timetracking/history', [TimetrackingController::class, 'getHistory']);
+
+    /**
+     * Route после рефактора метода settings в TimetrackController
+     */
     Route::any('/timetracking/settings-new', [TimeTrackSettingController::class, 'setting']);
-    Route::any('/timetracking/settings', [TimeTrackSettingController::class, 'setting']);
+    #=======================
+
+    Route::any('/timetracking/settings', [TimetrackingController::class, 'settings']);
     Route::any('/timetracking/settings/positions', [TimetrackingController::class, 'positions']);
     Route::any('/timetracking/settings/positions/get', [TimetrackingController::class, 'getPosition']);
     Route::any('/timetracking/settings/positions/save', [TimetrackingController::class, 'savePositions']);
@@ -417,8 +424,17 @@ Route::middleware([
     Route::post('/timetracking/settings/positions/delete', [TimetrackingController::class, 'deletePosition']);
     Route::post('timetracking/settings/get_time_addresses', [TimetrackingController::class, 'getTimeAddresses']);
     Route::post('timetracking/settings/save_time_addresses', [TimetrackingController::class, 'saveTimeAddresses']);
-    Route::any('/timetracking/settings/add', [TimeTrackSettingController::class, 'create']);
-    Route::any('/timetracking/settings/delete', [TimeTrackSettingController::class, 'delete']);
+    Route::any('/timetracking/settings/add', [TimetrackingController::class, 'addsettings']);
+    Route::any('/timetracking/settings/delete', [TimetrackingController::class, 'deletesettings']);
+
+    /**
+     * Route-ы после рефактора методов addsettings, deletesettings в TimetrackController
+     */
+    Route::post('/timetracking/settings/add-new', [TimeTrackSettingController::class, 'create']);
+    Route::delete('/timetracking/settings/delete-new', [TimeTrackSettingController::class, 'delete']);
+    #==============================
+
+
     Route::any('/timetracking/user/save', [TimetrackingController::class, 'saveprofile']);
     Route::post('/timetracking/settings/notifications/update', [TimetrackingController::class, 'updateNotificationTemplate']);
     Route::get('/timetracking/settings/notifications/get', [TimetrackingController::class, 'getNotificationTemplates']);
