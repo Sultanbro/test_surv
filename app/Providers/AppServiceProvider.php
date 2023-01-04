2881b794fe63f4cd0846841350f2ab20bd8a7ba3
Abik Khoja <abik50000@gmail.com>
2023-01-04 09:16:57 +0000

    FIX owner <add cabinet> btn

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,37 +34,9 @@ class AppServiceProvider extends ServiceProvider
         });
 
         \View::composer('layouts.spa', function($view) {
-
-            if(!\Auth::guest()) {
-
-                $permissions = auth()->user()->getAllPermissions()->pluck('name')->toArray(); // Spatie permissions
-
-                if(auth()->user()->program_id === 1) {
-                    $permissions[] = 'ucalls_view';
-                }
-
-                $view->with([
-                    'laravelToVue' => [
-                        'csrfToken'   => csrf_token(),
-                        'userId'      => auth()->id(),
-                        'avatar'      => isset(auth()->user()->img_url) && !is_null(auth()->user()->img_url) && auth()->user()->img_url !== ''
-                            ? '/users_img/' . auth()->user()->img_url
-                            : 'https://cp.callibro.org/files/img/8.png',
-                        'email'       => auth()->user()->email,
-                        'is_admin'    => auth()->user()->is_admin == 1,
-                        'permissions' => $permissions,
-                        'tenants'     => auth()->user()->tenants()->pluck('id')->toArray()
-                    ]
-                ]);
-
-            } else {
-                $view->with([
-                    'laravelToVue' => [
-                        'csrfToken'   => csrf_token(),
-                    ]
-                ]);
-            }
-
+            $view->with([
+                'laravelToVue' => $this->dataToVue()
+            ]);
         });
 
         \View::composer('home', function($view) {
