9e7ccfc0d70a89a3eef8b3b7e06c9ef6ad66497b
takodachi2501 <vrnehot@yandex.kz>
2023-01-13 10:21:44 +0600

    SCRUM-191 кнопка битрикса только на странице профиля

--- resources/js/views/FAQView.vue
+++ resources/js/views/FAQView.vue
@@ -10,18 +10,6 @@ export default {
 		return {
 			page: '',
 		}
-	},
-	mounted(){
-		(function(w,d,u){
-			var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);s.id='bitrix-loader';
-			var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-		})(window,document,'https://cdn-ru.bitrix24.kz/b1734679/crm/site_button/loader_8_dzfbjh.js');
-	},
-	beforeUnmount(){
-		const scriptTag = document.getElementById('bitrix-loader')
-		if(!scriptTag) return
-		scriptTag.remove()
-		// remove bitrix site button
 	}
 }
 </script>
