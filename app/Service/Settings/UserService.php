2aeb0f828360d88e52fa704c1149d5d1085cde3a
ysharofidinov <cool.krutoi@inbox.ru>
2023-01-23 16:01:07 +0600

    fix: fix user store

--- app/Service/Settings/UserService.php
+++ app/Service/Settings/UserService.php
@@ -108,8 +108,8 @@ class UserService
             {
                 $user = $this->userRepository->updateOrCreateNewEmployee($dto->toArray());
             }
-
-            (new DepartmentUserService)->setGroup($dto->group, $user->id, 'add');
+            dd($user);
+             (new DepartmentUserService)->setGroup($dto->group, $user->id, 'add');
 
             if ($dto->headGroup != 0 && $dto->positionId == Position::GROUP_HEAD)
             {
