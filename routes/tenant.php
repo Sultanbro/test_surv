9c8e20c0a62cf005bfb16ae900c53db65b4db2c2
ysharofidinov <cool.krutoi@inbox.ru>
2023-01-23 16:24:57 +0600

    fix: fix user store

--- routes/tenant.php
+++ routes/tenant.php
@@ -69,7 +69,7 @@ Route::middleware(['web','tenant','not_admin_subdomain'])->group(function () {
     Route::any('/timetracking/user/{id}', [User\EmployeeController::class, 'profile']);
     Route::any('/timetracking/get-persons', [User\EmployeeController::class, 'getpersons']);
     Route::get('/timetracking/create-person', [User\EmployeeController::class, 'createPerson'])->name('users.create');
-    Route::post('/timetracking/person/store', [Settings\UserController::class, 'store'])->name('users.store');
+    Route::post('/timetracking/person/store', [User\EmployeeController::class, 'storePerson'])->name('users.store');
     Route::get('/timetracking/edit-person', [User\EmployeeController::class, 'editperson'])->name('users.edit');
     Route::post('/timetracking/person/update', [User\EmployeeController::class, 'updatePerson'])->name('users.update');
     Route::post('/timetracking/edit-person/group', [User\EmployeeController::class, 'editPersonGroup']); // Удалять добавлять пользователя в группы
