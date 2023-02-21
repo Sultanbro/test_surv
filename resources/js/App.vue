665be0c9488a96d9953d2083514dba259d679e8e
timur <armor.d.r.x@gmail.com>
2023-02-21 16:07:58 +0600

    more hotfix

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,10 +2,12 @@
 export default {
 	name: 'JobtronApp',
 	mounted(){
-		(function(w,d,u){
-			var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);s.id='bitrix-loader';
-			var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-		})(window,document,'https://cdn-ru.bitrix24.kz/b1734679/crm/site_button/loader_8_dzfbjh.js');
+		if(window.location.hostname.split('.')[0] === 'bp'){
+			(function(w,d,u){
+				var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);s.id='bitrix-loader';
+				var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+			})(window,document,'https://cdn-ru.bitrix24.kz/b1734679/crm/site_button/loader_8_dzfbjh.js');
+		}
 	},
 	beforeUnmount(){
 		const scriptTag = document.getElementById('bitrix-loader')
