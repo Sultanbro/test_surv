989d3a36b47d61172a66b11028ec08d35abe02a4
SeregaWong <serega1g1g@gmail.com>
2023-02-22 14:15:16 +0800

    fix: yookassa config

--- config/yookassa.php
+++ config/yookassa.php
@@ -2,10 +2,6 @@
 
 return [
     #YooKassa config test
-    'merchant_id' => 983846,
-    'secret_key' => 'test_oBJ51_hczCE2OfZc-rW85D_83YrrRPLkhpxQRicXthU',
-
-    #Yookassa config production
-//    'merchant_id' => 978439,
-//    'secret_key' => 'live_UCwoQ2PpDv9rEx1LTgsECvyk_jNUo6XfQBVHUhECv08',
+    'merchant_id' => env('YOOKASSA_MERCHANT_ID', false),
+    'secret_key' => env('YOOKASSA_SECRET_KEY', false),
 ];
