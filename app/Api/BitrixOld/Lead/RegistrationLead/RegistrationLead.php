46b0e87d8bdde908c1fb2725c57eff7e1f5fec58
SeregaWong <serega1g1g@gmail.com>
2023-02-27 19:01:15 +0800

    feat: RegistrationLead

---
+++ app/Api/BitrixOld/Lead/RegistrationLead/RegistrationLead.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Api\BitrixOld;
+
+use App\Api\BitrixOld;
+use App\Api\BitrixOld\Lead\Field;
+use App\Api\BitrixOld\Lead\Field\AssignedToValeria as AssignedToValeriaField;
+use App\Api\BitrixOld\Lead\Field\Phone as PhoneField;
+use App\Api\BitrixOld\Lead\Fields;
+use App\User;
+
+final class RegistrationLead extends Lead
+{
+
+    public function __construct(
+        User $user,
+        ?BitrixOld $bitrix,
+    )
+    {
+        parent::__construct(new Fields(
+            new Field('TITLE', "Jobtron.org - Регистрация " . $user->name),
+            new Field('NAME', $user->name),
+            new AssignedToValeriaField(),
+        ), $bitrix);
+
+        if ($user->phone) {
+            $this->fields->addFields(new PhoneField($user->phone));
+        }
+    }
+}
