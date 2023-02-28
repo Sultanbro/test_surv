df7b6580b5ed659eb794e141888e863811d659d4
takodachi2501 <vrnehot@yandex.kz>
2023-02-28 15:00:17 +0600

    hotfix: правки текста в лендинге

--- resources/js/home/components/section1/Section1Profile.vue
+++ resources/js/home/components/section1/Section1Profile.vue
@@ -1,6 +1,11 @@
 <script>
+import Section1InlinePopup from './Section1InlinePopup'
+
 export default {
 	name: 'Section1Profile',
+	components: {
+		Section1InlinePopup
+	},
 	computed: {
 		lang () {
 			return this.$root.$data.lang
@@ -11,6 +16,27 @@ export default {
 <template>
 	<div class="jSec1-tabs-item jSec1-profile">
 		<div class="jSec1-profile-text">
+			<h3 class="jSec1-profile-title">
+				{{ $lang(lang, 's1-for-super-profile') }}
+			</h3>
+			<ul class="jSec1-profile-list">
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-profile-1') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-profile-1p') }}</Section1InlinePopup>
+				</li>
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-profile-2') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-profile-2p') }}</Section1InlinePopup>
+				</li>
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-profile-3') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-profile-3p') }}</Section1InlinePopup>
+				</li>
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-profile-4') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-profile-4p') }}</Section1InlinePopup>
+				</li>
+			</ul>
 			<h3 class="jSec1-profile-title">
 				{{ $lang(lang, 's1-for-worker-profile') }}
 			</h3>
@@ -28,23 +54,6 @@ export default {
 					{{ $lang(lang, 's1-for-worker-profile-4') }}
 				</li>
 			</ul>
-			<h3 class="jSec1-profile-title">
-				{{ $lang(lang, 's1-for-super-profile') }}
-			</h3>
-			<ul class="jSec1-profile-list">
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-profile-1') }}
-				</li>
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-profile-2') }}
-				</li>
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-profile-3') }}
-				</li>
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-profile-4') }}
-				</li>
-			</ul>
 			<a
 				href="/register"
 				class="jSec1-profile-button jButton"
@@ -52,7 +61,7 @@ export default {
 		</div>
 		<div class="jSec1-profile-banner">
 			<img
-				:src="require('../../assets/img/profile.jpg').default"
+				:src="require('../../assets/img/profile.gif').default"
 				alt=""
 				class="jSec1-profile-banner-img"
 			>
--- resources/js/home/components/section1/Section1Struct.vue
+++ resources/js/home/components/section1/Section1Struct.vue
@@ -1,6 +1,10 @@
 <script>
+import Section1InlinePopup from './Section1InlinePopup'
 export default {
 	name: 'Section1Struct',
+	components: {
+		Section1InlinePopup
+	},
 	computed: {
 		lang () {
 			return this.$root.$data.lang
@@ -12,6 +16,23 @@ export default {
 	<div class="jSec1-tabs-item jSec1-profile">
 		<div class="jSec1-profile-text">
 			<h3 class="jSec1-profile-title">
+				{{ $lang(lang, 's1-for-super-struct') }}
+			</h3>
+			<ul class="jSec1-profile-list">
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-struct-1') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-struct-1p') }}</Section1InlinePopup>
+				</li>
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-struct-2') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-struct-2p') }}</Section1InlinePopup>
+				</li>
+				<li class="jSec1-profile-list-item">
+					{{ $lang(lang, 's1-for-super-struct-3') }}
+					<Section1InlinePopup>{{ $lang(lang, 's1-for-super-struct-3p') }}</Section1InlinePopup>
+				</li>
+			</ul>
+			<h3 class="jSec1-profile-title">
 				{{ $lang(lang, 's1-for-worker-struct') }}
 			</h3>
 			<ul class="jSec1-profile-list">
@@ -20,20 +41,6 @@ export default {
 				</li>
 				<li class="jSec1-profile-list-item">
 					{{ $lang(lang, 's1-for-worker-struct-2') }}
-				</li>
-			</ul>
-			<h3 class="jSec1-profile-title">
-				{{ $lang(lang, 's1-for-super-struct') }}
-			</h3>
-			<ul class="jSec1-profile-list">
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-struct-1') }}
-				</li>
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-struct-2') }}
-				</li>
-				<li class="jSec1-profile-list-item">
-					{{ $lang(lang, 's1-for-super-struct-3') }}
 				</li>
 			</ul>
 			<a
@@ -46,6 +53,7 @@ export default {
 				:src="require('../../assets/img/struct.png').default"
 				alt=""
 				class="jSec1-profile-banner-img"
+				width="760"
 			>
 		</div>
 	</div>
