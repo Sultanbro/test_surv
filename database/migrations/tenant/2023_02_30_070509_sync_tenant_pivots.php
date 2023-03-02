d5165d285e153abce2dd9a3a1080a87f0ff6a991
SeregaWong <serega1g1g@gmail.com>
2023-03-02 20:53:05 +0800

    hotfix: sync tenant pivot migration

---
+++ database/migrations/tenant/2023_02_30_070509_sync_tenant_pivots.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tenantUserEmails = DB::table('users')
+            ->select(['email'])
+            ->get()
+            ->pluck('email')
+            ->toArray();
+
+        $centralUserIds = DB::connection('mysql')
+            ->table('users')
+            ->select(['id'])
+            ->whereIn('email', $tenantUserEmails)
+            ->get()
+            ->pluck('id')
+            ->toArray();
+        
+        $tenantId = tenant('id');
+
+        $tenantPivotUserIds = DB::connection('mysql')
+            ->table('tenant_pivot')
+            ->select(['user_id'])
+            ->where('tenant_id', $tenantId)
+            ->whereIn('user_id', $centralUserIds)
+            ->get()
+            ->pluck('user_id')
+            ->toArray();
+
+        $newTenantPivots = [];
+
+        foreach ($centralUserIds as $centralUserId) {
+            if (in_array($centralUserId, $tenantPivotUserIds )) continue;
+
+            array_push($newTenantPivots, [
+                'tenant_id' => $tenantId,
+                'user_id' => $centralUserId,
+                'owner' => 0,
+            ]);
+        }
+
+        DB::connection('mysql')
+            ->table('tenant_pivot')
+            ->insert($newTenantPivots);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
