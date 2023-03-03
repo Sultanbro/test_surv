729fb0dbe8a5cb486253e6616b0de2f6c0a8564a
ysharofidinov <cool.krutoi@inbox.ru>
2023-03-03 16:40:22 +0600

    fix: fix

--- app/Service/WorkChart/Types/FiveByTwoChart.php
+++ app/Service/WorkChart/Types/FiveByTwoChart.php
@@ -8,6 +8,7 @@ use App\Enums\WorkChart\WorkChartEnum;
 use App\Service\WorkChart\Chart;
 use App\Support\Core\CustomException;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 
 class FiveByTwoChart implements Chart
@@ -15,6 +16,7 @@ class FiveByTwoChart implements Chart
     /**
      * @param Builder $builder
      * @return bool
+     * @throws Exception
      */
     public function chartProcess(Builder $builder): bool
     {
@@ -28,7 +30,8 @@ class FiveByTwoChart implements Chart
 
         if ($countOfDayPerWeek >= WorkChartEnum::COUNT_OF_WORK_DAY_FOR_FIVE_BY_TWO)
         {
-            new CustomException("Вы отработали количество рабочих дней для своего графика", ErrorCode::BAD_REQUEST, []);
+            throw new Exception("Вы отработали количество рабочих дней для своего графика", ErrorCode::BAD_REQUEST, []);
+
         }
 
         return true;
