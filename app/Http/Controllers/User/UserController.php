ddc578b0e00bd3788cd41831f8724d4373a7840d
SeregaWong <serega1g1g@gmail.com>
2023-03-14 13:08:02 +0800

    fix: user request type

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -7,13 +7,14 @@ use App\Http\Resources\Responses\JsonSuccessResponse;
 use App\Http\Resources\Users\UserResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use \Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     /**
      * @return JsonResponse
      */
-    public function me($request): JsonResponse
+    public function me(Request $request): JsonResponse
     {
         return response()->json(
             new JsonSuccessResponse(
