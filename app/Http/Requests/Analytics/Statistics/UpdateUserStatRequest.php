d7eabe45203ffcd5835ce4a0c360e873ebaf74b8
ysharofidinov <cool.krutoi@inbox.ru>
2023-03-28 16:35:12 +0600

    fix: request check null

--- app/Http/Requests/Analytics/Statistics/UpdateUserStatRequest.php
+++ app/Http/Requests/Analytics/Statistics/UpdateUserStatRequest.php
@@ -46,7 +46,7 @@ class UpdateUserStatRequest extends FormRequest
         $activityId = Arr::get($validated, 'id');
         $groupId = Arr::get($validated, 'group_id');
         $employeeId = Arr::get($validated, 'employee_id');
-        $value = Arr::get($validated, 'value');
+        $value = Arr::get($validated, 'value') ?? 0;
         $year = Arr::get($validated, 'year');
         $month = Arr::get($validated, 'month');
         $day = Arr::get($validated, 'day');
