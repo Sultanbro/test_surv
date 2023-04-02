4210f1dfd5751c651ff92555c517068ed22c672b
takodachi2501 <vrnehot@yandex.kz>
2023-04-02 17:28:41 +0600

    hotfix: исправлено отображение баланса в рублях

--- resources/js/stores/ProfileSalary.js
+++ resources/js/stores/ProfileSalary.js
@@ -16,6 +16,11 @@ export const useProfileSalaryStore = defineStore('profileSalary', {
 			try{
 				const data = await fetchProfileSalary(year, month)
 				this.user_earnings = data.user_earnings
+				this.user_earnings.sumSalary = parseInt(this.user_earnings.sumSalary)
+				this.user_earnings.sumKpi = parseInt(this.user_earnings.sumKpi)
+				this.user_earnings.sumBonuses = parseInt(this.user_earnings.sumBonuses)
+				this.user_earnings.sumQuartalPremiums = parseInt(this.user_earnings.sumQuartalPremiums)
+				this.user_earnings.sumNominations = parseInt(this.user_earnings.sumNominations)
 				this.has_qp = data.has_qp
 				this.isReady = true
 			}
