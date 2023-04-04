b5e7082f46aad5d66e4cc97272ba34e384362344
Asset1996 <khaitbay96@gmail.com>
2023-04-04 16:23:24 +0500

    hotfix

--- app/Service/KpiStatisticService.php
+++ app/Service/KpiStatisticService.php
@@ -1393,7 +1393,10 @@ class KpiStatisticService
                             $count = 0;
 
                             foreach ($weeks as $key => $week) {
-                                $val = $query->whereBetween('day', [$week[0], $week[count($week) - 1]])->avg('value');
+                                if(isset($week[0]))
+                                $val = isset($week[0])
+                                    ? $query->whereBetween('day', [$week[0], $week[count($week) - 1]])->avg('value')
+                                    : 0;
 
                                 if($val && $val > 0) {
                                     $avg += $val;
@@ -1513,7 +1516,9 @@ class KpiStatisticService
                     $count = 0;
 
                     foreach ($weeks as $key => $week) {
-                        $val = $user->whereBetween('day', [$week[0], $week[count($week) - 1]])->avg('value');
+                        $val = isset($week[0])
+                            ? $user->whereBetween('day', [$week[0], $week[count($week) - 1]])->avg('value')
+                            : 0;
 
                         if($val && $val > 0) {
                             $avg += $val;
