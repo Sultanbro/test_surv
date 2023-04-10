18cf9ae5429f5ff96fd6c7b059d32f14da91658d
ysharofidinov <cool.krutoi@inbox.ru>
2023-04-10 15:32:29 +0600

    fix: excel export fix

--- app/Http/Controllers/Salary/SalaryController.php
+++ app/Http/Controllers/Salary/SalaryController.php
@@ -478,8 +478,6 @@ class SalaryController extends Controller
                         users.phone as phone,
                         users.program_id as program_id,
                         CONCAT(users.last_name,' ',users.name) as full_name,
-                        users.working_time_id as working_time_id,
-                        users.working_day_id as working_day_id,
                         users.birthday as birthday,
                         z.zarplata as salary,
                         z.card_kaspi as card_kaspi,
@@ -491,8 +489,8 @@ class SalaryController extends Controller
                         users.currency as currency,
                         CONCAT('KASPI', '') as card
                         ")
-            ->groupBy('id', 'phone', 'full_name', 'workDay', 'working_time_id', 'workTime', 'salary', 
-            'card_kaspi', 'card_jysan', 'jysan', 'kaspi','kaspi_cardholder','jysan_cardholder', 'card', 'program_id', 'birthday','currency', 'working_day_id')
+            ->groupBy('id', 'phone', 'full_name', 'salary',
+            'card_kaspi', 'card_jysan', 'jysan', 'kaspi','kaspi_cardholder','jysan_cardholder', 'card', 'program_id', 'birthday','currency')
             ->get();
 
             //if($users->where('id', 14073)->first()) dd($users_ids);
