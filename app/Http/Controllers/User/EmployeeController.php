1143729c20d16d9f38e00ebdf59255624e030f0f
ysharofidinov <cool.krutoi@inbox.ru>
2023-04-24 12:42:21 +0600

    fix: dd to get persons api

--- app/Http/Controllers/User/EmployeeController.php
+++ app/Http/Controllers/User/EmployeeController.php
@@ -203,7 +203,7 @@ class EmployeeController extends Controller
             'ud.fire_cause',
             'ud.applied',
         ]);
-
+        dd($users);
         foreach ($users as $key => $user) {
 
             $_user = User::withTrashed()->find($user->id);
