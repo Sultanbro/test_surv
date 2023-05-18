9314f82dad05cea8640fbadd301725b0b711a208
ysharofidinov <cool.krutoi@inbox.ru>
2023-05-18 11:05:31 +0600

    fix: chat fix

--- packages/eddir/messenger/src/Messenger.php
+++ packages/eddir/messenger/src/Messenger.php
@@ -102,8 +102,8 @@ class Messenger {
 
         if ( $chat->private ) {
             // get second user in private chat
-
-            $second_user = $chat->users->firstWhere( 'id', '!=', $user->id );
+            $users =  $chat->users;
+            $second_user = $users->count() >= 2 ? $users->firstWhere( 'id', '!=', $user->id ) : $users->firstWhere( 'id', '=', $user->id );
 
             if ( $second_user && !$second_user->deleted_at) {
                 $chat->second_user = $second_user;
