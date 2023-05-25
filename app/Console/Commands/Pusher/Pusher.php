d5c9ec8fb7dab5f37154d7a3b2af2660c669ecbf
ysharofidinov <cool.krutoi@inbox.ru>
2023-05-25 15:37:31 +0600

    fix: fix pusher

--- app/Console/Commands/Pusher/Pusher.php
+++ app/Console/Commands/Pusher/Pusher.php
@@ -30,10 +30,10 @@ class Pusher extends Command
     /**
      * Execute the console command.
      *
-     * @return ?bool
+     * @return void
      * @throws Exception
      */
-    public function handle(): ?bool
+    public function handle(): void
     {
         $notifications = MailingNotification::with('recipients')
             ->whereIn('frequency', [MailingEnum::DAILY, MailingEnum::WEEKLY, MailingEnum::MONTHLY])
@@ -48,7 +48,7 @@ class Pusher extends Command
                 throw new Exception("Method $frequency does not exist");
             }
 
-            return $this->{$frequency}($notification);
+            $this->{$frequency}($notification);
         }
     }
 
