c3a614c62cb025be92489270b291e7940459e4c8
takodachi2501 <vrnehot@yandex.kz>
2023-06-20 09:17:25 +0600

    fix: аналитика - мягкие цвета для градииента

--- resources/js/pages/AnalyticsPage.vue
+++ resources/js/pages/AnalyticsPage.vue
@@ -691,12 +691,13 @@ export default {
 			const perc = percentMinMax(value, this.yearMin, this.yearMax) * 100
 			let r, g, b = 0;
 			if(perc < 50) {
-				r = 255;
+				r = 235;
 				g = Math.round(5.1 * perc);
+				b = Math.round(113 - 1.13 * perc);
 			}
 			else {
-				g = 255;
-				r = Math.round(510 - 5.10 * perc);
+				g = 225;
+				r = Math.round(510 - 5.1 * perc);
 			}
 			const h = r * 0x10000 + g * 0x100 + b * 0x1;
 			return '#' + ('000000' + h.toString(16)).slice(-6);
