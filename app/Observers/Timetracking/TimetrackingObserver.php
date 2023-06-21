cd81dd4a4a167b87c01799febc6a91cc2b7a0ad4
galyym <ksgalym@mail.ru>
2023-06-21 11:11:25 +0500

    fix: Attempt to read property "work_charts_type" on null

--- app/Observers/Timetracking/TimetrackingObserver.php
+++ app/Observers/Timetracking/TimetrackingObserver.php
@@ -20,7 +20,7 @@ class TimetrackingObserver
         $user = User::findOrFail($timetracking->user_id);
         if (empty($user->first_work_day)) {
             $workChart = $user->getWorkChart();
-            if ($workChart->work_charts_type === WorkChartModel::WORK_CHART_TYPE_REPLACEABLE) {
+            if ($workChart->work_charts_type && $workChart->work_charts_type === WorkChartModel::WORK_CHART_TYPE_REPLACEABLE) {
                 $user->first_work_day = Carbon::now()->format("Y-m-d 00:00:00");
                 $user->save();
             }
