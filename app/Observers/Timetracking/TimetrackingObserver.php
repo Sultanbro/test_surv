535f01e2ead1831a6c6033217e7c3cb0ba8417e3
galyym <ksgalym@mail.ru>
2023-06-22 11:30:42 +0500

    fix: check work_charts_type

--- app/Observers/Timetracking/TimetrackingObserver.php
+++ app/Observers/Timetracking/TimetrackingObserver.php
@@ -20,11 +20,10 @@ class TimetrackingObserver
         $user = User::findOrFail($timetracking->user_id);
         if (empty($user->first_work_day)) {
             $workChart = $user->getWorkChart();
-            if ($workChart->work_charts_type && $workChart->work_charts_type === WorkChartModel::WORK_CHART_TYPE_REPLACEABLE) {
+            if (property_exists($workChart, "work_charts_type") && $workChart->work_charts_type !== null && $workChart->work_charts_type === WorkChartModel::WORK_CHART_TYPE_REPLACEABLE) {
                 $user->first_work_day = Carbon::now()->format("Y-m-d 00:00:00");
                 $user->save();
             }
         }
-
     }
 }
