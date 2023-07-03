16893218abbcef1b21425f2b49cad12ad665a9b1
takodachi2501 <vrnehot@yandex.kz>
2023-07-03 10:07:19 +0600

    fix: профиль - справлено зависание шкал баланса при обновлении кэша

--- resources/js/pages/Profile/IntroStats.vue
+++ resources/js/pages/Profile/IntroStats.vue
@@ -376,6 +376,10 @@ export default {
 				})
 			})
 		},
+		fixStat(prop, value, max = 1){
+			this.animateValues[prop].value = this.separateNumber(parseInt(value))
+			this.animateValues[prop].height = value / max * 100
+		},
 
 		async animateStats(){
 			const MAXBALANCE = parseInt(this.salary.replace(/\s/g, '')) || 0
@@ -392,6 +396,11 @@ export default {
 				this.animateStat('premium', this.user_earnings.sumQuartalPremiums, MAXKVARTAL),
 				this.animateStat('nominations', this.user_earnings.sumNominations, MAXNOMINATIONS),
 			]);
+			this.fixStat('balance', this.user_earnings.sumSalary, parseInt(this.salary.replace(/\s/g, '')) || 0)
+			this.fixStat('kpi', this.user_earnings.sumKpi, this.user_earnings.kpiMax)
+			this.fixStat('bonus', this.user_earnings.sumBonuses)
+			this.fixStat('premium', this.user_earnings.sumQuartalPremiums)
+			this.fixStat('nominations', this.user_earnings.sumNominations)
 			this.animated = false
 		},
 		separateNumber(x){
