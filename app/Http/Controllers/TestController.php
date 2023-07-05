f91ca7e3dea2d410d64984ce5fbb437d8d3a0eb9
Abik Khoja <abik50000@gmail.com>
2023-07-05 08:57:05 +0000

    fix hh

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,7 +10,7 @@ class TestController extends Controller
 
 	public function test() {
 		// https://hh.ru/oauth/authorize?response_type=code&client_id=LPAJVTT5AU6U3CJBC1M8RL0KQ5CR2N5OBBEBCHKDK5EJ8V450919BEOMSQOTHNTI&state=um_state&redirect_uri=https://bpartners.kz/token
-		$auth_code = 'MDGUQDPM2ML50UTBIT6G8DM2L4JQ4L9DHE829U59B1TE0M89JP25GB8Q2G0KUM71';
+		$auth_code = 'RQ5BLDMUIGDTDP2SL2RVLQQ0TPE2N45QQ3BD8AUVFV2UOSBMNJO7C2PPKL10T42I';
 		dd((new \App\Api\HeadHunter)->refresh($auth_code));
 	}
 }
