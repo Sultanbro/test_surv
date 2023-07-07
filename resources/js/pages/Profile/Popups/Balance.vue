408fd9174487e4e14400d3d628cd6770a8fe61e9
takodachi2501 <vrnehot@yandex.kz>
2023-07-07 10:13:47 +0600

    fix: начисления, профиль - цвета в таблице начислений

--- resources/js/pages/Profile/Popups/Balance.vue
+++ resources/js/pages/Profile/Popups/Balance.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="popup__content  mt-3"
+		class="PopupBalance popup__content  mt-3"
 		:class="{'v-loading': loading}"
 	>
 		<div class="popup__filter pb-4">
@@ -21,7 +21,7 @@
 							v-for="field in fields"
 							:class="{
 								'text-center': field.key != '0',
-								[field.class]: field.class !== undefined
+								'SalaryCell-weekend-0': field.weekend
 							}"
 							:key="field.key"
 						>
@@ -48,15 +48,11 @@
 							v-for="field in fields"
 							:key="field.key"
 							@click="showHistory(field.key)"
-							:class="{
-								'day-fine': item[field.key] !== undefined && item[field.key].hasFine,
-								'day-avans': item[field.key] !== undefined && item[field.key].hasAvans,
-								'day-bonus': item[field.key] !== undefined && item[field.key].hasBonus,
-								'text-center': field.key != '0',
-								'day-training': item[field.key] !== undefined && item[field.key].training,
+							:class="[{
+								'text-center': field.key,
 								'balance__table-day': parseInt(field.key) > 0,
-								[field.class]: field.class !== undefined
-							}"
+								'SalaryCell': item[field.key] && item[field.key].dayType,
+							}, item[field.key] ? `SalaryCell${field.weekend ? '-weekend' : ''}-${item[field.key].dayType || '0'}` : '']"
 						>
 							<template v-if="item[field.key] !== undefined">
 								{{ item[field.key].value }}
@@ -188,6 +184,7 @@ import { usePortalStore } from '@/stores/Portal'
 import BalanceItem from './BalanceItem'
 import { useYearOptions } from '@/composables/yearOptions'
 import DateSelect from '../DateSelect'
+import salaryCellType from '@/composables/salaryCellType'
 
 export default {
 	name: 'PopupBalance',
@@ -199,10 +196,17 @@ export default {
 	data: function () {
 		const now = new Date()
 		return {
-			data: [],
-			items: [],
-			totalFines: null,
-			total_avanses: null,
+			data: {
+				hours: [],
+				salaries: [],
+				times: [],
+				total: {
+					salaries: 0,
+					hours: 0,
+				}
+			},
+			totalFines: 0,
+			total_avanses: 0,
 			selectedDate: this.$moment().format('DD.MM.YYYY'),
 			currentDay: now.getDate(),
 			history: null,
@@ -249,21 +253,40 @@ export default {
 				}
 			]
 
-
-
 			for (let i = 1; i <= this.daysInMonth; ++i) {
 				const dayName = this.$moment(`${i} ${this.currentMonth} ${this.currentYear}`, 'D MMMM YYYY').locale('en').format('ddd')
 				fields.push({
 					key: `${i}`,
 					label: `${i}`,
 					sortable: false,
-					class: `day ${dayName}`,
+					weekend: dayName === 'Sat' || dayName === 'Sun',
 				})
 			}
 			return fields
 		},
 		daysInMonth(){
 			return this.$moment(`${this.currentMonth} ${this.currentYear}`, 'MMMM YYYY').daysInMonth()
+		},
+		items(){
+			const totalSalary = Number(this.data.total.salaries) - Number(this.totalFines) - Number(this.total_avanses)
+			return [
+				{
+					...this.data.times,
+					0: {value: 'Время прихода'},
+				},
+				{
+					...this.data.salaries,
+					0: {value: 'Начисления'},
+					total: {value: Number(totalSalary).toFixed(0)},
+					avanses: {value: Number(this.total_avanses).toFixed(0)},
+					fines: {value: Number(this.totalFines).toFixed(0)},
+				},
+				{
+					...this.data.hours,
+					0: {value: 'Отработанные часы'},
+					total: {value: Number(this.data.total.hours).toFixed(1)}
+				},
+			]
 		}
 	},
 	watch: {
@@ -319,100 +342,48 @@ export default {
 			this.axios.post('/timetracking/zarplata-table-new', {
 				month: this.$moment(this.currentMonth, 'MMMM').format('M'),
 				year: this.currentYear
-			}).then(response => {
-				this.data = response.data.data
-				this.totalFines = response.data.totalFines
-				this.total_avanses = response.data.total_avanses
-
-				this.loadItems()
+			}).then(({data}) => {
+				this.data = this.prepareData(data.data)
+				this.totalFines = data.totalFines
+				this.total_avanses = data.total_avanses
 
 				this.showHistory()
 				this.loading = false
 			}).catch(e => console.error(e))
 		},
 
-		loadItems() {
-			const items = [];
-			const temp = [];
+		prepareData({salaries, hours, times}){
 			const total = {
 				salaries: 0,
 				hours: 0,
-			};
-
-			for (let key in this.data) {
-				temp[key] = []
-				for (let keyt in this.data[key]) {
-					const hasBonus = this.data[key][keyt]['bonuses'] !== undefined && this.data[key][keyt]['bonuses'].length
-					const hasAwards = this.data[key][keyt]['awards'] !== undefined && this.data[key][keyt]['awards'].length
-					const hasTestBonus = this.data[key][keyt]['test_bonus'] !== undefined && this.data[key][keyt]['test_bonus'].length
-					const hasFine = this.data[key][keyt]['fines'] !== undefined && this.data[key][keyt]['fines'].length
-					const hasAvans = this.data[key][keyt]['avanses'] !== undefined && this.data[key][keyt]['avanses'].length
-					temp[key][keyt] = ({
-						value: this.data[key][keyt]['value'],
-						fines: this.data[key][keyt]['fines'],
-						avanses: this.data[key][keyt]['avanses'],
-						bonuses: this.data[key][keyt]['bonuses'],
-						test_bonus: this.data[key][keyt]['test_bonus'],
-						awards: this.data[key][keyt]['awards'],
-						hasFine,
-						hasBonus: hasBonus || hasAwards || hasTestBonus,
-						hasAvans,
-						training: this.data[key][keyt]['training'],
-					})
-
-					if(key === 'times' && temp[key][keyt].value){
-						temp[key][keyt].value = this.$moment.utc(temp[key][keyt].value, 'hh:mm').local().format('hh:mm')
-					}
-
-					if(key == 'salaries' || key == 'hours') {
-						let val = Number(this.data[key][keyt].value);
-						total[key] += isNaN(val) ? 0 : val;
-					}
-				}
 			}
 
-			temp['salaries'][0] = {
-				value: 'Начисления',
-			};
+			for (let day of Object.keys(salaries)){
+				const hasBonus = salaries[day].bonuses && salaries[day].bonuses.length
+				const hasAwards = salaries[day].awards && salaries[day].awards.length
+				const hasTestBonus = salaries[day].test_bonus && salaries[day].test_bonus.length
+				const hasFine = salaries[day].fines && salaries[day].fines.length ? salaryCellType.FINE : 0
+				const hasAvans = salaries[day].avanses && salaries[day].avanses.length ? salaryCellType.AVANS : 0
+				const hasBonuses = hasBonus || hasAwards || hasTestBonus ? salaryCellType.BONUS : 0
+				const isTraning = salaries[day].training ? salaryCellType.TRAINING : 0
 
-			const total_salary = Number(total['salaries']) - Number(this.totalFines) - Number(this.total_avanses)
+				salaries[day].dayType = isTraning | hasBonuses | hasAvans | hasFine
 
-			temp['salaries']['total'] = {
-				value: Number(total_salary).toFixed(0),
-			};
-			temp['salaries']['avanses'] = {
-				value: Number(this.total_avanses).toFixed(0)
-			};
-			temp['salaries']['fines'] = {
-				value: Number(this.totalFines).toFixed(0)
-			};
+				let val = Number(salaries[day].value);
+				total.salaries += isNaN(val) ? 0 : val;
+			}
 
+			for (let day of Object.keys(times)){
+				if(times[day].value)
+					times[day].value = this.$moment.utc(times[day].value, 'hh:mm').local().format('hh:mm')
+			}
 
-			temp['times'][0] = {
-				value: 'Время прихода',
-			};
-			temp['hours'][0] = {
-				value: 'Отработанные часы',
-			};
-			temp['hours']['total'] = {
-				value: Number(total['hours']).toFixed(1),
-			};
-			temp['times']['avanses'] = {
-				value: 0
-			};
-			temp['times']['fines']= {
-				value: 0
-			};
-			temp['hours']['avanses'] = {
-				value: 0
-			};
-			temp['hours']['fines'] = {
-				value: 0
-			};
-			items.push(temp['times'])
-			items.push(temp['salaries'])
-			items.push(temp['hours'])
-			this.items = items
+			for (let day of Object.keys(hours)){
+				let val = Number(hours[day].value);
+				total.hours += isNaN(val) ? 0 : val;
+			}
+
+			return {salaries, hours, times, total}
 		},
 	}
 };
@@ -439,6 +410,7 @@ export default {
 		width: 100%;
 		margin: 4rem 0.1rem 0;
 		border-spacing: 0;
+		color: #62788B;
 
 		td:first-child, th:first-child {
 			text-align: left;
@@ -482,7 +454,6 @@ export default {
 			height: 4rem;
 			font-size: 1.2rem;
 			font-family: "Inter", sans-serif;
-			color: #62788B;
 			text-align: center;
 
 			&.yellow {
@@ -546,68 +517,6 @@ export default {
 		font-size: 0.9rem;
 	}
 
-	.fine, .avans, .bonus {
-		color: #fff;
-	}
-
-	.balance__table {
-		.day-fine {
-			background: $fine;
-			color: #fff;
-		}
-
-		.day-training {
-			background: $training;
-			color: #fff;
-
-			&.day-fine {
-				background: linear-gradient(110deg, $training 50%, $fine 50%);
-			}
-		}
-
-		.day-bonus {
-			background: $bonus;
-			color: #fff;
-
-			&.day-fine {
-				background: linear-gradient(110deg, $bonus 50%, $fine 50%);
-			}
-
-			&.day-training {
-				background: linear-gradient(110deg, $bonus 50%, $training 50%);
-			}
-
-			&.day-fine.day-training {
-				background: linear-gradient(110deg, $bonus 33%, transparent 33%), linear-gradient(110deg, $fine 66%, $training 66%);
-			}
-		}
-
-		.day-avans {
-			background: $avans;
-			color: #fff;
-
-			&.day-fine {
-				background: linear-gradient(110deg, $avans 50%, $fine 50%);
-			}
-
-			&.day-bonus {
-				background: linear-gradient(110deg, $avans 50%, $bonus 50%);
-			}
-
-			&.day-training {
-				background: linear-gradient(110deg, $avans 50%, $training 50%);
-			}
-
-			&.day-bonus.day-fine {
-				background: linear-gradient(110deg, $avans 33%, transparent 33%), linear-gradient(110deg, $bonus 66%, $fine 66%);
-			}
-
-			&.day-bonus.day-fine.day-training {
-				background: linear-gradient(110deg, $avans 25%, transparent 25%), linear-gradient(110deg, $bonus 50%, $fine 50%), linear-gradient(110deg, $fine 75%, $training 75%);
-			}
-		}
-	}
-
 	@media(max-width: 1200px) {
 		.balance__inner {
 			width: 90rem;
@@ -625,4 +534,6 @@ export default {
 			font-size: 2.7rem
 		}
 	}
+
+
 </style>
