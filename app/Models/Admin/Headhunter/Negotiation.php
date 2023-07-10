8e9439f74cc5ec97e5b33d2f74eafcba8e7ecb1e
galyym <ksgalym@mail.ru>
2023-07-10 07:51:07 +0500

    fix: check hh2 from

--- app/Models/Admin/Headhunter/Negotiation.php
+++ app/Models/Admin/Headhunter/Negotiation.php
@@ -5,7 +5,10 @@ namespace App\Models\Admin\Headhunter;
 use Illuminate\Database\Eloquent\Model;
 
 class Negotiation extends Model
-{   
+{
+    const FROM_TYPE_HH = 1;
+    const FROM_TYPE_HH2 = 2;
+
     protected $table = 'headhunter_negotiations';
 
     public $timestamps = true;
@@ -18,5 +21,6 @@ class Negotiation extends Model
         'phone',
         'name',
         'resume_id',
+        'from'
     ];
 }
