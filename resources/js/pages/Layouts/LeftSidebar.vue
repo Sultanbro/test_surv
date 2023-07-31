5a9a268f550e26cb7ac57a35a5469ef619b4b138
takodachi2501 <vrnehot@yandex.kz>
2023-07-31 10:59:30 +0600

    fix: структура - вакантная позиция

--- resources/js/pages/Layouts/LeftSidebar.vue
+++ resources/js/pages/Layouts/LeftSidebar.vue
@@ -275,7 +275,6 @@ export default {
 					icon: 'icon-nd-struct',
 					height: 0,
 					route: 'structure',
-					hide: !(this.isAdmin && this.isMainProject)
 				},
 				{
 					name: 'База знаний',
