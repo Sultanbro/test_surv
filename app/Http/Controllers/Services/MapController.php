4cf0b3a364fa92f516c8a14e809f788add6437b8
takodachi2501 <vrnehot@yandex.kz>
2023-08-09 18:19:52 +0600

    feature: карта - новые города на карте

--- app/Http/Controllers/Services/MapController.php
+++ app/Http/Controllers/Services/MapController.php
@@ -14,20 +14,20 @@ class MapController extends Controller
     {
         View::share('menu', 'maps');
         View::share('link', 'maps');
-        $allUsers = User::on()->select('working_city')->distinct('working_city')->get()->toArray();
-      
+        // $allUsers = User::on()->select('working_city')->distinct('working_city')->get()->toArray();
+
         $maps_array = [];
 
-        if (!empty($allUsers)) {
-            foreach ($allUsers as $key => $allUser) {
-                if($allUser['working_city'] == null) continue; 
-                $allUsersCount = User::on()->where('working_city',$allUser['working_city'])->count();
-                $geo_location = DB::table('coordinates')->find($allUser['working_city']);
-                $maps_array[$key]['count'] = $allUsersCount;
-                $maps_array[$key]['geo_lat'] = $geo_location->geo_lat;
-                $maps_array[$key]['geo_lon'] = $geo_location->geo_lon;
-            }
-        }
+        // if (!empty($allUsers)) {
+        //     foreach ($allUsers as $key => $allUser) {
+        //         if($allUser['working_city'] == null) continue;
+        //         $allUsersCount = User::on()->where('working_city',$allUser['working_city'])->count();
+        //         $geo_location = DB::table('coordinates')->find($allUser['working_city']);
+        //         $maps_array[$key]['count'] = $allUsersCount;
+        //         $maps_array[$key]['geo_lat'] = $geo_location->geo_lat;
+        //         $maps_array[$key]['geo_lon'] = $geo_location->geo_lon;
+        //     }
+        // }
 
         return view('surv.maps', compact('maps_array'));
     }
