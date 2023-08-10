d398070bd1e1540840358a3217577f1bb4de5647
takodachi2501 <117822533+takodachi2501@users.noreply.github.com>
2023-08-10 16:36:59 +0600

    Revert "fix: табель - убран лимит по часам"

--- resources/js/pages/TableReport.vue
+++ resources/js/pages/TableReport.vue
@@ -249,6 +249,7 @@
 			</div>
 		</div>
 
+
 		<Sidebar
 			v-if="showExcelImport"
 			title="Импорт EXCEL"
@@ -258,6 +259,7 @@
 		>
 			<GroupExcelImport :group_id="currentGroup" />
 		</Sidebar>
+
 
 		<aside
 			v-if="openSidebar"
@@ -291,8 +293,8 @@
 										class="mb-3"
 									>
 										<p class="fz12">
-											<b class="text-black">Дата:</b>
-											{{ (new Date(item.created_at)).addHours(-6).toLocaleString('ru-RU') }}
+											<b class="text-black">Дата:</b> {{ (new
+												Date(item.created_at)).addHours(-6).toLocaleString('ru-RU') }}
 										</p>
 										<p class="fz12">
 											<b class="text-black">Автор:</b> {{ item.author }} <br>
@@ -312,6 +314,8 @@
 
 						<template v-if="canEdit">
 							<b-tab title="📆 Статус">
+								<!-- <div v-html="sidebarContent.history"></div>
+					<div v-html="sidebarContent.historyTotal"></div> -->
 								<template v-if="!sidebarContent.data.item.is_trainee">
 									<div class="temari">
 										<div
@@ -510,6 +514,8 @@
 
 				<template v-if="canEdit">
 					<b-tab title="📆">
+						<!-- <div v-html="sidebarContent.history"></div>
+            <div v-html="sidebarContent.historyTotal"></div> -->
 						<template v-if="!sidebarContent.data.item.is_trainee">
 							<div class="temari">
 								<div
@@ -637,6 +643,7 @@
 			</b-tabs>
 		</Sidebar>
 
+
 		<b-modal
 			v-model="modalVisibleFines"
 			ok-text="Да"
@@ -709,6 +716,7 @@
 			/>
 		</b-modal>
 
+
 		<b-modal
 			v-model="modalVisibleAbsence"
 			ok-text="Да"
@@ -766,6 +774,7 @@
 				</b-alert>
 			</template>
 
+
 			<select
 				class="form-control"
 				v-model="commentFiring2"
@@ -795,6 +804,7 @@
 				class="mt-3"
 			/>
 		</b-modal>
+
 
 		<b-modal
 			v-model="modalVisible"
@@ -1026,16 +1036,16 @@ export default {
 				return '';
 			}
 
-			this.axios.post('/autochecker/' + this.currentGroup, {}).then(response => {
-				if (response.data.code == 200) {
-					this.$toast.success('Ссылка скопирована. Через 30 минут (в ' + response.data.time + ') не отмеченные стажеры перейдут в статус "Отсутствует"')
-				}
-				else {
-					this.$toast.error('Попробуйте нажать еще раз')
-				}
-			}).catch(error => {
-				alert(error)
-			});
+			this.axios.post('/autochecker/' + this.currentGroup, {})
+				.then(response => {
+					if (response.data.code == 200) {
+						this.$toast.success('Ссылка скопирована. Через 30 минут (в ' + response.data.time + ') не отмеченные стажеры перейдут в статус "Отсутствует"')
+					} else {
+						this.$toast.error('Попробуйте нажать еще раз')
+					}
+				}).catch(error => {
+					alert(error)
+				});
 		},
 
 		openModalDay(dayType) {
@@ -1078,8 +1088,7 @@ export default {
 
 			try {
 				this.$toast.info('C ' + this.currentEditingCell.item[this.currentEditingCell.field.key].hour + ' на ' + hour);
-			}
-			catch (e) {
+			} catch (e) {
 				alert(e);
 			}
 		},
@@ -1127,6 +1136,7 @@ export default {
 					'Content-Type': 'multipart/form-data'
 				}
 			}).then(response => {
+
 				let v = this.items[this.sidebarContent.data.index]['_cellVariants'];
 				[this.sidebarContent.day] = `day-${this.currentDayType.type}`
 
@@ -1134,9 +1144,11 @@ export default {
 
 				this.fetchData()
 
+
 				this.openSidebar = false
 
 				if (response.data.success == 1) {
+
 					this.sidebarHistory.push(response.data.history)
 					this.modalVisibleFiring = false
 					this.commentFiring = ''
@@ -1162,6 +1174,7 @@ export default {
 				comment: ' ',
 				year: this.dateInfo.currentYear,
 			}).then(response => {
+
 				let v = this.items[this.sidebarContent.data.index]['_cellVariants'];
 				[day] = `day-${this.currentDayType.type}`
 
@@ -1169,7 +1182,9 @@ export default {
 
 				this.fetchData()
 
+
 				this.openSidebar = false
+
 				if (response.data.success == 1) {
 					this.sidebarHistory.push(response.data.history)
 					this.currentDayType = {}
@@ -1189,12 +1204,14 @@ export default {
 					comment: this.commentDay,
 					year: this.dateInfo.currentYear,
 				}).then(response => {
+
 					let v = this.items[this.sidebarContent.data.index]['_cellVariants'];
 					[this.sidebarContent.day] = `day-${this.currentDayType.type}`
 
 					this.items[this.sidebarContent.data.index]['_cellVariants'] = v
 
 					this.fetchData()
+
 
 					this.openSidebar = false
 
@@ -1207,8 +1224,7 @@ export default {
 				}).catch(error => {
 					alert(error)
 				});
-			}
-			else {
+			} else {
 				this.errors = ['Комментарий обязателен']
 			}
 		},
@@ -1231,8 +1247,7 @@ export default {
 					loader.hide()
 					alert(error)
 				});
-			}
-			else {
+			} else {
 				this.errors = ['Комментарий обязателен']
 			}
 		},
@@ -1311,14 +1326,16 @@ export default {
 			if (url === null) {
 				if (this.url_page === '') {
 					url = '/timetracking/reports';
-				}
-				else {
+				} else {
 					url = this.url_page;
 				}
-			}
-			else {
+			} else {
+				// /timetracking/reports?page=2
 				this.url_page = url;
 			}
+
+			/*console.log('URL');
+				console.log(url);*/
 
 			let loader = this.$loading.show();
 
@@ -1344,6 +1361,7 @@ export default {
 				this.setFields()
 				this.loadItems()
 
+
 				this.dataLoaded = true
 				setTimeout(() => {
 					var container = document.querySelector('.table-responsive')
@@ -1362,6 +1380,7 @@ export default {
 
 		//Добавление загруженных данных в таблицу
 		loadItems() {
+
 			let items = []
 
 			let daily_totals = {};
@@ -1371,6 +1390,7 @@ export default {
 			}
 
 			this.data.users.forEach(item => {
+
 				let dayHours = []
 				let startEnd = []
 
@@ -1400,9 +1420,19 @@ export default {
 						}
 					}
 					else {
+
+
 						if (tt.minutes > 0) {
 							dayHours[tt.date].hour += Number(tt.minutes / 60);
 							tt_hours += Number(tt.minutes / 60);
+						}
+
+
+						var maxHour = item.working_time_id === 1 ? 8 : 9;
+
+						if (dayHours[tt.date].hour > maxHour && tt.updated === 0) {
+							dayHours[tt.date].hour = maxHour;
+							tt_hours = maxHour;
 						}
 					}
 
@@ -1412,6 +1442,7 @@ export default {
 						daily_totals[tt.date] += Number(tt_hours);
 					}
 				})
+
 
 				//Время, история
 				dayHours.forEach((dh, key) => {
@@ -1436,11 +1467,14 @@ export default {
 					if (item.dayTypes) v[k] = `day-${item.dayTypes[k]}`
 				});
 
+
 				Object.keys(item.fines).forEach(k => {
 					if (item.fines[k].status == 1) {
 						v[parseInt(item.fines[k].date)] += ' table-day-2'
 					}
+
 				});
+
 
 				Object.keys(item.weekdays).forEach(k => {
 					if (Number(item.weekdays[k]) == 1) {
@@ -1451,6 +1485,7 @@ export default {
 				var variants = {
 					_cellVariants: v
 				}
+
 
 				items.push({
 					name: `${item.name} ${item.last_name}`,
@@ -1467,6 +1502,7 @@ export default {
 					...variants,
 					...dayHours,
 				})
+
 			})
 			this.items = items
 
@@ -1478,10 +1514,10 @@ export default {
 		},
 
 		editDay(data) {
+
 			try {
 				this.$toast.info('Вы редактируете ' + this.currentEditingCell.field.key + ' число  у ' + this.currentEditingCell.item.name);
-			}
-			catch (err) {
+			} catch (err) {
 				console.error('editDay')
 			}
 
@@ -1522,8 +1558,7 @@ export default {
 					loader.hide()
 					alert(error)
 				});
-			}
-			else {
+			} else {
 				this.errors = ['Комментарий обязателен']
 			}
 		},
@@ -1587,6 +1622,7 @@ export default {
 
 				this.fetchData()
 
+
 				this.openSidebar = false
 
 				if (response.data.success == 1) {
@@ -1601,6 +1637,8 @@ export default {
 			}).catch(error => {
 				alert(error)
 			});
+
+
 		},
 
 		detectClick(data) {
@@ -1613,18 +1651,17 @@ export default {
 					setTimeout(function () {
 						if (self.numClicks === 1) {
 							self.openDay(data)
-						}
-						else {
+						} else {
 							self.editDay(data)
 						}
 						self.numClicks = 0;
 					}, 300);
 				}
 
-			}
-			else { // ANOTHER GGROUPS JUST OPEN SIDEBAR
+			} else { // ANOTHER GGROUPS JUST OPEN SIDEBAR
 				this.openDay(data);
 			}
+
 		},
 
 		sortCompare(aRow, bRow, key, sortDesc, formatter, compareOptions, compareLocale) {
@@ -1638,8 +1675,7 @@ export default {
 			) {
 				// If both compared fields are native numbers or both are native dates
 				return a < b ? -1 : a > b ? 1 : 0
-			}
-			else {
+			} else {
 				// Otherwise stringify the field data and use String.prototype.localeCompare
 				return (b || '').toString().localeCompare((a || '').toString(), compareLocale, compareOptions)
 			}
@@ -1649,335 +1685,337 @@ export default {
 </script>
 
 <style lang="scss">
-.table-report-sidebar{
-	position: fixed;
-	top: 0;
-	right: 6rem;
-	z-index: 100;
-	width: 100%;
-	height: 100%;
-	.table-report-backdrop{
-		position: absolute;
+
+
+	.table-report-sidebar{
+		position: fixed;
 		top: 0;
-		left: 0;
-		z-index: 10;
+		right: 6rem;
+		z-index: 100;
 		width: 100%;
 		height: 100%;
-		background-color: #333;
-		opacity: 0.5;
-	}
-	.table-report-content{
-		position: absolute;
-		top: 0;
-		right: 0;
-		width: 400px;
-		height: 100vh;
-		border-radius: 20px 0 0 20px;
-		z-index: 15;
-		background-color: #fff;
-		.table-report-header{
-			background: #ECF0F9;
-			padding: 3rem;
-			display: flex;
-			align-items: center;
-			.table-report-title{
-				font-size: 16px;
-				font-weight: 600;
-				line-height: 1;
-			}
-			.table-report-close{
-				width: 35px;
-				height: 35px;
-				cursor: pointer;
-				margin-right: 15px;
-			}
+		.table-report-backdrop{
+			position: absolute;
+			top: 0;
+			left: 0;
+			z-index: 10;
+			width: 100%;
+			height: 100%;
+			background-color: #333;
+			opacity: 0.5;
 		}
-		.table-report-body{
-			.nav-tabs{
-				.nav-item{
-					.nav-link{
-						color: #8D8D8D;
-						font-size: 1.7rem;
-						font-weight: 600;
-						transition: color 0.3s;
-						padding-top: 1.5rem;
-						cursor: pointer;
-						margin-right: 0;
-						border-bottom: none;
-						&.active{
-							border-top: 4px solid #ED2353;
-							color: #ED2353;
+		.table-report-content{
+			position: absolute;
+			top: 0;
+			right: 0;
+			width: 400px;
+			height: 100vh;
+			border-radius: 20px 0 0 20px;
+			z-index: 15;
+			background-color: #fff;
+			.table-report-header{
+				background: #ECF0F9;
+				padding: 3rem;
+				display: flex;
+				align-items: center;
+				.table-report-title{
+					font-size: 16px;
+					font-weight: 600;
+					line-height: 1;
+				}
+				.table-report-close{
+					width: 35px;
+					height: 35px;
+					cursor: pointer;
+					margin-right: 15px;
+				}
+			}
+			.table-report-body{
+				.nav-tabs{
+					.nav-item{
+						.nav-link{
+							color: #8D8D8D;
+							font-size: 1.7rem;
+							font-weight: 600;
+							transition: color 0.3s;
+							padding-top: 1.5rem;
+							cursor: pointer;
+							margin-right: 0;
+							border-bottom: none;
+							&.active{
+								border-top: 4px solid #ED2353;
+								color: #ED2353;
+							}
 						}
 					}
 				}
-			}
-			.tab-content{
-				padding: 0 20px;
+				.tab-content{
+					padding: 0 20px;
+				}
 			}
 		}
 	}
-}
-.hovered-text {
-	margin-top: 15px;
-	color: #62788B;
-}
-
-.table-custom-report {
-	th, td {
-		vertical-align: middle;
-
-		.td-div {
-			height: 40px;
-			min-width: 50px;
-			padding: 0 10px;
-			position: relative;
-			display: inline-flex;
-			align-items: center;
-			justify-content: center;
-		}
+	.hovered-text {
+		margin-top: 15px;
+		color: #62788B;
 	}
 
-	thead {
+	.table-custom-report {
 		th, td {
-			text-align: center;
-			padding: 10px !important;
 			vertical-align: middle;
 
-			&:first-child {
-				padding: 0 15px !important;
+			.td-div {
+				height: 40px;
+				min-width: 50px;
+				padding: 0 10px;
+				position: relative;
+				display: inline-flex;
+				align-items: center;
+				justify-content: center;
 			}
 		}
-	}
 
-	tbody {
-		th, td {
-			padding: 0 !important;
+		thead {
+			th, td {
+				text-align: center;
+				padding: 10px !important;
+				vertical-align: middle;
 
-			&:first-child {
-				padding: 0 15px !important;
+				&:first-child {
+					padding: 0 15px !important;
+				}
 			}
 		}
-	}
 
-	.td-lightgreen {
-		background-color: #B7E100;
-	}
+		tbody {
+			th, td {
+				padding: 0 !important;
 
-	.table-day-2 {
-		color: #333;
-		background-color: #f58c94;
+				&:first-child {
+					padding: 0 15px !important;
+				}
+			}
+		}
 
-		input {
+		.td-lightgreen {
+			background-color: #B7E100;
+		}
+
+		.table-day-2 {
 			color: #333;
+			background-color: #f58c94;
+
+			input {
+				color: #333;
+			}
+		}
+
+		.table-day-3 {
+			color: rgb(0, 0, 0);
+			background-color: aqua !important;
+		}
+
+		.table-day-4 {
+			color: rgb(0, 0, 0);
+			background-color: rgb(200, 162, 200) !important;
+		}
+
+		.table-day-5 {
+			color: rgb(0, 0, 0);
+			background-color: #ffd76d !important;
+		}
+
+		.table-day-6 {
+			color: #fff;
+			background-color: pink !important;
+		}
+
+		.table-day-7 {
+			color: #fff;
+			background-color: #ffc107 !important;
+		}
+
+		.cell-border {
+			position: absolute;
+			right: -1px;
+			bottom: -5px;
+			border-top: 6px solid transparent;
+			border-bottom: 6px solid transparent;
+			border-left: 6px solid #b8daff;
+			-webkit-transform: rotate(45deg);
+			transform: rotate(45deg);
 		}
 	}
 
-	.table-day-3 {
-		color: rgb(0, 0, 0);
-		background-color: aqua !important;
+
+	.editmode {
+		opacity: 0;
+		height: 36px;
 	}
 
-	.table-day-4 {
-		color: rgb(0, 0, 0);
-		background-color: rgb(200, 162, 200) !important;
+	.editmode:active {
+		opacity: 1;
 	}
 
-	.table-day-5 {
-		color: rgb(0, 0, 0);
-		background-color: #ffd76d !important;
+	.history {
+		height: 100vh;
+		overflow-y: auto;
+
+		p {
+			font-size: 14px;
+			color: #424242;
+		}
 	}
 
-	.table-day-6 {
+	.fines-modal {
+		overflow-y: auto;
+		max-height: calc(100vh - 225px);
+		.custom-checkbox{
+			margin-bottom: 10px;
+		}
+	}
+
+
+	.b-table-sticky-header {
+		max-height: calc(100vh - 250px);
+	}
+
+	.table-day-1 {
+		color: rgb(0, 0, 0);
+		background: #fef1cb !important;
+	}
+
+	.temari{
+		.btn {
+		}
+		.button-day{
+			&_0{
+				border: 1px solid #999;
+				color: #333;
+				background-color: #fff;
+				&:hover{
+					background-color: #d8d8d8;
+				}
+			}
+			&_1{
+				border: 1px solid #958d73;
+				background-color: #e5dab6;
+				color: #333;
+				&:hover{
+					background-color: #c7bd9e;
+				}
+			}
+			&_2{
+				.img-info{
+					filter: contrast(100);
+				}
+			}
+			&_3{
+				border: 1px solid #4489c9;
+				background-color: #4c9ee5;
+				color: #fff;
+				&:hover{
+					background-color: #4489c9;
+				}
+				.img-info{
+					filter: contrast(100);
+				}
+			}
+			&_5{
+				border: 1px solid #e6983f;
+				background-color: #faa544;
+				color: #fff;
+				&:hover{
+					background-color: #e6983f;
+				}
+				.img-info{
+					filter: contrast(100);
+				}
+			}
+			&_6{
+				border: 1px solid #98116c;
+				background-color: #bc1585;
+				color: #fff;
+				&:hover{
+					background-color: #98116c;
+				}
+				.img-info{
+					filter: contrast(100);
+				}
+			}
+			&_7{
+				border: 1px solid #bf2216;
+				background-color: #df271a;
+				color: #fff;
+				&:hover{
+					background-color: #bf2216;
+				}
+				.img-info{
+					filter: contrast(100);
+				}
+			}
+		}
+	}
+
+
+	.my-table .day.Sat.table-day-2, .my-table .day.Sun.table-day-2 {
 		color: #fff;
-		background-color: pink !important;
+		background-color: red;
 	}
 
-	.table-day-7 {
-		color: #fff;
-		background-color: #ffc107 !important;
+
+	.updated {
+		.cell-border {
+			border-left-color: red;
+		}
 	}
 
-	.cell-border {
-		position: absolute;
-		right: -1px;
-		bottom: -5px;
-		border-top: 6px solid transparent;
-		border-bottom: 6px solid transparent;
-		border-left: 6px solid #b8daff;
-		-webkit-transform: rotate(45deg);
-		transform: rotate(45deg);
+
+	.badgy {
+		font-size: 0.75em;
 	}
-}
 
+	.temari {
+		height: calc(100vh - 180px);
+		display: flex;
+		flex-direction: column;
+	}
 
-.editmode {
-	opacity: 0;
-	height: 36px;
-}
+	.ddf div {
+		display: flex;
+	}
 
-.editmode:active {
-	opacity: 1;
-}
+	.ddf .custom-control {
+		margin-right: 15px;
+	}
 
-.history {
-	height: 100vh;
-	overflow-y: auto;
+	.fz12 {
+		line-height: 1.4em;
+		font-size: 12px;
+		margin-bottom: 0;
+	}
 
-	p {
+	.fz14 {
 		font-size: 14px;
-		color: #424242;
+		line-height: 1.4em;
+		padding: 10px 0;
 	}
-}
 
-.fines-modal {
-	overflow-y: auto;
-	max-height: calc(100vh - 225px);
-	.custom-checkbox{
-		margin-bottom: 10px;
+	hr {
+		margin: 2px !important;
 	}
-}
 
-
-.b-table-sticky-header {
-	max-height: calc(100vh - 250px);
-}
-
-.table-day-1 {
-	color: rgb(0, 0, 0);
-	background: #fef1cb !important;
-}
-
-.temari{
-	.btn {
+	.hider {
+		position: absolute;
+		left: -10px;
+		width: 10px;
+		height: 10px;
+		opacity: 0;
+		display: block;
 	}
-	.button-day{
-		&_0{
-			border: 1px solid #999;
-			color: #333;
-			background-color: #fff;
-			&:hover{
-				background-color: #d8d8d8;
-			}
-		}
-		&_1{
-			border: 1px solid #958d73;
-			background-color: #e5dab6;
-			color: #333;
-			&:hover{
-				background-color: #c7bd9e;
-			}
-		}
-		&_2{
-			.img-info{
-				filter: contrast(100);
-			}
-		}
-		&_3{
-			border: 1px solid #4489c9;
-			background-color: #4c9ee5;
-			color: #fff;
-			&:hover{
-				background-color: #4489c9;
-			}
-			.img-info{
-				filter: contrast(100);
-			}
-		}
-		&_5{
-			border: 1px solid #e6983f;
-			background-color: #faa544;
-			color: #fff;
-			&:hover{
-				background-color: #e6983f;
-			}
-			.img-info{
-				filter: contrast(100);
-			}
-		}
-		&_6{
-			border: 1px solid #98116c;
-			background-color: #bc1585;
-			color: #fff;
-			&:hover{
-				background-color: #98116c;
-			}
-			.img-info{
-				filter: contrast(100);
-			}
-		}
-		&_7{
-			border: 1px solid #bf2216;
-			background-color: #df271a;
-			color: #fff;
-			&:hover{
-				background-color: #bf2216;
-			}
-			.img-info{
-				filter: contrast(100);
-			}
-		}
+
+	.ddpointer {
+		margin-top: 2px;
+		cursor: pointer;
 	}
-}
-
-
-.my-table .day.Sat.table-day-2, .my-table .day.Sun.table-day-2 {
-	color: #fff;
-	background-color: red;
-}
-
-
-.updated {
-	.cell-border {
-		border-left-color: red;
-	}
-}
-
-
-.badgy {
-	font-size: 0.75em;
-}
-
-.temari {
-	height: calc(100vh - 180px);
-	display: flex;
-	flex-direction: column;
-}
-
-.ddf div {
-	display: flex;
-}
-
-.ddf .custom-control {
-	margin-right: 15px;
-}
-
-.fz12 {
-	line-height: 1.4em;
-	font-size: 12px;
-	margin-bottom: 0;
-}
-
-.fz14 {
-	font-size: 14px;
-	line-height: 1.4em;
-	padding: 10px 0;
-}
-
-hr {
-	margin: 2px !important;
-}
-
-.hider {
-	position: absolute;
-	left: -10px;
-	width: 10px;
-	height: 10px;
-	opacity: 0;
-	display: block;
-}
-
-.ddpointer {
-	margin-top: 2px;
-	cursor: pointer;
-}
 </style>
