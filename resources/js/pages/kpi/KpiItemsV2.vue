4b9f1bc2eb51b2c420b81a05d013597124a8f211
takodachi2501 <vrnehot@yandex.kz>
2023-09-07 15:09:15 +0600

    Revert "fix: kpi - план"
    This reverts commit 0eb5f309517a8bc5f08258d9c6ec82a68f45216e.

--- resources/js/pages/kpi/KpiItemsV2.vue
+++ resources/js/pages/kpi/KpiItemsV2.vue
@@ -432,7 +432,6 @@ export default {
 				// ) {
 				//     el.plan = el.daily_plan * numberize(el.workdays);
 				// }
-				console.warn('recalc', JSON.parse(JSON.stringify(el)), calcCompleted(el));
 				el.percent = calcCompleted(el);
 				el.sum = calcSum(
 					el,
