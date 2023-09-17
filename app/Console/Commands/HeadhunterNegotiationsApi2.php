1a4f4795c76eb5a11d8757a23996314783dff189
JaloladdinRozmetov <jal99ol3bek@gmail.com>
2023-09-17 15:37:13 +0700

    has created dump to see which user and how many minutes in

--- app/Console/Commands/HeadhunterNegotiationsApi2.php
+++ app/Console/Commands/HeadhunterNegotiationsApi2.php
@@ -309,12 +309,12 @@ class HeadhunterNegotiationsApi2 extends Command
                     ->delete();
 
 
-                $neg->has_updated = $hh_neg->has_updates;
-                $neg->time = $time;
-                $neg->resume_id = $resume_id;
-                $neg->name = $name;
-                $neg->from = HeadHunterApi2::FROM_STATUS;
-                $neg->save();
+//                $neg->has_updated = $hh_neg->has_updates;
+//                $neg->time = $time;
+//                $neg->resume_id = $resume_id;
+//                $neg->name = $name;
+//                $neg->from = HeadHunterApi2::FROM_STATUS;
+//                $neg->save();
             } else {
                 Negotiation::create([
                     'vacancy_id' => $vacancy->vacancy_id,
