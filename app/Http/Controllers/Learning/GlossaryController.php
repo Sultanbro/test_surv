9e732590e467870968bcc1ae92d4b7c0cc3e04a1
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2023-10-12 10:46:10 +0500

    glossary permissions

--- app/Http/Controllers/Learning/GlossaryController.php
+++ app/Http/Controllers/Learning/GlossaryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Learning;
 
 use App\Http\Controllers\Controller;
+use App\Models\GlossaryModel;
 use Illuminate\Http\Request;
 use App\Models\GlossaryWord as Word;
 
@@ -15,19 +16,19 @@ class GlossaryController extends Controller
 
     /**
      * @param Request $request
-     * 
-     * @return Collection
+     *
+     * @return mixed
      */
     public function get(Request $request)
     {
         return Word::orderBy('word')->get();
-    }   
+    }
 
 
     /**
      * @param Request $request
-     * 
-     * @return Collection
+     *
+     * @return void
      */
     public function delete(Request $request)
     {
@@ -37,23 +38,45 @@ class GlossaryController extends Controller
 
     /**
      * @param Request $request
-     * 
+     *
      * @return int
      */
     public function save(Request $request)
-    {   
+    {
         $input = [
             'word' => $request->word['word'],
             'definition' => $request->word['definition'],
         ];
 
-        if($request->id == 0){
+        if ($request->id == 0) {
             $word = Word::create($input);
         } else {
             $word = Word::find($request->id);
             $word->update($input);
         }
-        
-        return $word  ? $word->id : 0;
+
+        return $word ? $word->id : 0;
+    }
+
+    public function updateAccess(Request $request)
+    {
+        GlossaryModel::query()->truncate();
+
+        foreach ($request['who_can_edit'] as $item) {
+            if ($item['type'] == 1) $model = 'App\\User';
+            if ($item['type'] == 2) $model = 'App\\ProfileGroup';
+            if ($item['type'] == 3) $model = 'App\\Position';
+
+            GlossaryModel::create([
+                'model_type' => $model,
+                'model_id' => $item['id'],
+                'access' => GlossaryModel::EDIT_ACCESS
+            ]);
+        }
+    }
+
+    private function saveGlossaryAccesses($items)
+    {
+
     }
 }
