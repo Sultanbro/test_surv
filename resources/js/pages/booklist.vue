c87bdbbc782ee0a75345cc5c47ea2e04a2f40f93
takodachi2501 <vrnehot@yandex.kz>
2023-10-24 15:20:18 +0600

    fix: бз - ошибка загрузки изображения

--- resources/js/pages/booklist.vue
+++ resources/js/pages/booklist.vue
@@ -253,127 +253,9 @@
 						v-model="activesbook.text"
 						api-key="mve9w0n1tjerlwenki27p4wjid4oqux1xp0yu0zmapbnaafd"
 						:init="{
-							images_upload_url: '/upload/images/',
-							automatic_uploads: true,
+							...editorConfig,
 							height: editorHeight,
-							setup: function (editor) {
-								editor.on('init change', function () {
-									editor.uploadImages();
-								});
-							},
 							images_upload_handler: submit_tinymce,
-							//paste_data_images: false,
-							resize: true,
-							autosave_ask_before_unload: true,
-							powerpaste_allow_local_images: true,
-							browser_spellcheck: true,
-							contextmenu: true,
-							spellchecker_whitelist: ['Ephox', 'Moxiecode'],
-							language: 'ru',
-							convert_urls: false,
-							relative_urls: false,
-							language_url: '/static/langs/ru.js',
-							content_css: '/static/css/mycontent.css',
-							fontsize_formats:
-								'8pt 10pt 12pt 13pt 14pt 15pt 16pt 17pt 18pt 20pt 22pt 24pt 26pt 28pt 30pt 36pt',
-							lineheight_formats:
-								'8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 20pt 22pt 24pt 26pt 36pt',
-							plugins: [
-								' advlist anchor autolink codesample colorpicker fullscreen help image imagetools ',
-								' lists link media noneditable  preview',
-								' searchreplace table template textcolor  visualblocks wordcount ',
-							],
-							menubar: false, //'file edit view insert format tools table help',
-							toolbar:
-								'styleselect  | bold italic underline strikethrough | table | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | fullscreen  preview |  media  link | undo redo',
-							toolbar_sticky: true,
-							content_style:
-								'.lineheight20px { line-height: 20px; }' +
-								'.lineheight22px { line-height: 22px; }' +
-								'.lineheight24px { line-height: 24px; }' +
-								'.lineheight26px { line-height: 26px; }' +
-								'.lineheight28px { line-height: 28px; }' +
-								'.lineheight30px { line-height: 30px; }' +
-								'.lineheight32px { line-height: 32px; }' +
-								'.lineheight34px { line-height: 34px; }' +
-								'.lineheight36px { line-height: 36px; }' +
-								'.lineheight38px { line-height: 38px; }' +
-								'.lineheight40px { line-height: 40px; }' +
-								'body { padding: 20px;max-width: 960px;margin: 0 auto; }' +
-								'.tablerow1 { background-color: #D3D3D3; }',
-							formats: {
-								lineheight20px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight20px',
-								},
-								lineheight22px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight22px',
-								},
-								lineheight24px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight24px',
-								},
-								lineheight26px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight26px',
-								},
-								lineheight28px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight20px',
-								},
-								lineheight30px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight30px',
-								},
-								lineheight32px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight32px',
-								},
-								lineheight34px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight34px',
-								},
-								lineheight36px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight36px',
-								},
-								lineheight38px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight38px',
-								},
-								lineheight40px: {
-									selector:
-										'span,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
-									classes: 'lineheight40px',
-								},
-							},
-							style_formats: [
-								{ title: 'lineheight20px', format: 'lineheight20px' },
-								{ title: 'lineheight22px', format: 'lineheight22px' },
-								{ title: 'lineheight24px', format: 'lineheight24px' },
-								{ title: 'lineheight26px', format: 'lineheight26px' },
-								{ title: 'lineheight28px', format: 'lineheight28px' },
-								{ title: 'lineheight30px', format: 'lineheight30px' },
-								{ title: 'lineheight32px', format: 'lineheight32px' },
-								{ title: 'lineheight34px', format: 'lineheight34px' },
-								{ title: 'lineheight36px', format: 'lineheight36px' },
-								{ title: 'lineheight38px', format: 'lineheight38px' },
-								{ title: 'lineheight40px', format: 'lineheight40px' },
-							],
-							content_css: [
-								'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-							],
 						}"
 						@onKeyUp="editorSave"
 						@onChange="editorSave"
@@ -550,6 +432,7 @@ import Editor from '@tinymce/tinymce-vue'
 import Questions from '@/pages/Questions'
 import ProgressBar from '@/components/ProgressBar'
 import Mark from 'mark.js/dist/mark.es6.js'
+import { editorConfig } from '@/components/pages/KB/helper.js'
 
 const quotes = ['«»', '“”', '""', '()']
 const enders = '.,!?:;'.split('')
@@ -667,6 +550,8 @@ export default {
 			attachment: null,
 			breadcrumbs: [],
 			highlight: '',
+			editorConfig,
+
 
 			// modals
 			showImageModal: false,
@@ -732,9 +617,25 @@ export default {
 	},
 
 	created() {
-		this.getTree();
-		this.parent_title = this.parent_name;
-		this.id = this.parent_id;
+		this.getTree()
+		this.parent_title = this.parent_name
+		this.id = this.parent_id
+
+		if(this.$debug){
+			this.editorConfig.toolbar = [
+				'styleselect',
+				'bold italic underline strikethrough',
+				'table',
+				'fontselect fontsizeselect formatselect',
+				'alignleft aligncenter alignright alignjustify',
+				'outdent indent',
+				'numlist bullist',
+				'forecolor backcolor removeformat',
+				'fullscreen preview',
+				'media link',
+				'undo redo code',
+			].join(' | ')
+		}
 	},
 
 	mounted() {
@@ -1175,19 +1076,22 @@ export default {
 			);
 		},
 
-		submit_tinymce(blobInfo, success) {
+		async submit_tinymce(blobInfo, success) {
+			const loader = this.$loading.show()
 
-			this.loader = true;
-			const formData = new FormData();
-			formData.append('attachment', blobInfo.blob());
-			formData.append('id', this.activesbook.id);
-			this.axios
-				.post('/upload/images/', formData)
-				.then((response) => {
-					success(response.data.location);
-					this.loader = false;
-				})
-				.catch((error) => console.error(error));
+			const formData = new FormData()
+			formData.append('attachment', blobInfo.blob())
+			formData.append('id', this.activesbook.id)
+			try {
+				const {data} = this.axios.post('/upload/images/', formData)
+				success(data.location)
+			}
+			catch (error) {
+				console.error(error)
+				window.onerror && window.onerror(error)
+				this.$toast.error('Ошибка загрузки изображения')
+			}
+			loader.hide()
 		},
 
 		submit() {
