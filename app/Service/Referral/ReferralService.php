8b35392c2a14f6889ad7d0dc691ad646461940a0
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-10-27 09:52:20 +0400

    refactoring referral

--- app/Service/Referral/ReferralService.php
+++ app/Service/Referral/ReferralService.php
@@ -2,30 +2,22 @@
 
 namespace App\Service\Referral;
 
-use App\Service\Referral\Core\{GeneratorInterface, ReferrerInterface, SalaryHandlerInterface};
+use App\Service\Referral\Core\GeneratorInterface;
 use App\User;
 
 class ReferralService
 {
     private GeneratorInterface $generator;
-    private SalaryHandlerInterface $salaryHandler;
 
     public function __construct(
-        GeneratorInterface     $generator,
-        SalaryHandlerInterface $salaryHandler,
+        GeneratorInterface $generator,
     )
     {
         $this->generator = $generator;
-        $this->salaryHandler = $salaryHandler;
     }
 
     public function url(User $user): Core\ReferralUrlDto
     {
         return $this->generator->generate($user);
-    }
-
-    public function handle(ReferrerInterface $referrer): void
-    {
-        $this->salaryHandler->handle($referrer);
     }
 }
