df8bfd7e25cfe1cb29880e53838ffbc00fed8cce
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-10-30 16:35:24 +0400

    lead segemnts command

--- app/Console/Commands/GetSegmentsFromBitrix.php
+++ app/Console/Commands/GetSegmentsFromBitrix.php
@@ -2,13 +2,13 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Api\BitrixOld as Bitrix;
 use App\Models\Bitrix\Lead;
+use Illuminate\Console\Command;
 
 class GetSegmentsFromBitrix extends Command
 {
-	/**
+    /**
      * The name and signature of the console command.
      *
      * @var string
@@ -16,41 +16,43 @@ class GetSegmentsFromBitrix extends Command
     protected $signature = 'bitrix:segments';
 
 
-    public function handle() {
+    public function handle()
+    {
         // 4147
-        $leads = Lead::whereMonth('created_at', '9')
-			->whereYear('created_at', '2022')
-			->where('segment', 99)
-			->get();
+        $leads = Lead::query()
+            ->whereMonth('created_at', '9')
+            ->whereYear('created_at', '2022')
+            ->where('segment', 99)
+            ->get();
 
         $count = $leads->count();
 
-		foreach ($leads as $key => $lead) {
-			
-			$lead->segment = $this->getSegmentAndSaveForLead($lead->id);
-			$lead->save();
+        foreach ($leads as $key => $lead) {
+            // TODO: check in prod
+            $lead->segment = $this->getSegmentAndSaveForLead($lead->lead_id);
+            $lead->save();
 
-            $this->line(($key + 1 ). ' from ' . $count);
-			usleep(1000000);
-		}
+            $this->line(($key + 1) . ' from ' . $count);
+            usleep(1000000);
+        }
 
 
     }
 
-	private function getSegmentAndSaveForLead($id)
+    private function getSegmentAndSaveForLead($id)
     {
 
-		$res =	(new Bitrix)->getLeads(0, '', 'ALL', 'ASC', '2010-01-01', '2050-01-01', "DATE_CREATE", $id, 'title');
-		
-		$segment = 999;
+        $res = (new Bitrix)->getLeads(0, '', 'ALL', 'ASC', '2010-01-01', '2050-01-01', "DATE_CREATE", $id, 'title');
 
-		if(array_key_exists('result',$res) && array_key_exists('UF_CRM_1498210379', $res['result'])) {
-			$segment = $res['result']['UF_CRM_1498210379'];
+        $segment = 999;
 
-			$segment = Lead::getSegmentAlt($segment);
-		}
+        if (array_key_exists('result', $res) && array_key_exists('UF_CRM_1498210379', $res['result'])) {
+            $segment = $res['result']['UF_CRM_1498210379'];
 
-		return $segment;
+            $segment = Lead::getSegmentAlt($segment);
+        }
 
-	}
+        return $segment;
+
+    }
 }
