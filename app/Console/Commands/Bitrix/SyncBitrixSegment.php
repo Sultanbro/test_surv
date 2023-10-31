0e06fab7fe3a88096dff2bcb3696284b7056c422
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2023-10-31 15:57:19 +0500

    segment command

--- app/Console/Commands/Bitrix/SyncBitrixSegment.php
+++ app/Console/Commands/Bitrix/SyncBitrixSegment.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Bitrix;
 use App\Api\Bitrix\LeadApi;
 use App\Api\BitrixOld;
 use App\Models\Bitrix\Lead;
+use App\Models\Bitrix\Segment;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -26,17 +27,28 @@ class SyncBitrixSegment extends Command
 
     public function handle(LeadApi $service): void
     {
-        $chunked = $this->leads()->chunk(50);
+        $chunked = $this->leads()->chunk(3);
         foreach ($chunked as $leads) {
             foreach ($leads as $lead) {
+
                 $leadFromBitrix = $service->get($lead->lead_id);
                 if (array_key_exists('result', $leadFromBitrix) && array_key_exists('UF_CRM_1498210379', $leadFromBitrix['result'])) {
                     $segment = $leadFromBitrix['result']['UF_CRM_1498210379'];
-                    // user ---> segment $leadFromBitrix->segment
-                    // lead ---> segment $leadFromBitrix->segment
+                    if (array_key_exists($segment, Segment::BITRIX_SEGMENTS)) {
+                        $name = Segment::BITRIX_SEGMENTS[$leadFromBitrix['result']['UF_CRM_1498210379']];
+                        $local_segment = Segment::query()->where('name', $name)->first();
+                        if ($local_segment) {
+                            $lead->update([
+                                'segment' => $local_segment->id
+                            ]);
+                        } else {
+                            $this->info("Segment: $name ; net v nashey baze!");
+                        }
+                    }
+                    $this->info("Segment id: $segment ; net v nashey spiske!");
                 }
+                sleep(1);
             }
-            sleep(2);
         }
     }
 
