872974846656cd413b27cfe4ce923b5f8659e716
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-11-09 09:10:45 +0400

    analystics

--- tests/Unit/Service/Analytics/AnalyticTest.php
+++ tests/Unit/Service/Analytics/AnalyticTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Service\Analytics;
 
+use App\DTO\Analytics\V2\GetAnalyticDto;
+use App\Facade\Analytics\Analytics;
 use PHPUnit\Framework\TestCase;
 
 class AnalyticTest extends TestCase
@@ -9,6 +11,9 @@ class AnalyticTest extends TestCase
 
     public function test_it_can_get_analytics()
     {
-        $service = app(Ana);
+        $dto = new  GetAnalyticDto();
+        /** @var Analytics $service */
+        $service = app(Analytics::class);
+        $service->analytics();
     }
 }
