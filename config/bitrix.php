d04e3f15aac89551cddfe0d34c26e996aa9b7206
JaloladdinRozmetov <jal99ol3bek@gmail.com>
2023-11-09 12:04:58 +0700

    change webhook in bitrix

--- config/bitrix.php
+++ config/bitrix.php
@@ -1,7 +1,9 @@
 <?php
 return [
-   'token' => 'rjyhyb5x6x62f2my',
-   'host'  => 'https://infinitys.bitrix24.kz/rest/1/',
+//   'token' => 'rjyhyb5x6x62f2my',
+   'token' => '2l3obr4gnld1dqys',
+//   'host'  => 'https://infinitys.bitrix24.kz/rest/1/',
+   'host'  => 'https://infinitys.bitrix24.kz/rest/158504/',
     'notification' => [
         'token' => '8gvj9j84ne251tod',
         'host' => 'https://infinitys.bitrix24.kz/rest/94132/'
