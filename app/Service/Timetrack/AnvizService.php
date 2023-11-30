98b22ce398d81a6444d7de54420bccbc5d273a1f
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-11-30 12:40:45 +0400

    fix

--- app/Service/Timetrack/AnvizService.php
+++ app/Service/Timetrack/AnvizService.php
@@ -2,12 +2,12 @@
 
 namespace App\Service\Timetrack;
 
-use App\Timetracking;
 use App\Models\Anviz\Time as AnvizTime;
+use App\Timetracking;
 use Carbon\Carbon;
 
 class AnvizService
-{   
+{
     public $date;
 
     public function __construct()
@@ -15,37 +15,37 @@ class AnvizService
 
     }
 
-    public function fetchMarkTimes($date = null) : void
-    {   
+    public function fetchMarkTimes($date = null): void
+    {
         $this->date = $date ?? date('Y-m-d');
 
         $timeToFetchPreviousDays = date('w', strtotime($date)) == 1
-                                && date("h:i") == '07:00';
+            && date("h:i") == '07:00';
 
         $dates = $timeToFetchPreviousDays
-               ? [date('Y-m-d'), date('Y-m-d', strtotime(' -1 day')), date('Y-m-d', strtotime(' -2 day'))]
-               : [$this->date];
+            ? [date('Y-m-d'), date('Y-m-d', strtotime(' -1 day')), date('Y-m-d', strtotime(' -2 day'))]
+            : [$this->date];
 
         foreach ($dates as $my_date) {
             $this->date = $my_date;
             $this->resolveOneDay();
-        } 
+        }
     }
 
-    private function resolveOneDay() : void
+    private function resolveOneDay(): void
     {
-        $anvizRecords        = $this->anvizRecords();
-        $usersIds            = $this->getUserIds($anvizRecords);
+        $anvizRecords = $this->anvizRecords();
+        $usersIds = $this->getUserIds($anvizRecords);
         $timetrackingRecords = $this->timetrackingRecords($usersIds);
 
-        foreach($usersIds as $user_id) {
-           
+        foreach ($usersIds as $user_id) {
+
             $user_records = $timetrackingRecords->where('user_id', $user_id);
 
             $last_anviz_date = $anvizRecords->where('Userid', $user_id)
-                                        ->sortByDesc('CheckTime')
-                                        ->first()
-                                        ->CheckTime;
+                ->sortByDesc('CheckTime')
+                ->first()
+                ->CheckTime;
 
 //            /**
 //             * If user already has Timetracking records for date
@@ -61,23 +61,23 @@ class AnvizService
             /**
              * If user not has Timetracking record for date
              */
-            if($user_records->isEmpty()) {
+            if ($user_records->isEmpty()) {
                 Timetracking::create([
-                    'enter'   => Carbon::parse($last_anviz_date)->subHours(6),
+                    'enter' => Carbon::parse($last_anviz_date)->subHours(6),
                     'user_id' => $user_id
                 ]);
             }
 
-        } 
+        }
     }
 
-    private function updateIfDiffMoreThan15Mins($anvizDate, Timetracking $timetracking) : void
+    private function updateIfDiffMoreThan15Mins($anvizDate, Timetracking $timetracking): void
     {
         $diffMore15Minutes = strtotime($anvizDate)
-                        - strtotime($timetracking->enter)
-                        - 900;
+            - strtotime($timetracking->enter)
+            - 900;
 
-        if($diffMore15Minutes > 0) {
+        if ($diffMore15Minutes > 0) {
             $timetracking
                 ->update([
                     'exit' => $anvizDate
@@ -88,25 +88,25 @@ class AnvizService
     private function anvizRecords()
     {
         return AnvizTime::orderBy('CheckTime', 'desc')
-                    ->whereDate('CheckTime', $this->date)
-                    ->get();
+            ->whereDate('CheckTime', $this->date)
+            ->get();
     }
 
-    private function getUserIds($records = null) : array
+    private function getUserIds($records = null): array
     {
         $users_array = [];
-        foreach($records->unique('Userid') as $record) {
+        foreach ($records->unique('Userid') as $record) {
             $users_array[] = $record->Userid;
         }
 
         return $users_array; // [1,2,3]
     }
-    
+
     private function timetrackingRecords(array $users)
     {
         return Timetracking::whereDate('enter', $this->date)
-                ->whereIn('user_id', $users)
-                ->get(); 
+            ->whereIn('user_id', $users)
+            ->get();
     }
 
 }
