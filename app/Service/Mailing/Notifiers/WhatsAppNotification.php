eae37c2e6158ee3a0f14928a52b298c94ca24d89
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-11-30 10:06:56 +0400

    fix wathsapp notification delay

--- app/Service/Mailing/Notifiers/WhatsAppNotification.php
+++ app/Service/Mailing/Notifiers/WhatsAppNotification.php
@@ -6,7 +6,6 @@ use App\Jobs\WhatsAppNotificationJob;
 use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Bus;
 
 class WhatsAppNotification implements Notification
 {
@@ -27,11 +26,9 @@ class WhatsAppNotification implements Notification
             return false;
         } else {
             $recipients = $recipients->where('phone', '!=', '');
-
             foreach ($recipients as $recipient) {
-                WhatsAppNotificationJob::dispatch($recipient, $message)->delay(1);
+                WhatsAppNotificationJob::dispatch($recipient, $message)->delay(now()->addSeconds(2));
             }
-
             return true;
         }
     }
