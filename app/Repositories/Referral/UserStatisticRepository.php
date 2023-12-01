edcca1f992b5acf74ffce23d29679ac2b0b03ce3
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-12-01 11:16:09 +0400

    refacoring

--- app/Repositories/Referral/UserStatisticRepository.php
+++ app/Repositories/Referral/UserStatisticRepository.php
@@ -146,6 +146,7 @@ class UserStatisticRepository implements UserStatisticRepositoryInterface
                 $query->orderBy('date');
             }])
             ->with(['groups' => function (BelongsToMany $query) use ($referrer) {
+                $query->where('active', 1);
                 $query->select(["id", 'name']);
             }])
             ->with(['user_description' => fn($query) => $query->select(['id', 'user_id', 'is_trainee'])])
