cf1baede348b884fb0ee58c67c7c7364cc08b5e5
takodachi2501 <vrnehot@yandex.kz>
2023-12-07 15:03:40 +0600

    fix: char - crown

---
+++ resources/js/components/ui/Icons/IconCrown.vue
@@ -0,0 +1,34 @@
+<template>
+	<svg
+		fill="none"
+		width="64"
+		height="64"
+		viewBox="0 0 128 128"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g>
+			<!-- fill="#6986B8" -->
+			<path
+				d="m 64.000012,31.484944 28.902346,36.127871 28.902242,-36.127871 -7.22553,65.030147 -101.158086,0 L 6.1954237,31.484944 35.0977,67.612815 64.000012,31.484944 z"
+				fill="#ffcd00"
+				stroke="#000000"
+				class="ChatIcon-shape"
+			/>
+			<circle
+				cx="19.5"
+				cy="19.5"
+				r="7"
+				fill="#fff"
+			/>
+		</g>
+	</svg>
+</template>
+<script>
+export default {
+	name: 'IconCog',
+	components: {},
+}
+</script>
+
+<style>
+</style>
