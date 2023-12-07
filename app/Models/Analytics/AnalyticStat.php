e5c786f140c9aed973603c5dc5d5ee5631a4aa95
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-12-07 13:51:43 +0400

    fix analystics 31 day

--- app/Models/Analytics/AnalyticStat.php
+++ app/Models/Analytics/AnalyticStat.php
@@ -443,10 +443,10 @@ class AnalyticStat extends Model
     public static function getLetter($number): string
     {
         $letters = range('A', 'Z');
-        $length = count($letters);
+        $length = count($letters) - 1;
 
         $fl_pos = $number % $length;
-        $sl_pos = floor($number / $length);
+        $sl_pos = floor($number / $length) - 1;
 
         if ($number < $length) {
             return $letters[$fl_pos];
