22099c4a4b39f5e476f3f2943f0b3b8fc1cedc85
takodachi2501 <vrnehot@yandex.kz>
2023-12-12 10:47:58 +0600

    Revert "comments in english"
    This reverts commit e338f76857407ec1e22733db846c1f0a66f9da26.

--- storage/framework/views/69c2ec5ab938f1e45e7936f5a1988c3c43445263.php
+++
@@ -1,9 +0,0 @@
-Уважаемый(я) <?php echo e($name); ?>,<br>
-Приглашаю вас на портал.<br><br>
-
-<br>
-Ваши данные для входа:<br>
-<b>Логин:</b>&nbsp;<?php echo e($email); ?><br>
-<b>Пароль:</b>&nbsp;<?php if($original_password): ?> <?php echo e($original_password); ?><?php endif; ?> <br>
-
-Используйте выданный Вам пароль к Вашему кабинету по ссылке <a target="_blank" href="https://admin.u-marketing.org/">admin.u-marketing.org</a>.<br>
