1c128c46976f5cc62c9355116153ce01ab0e7883
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-12-27 15:54:14 +0400

    fix salary work time graph

--- app/User.php
+++ app/User.php
@@ -1826,7 +1826,7 @@ class User extends Authenticatable implements Authorizable, ReferrerInterface
         $days = explode('-', $workChart->name);
         $workingDay = array_key_exists(0, $days) ? (int)$days[0] : throw new Exception(message: 'Проверьте график работы', code: 400);
         $dayOff = array_key_exists(1, $days) ? (int)$days[1] : throw new Exception(message: 'Проверьте график работы', code: 400);
-
+        dd($workingDay);
         $date1 = date_create($end->format('Y-m-d'));
         $date2 = date_create($start);
         $differBetweenFirstAndLastDay = date_diff($date1, $date2)->days;
