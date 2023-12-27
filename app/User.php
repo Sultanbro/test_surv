466529ffa1db223b21b5903e55a8485144babc9e
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-12-27 15:58:39 +0400

    fix salary work time graph

--- app/User.php
+++ app/User.php
@@ -1820,7 +1820,7 @@ class User extends Authenticatable implements Authorizable, ReferrerInterface
     private function dayInWorkDaysDiapason(WorkChartModel $workChart, Carbon $date): bool
     {
         $start = $this->first_work_day ?? $this->timetracking()->first()?->exit;
-        dump($start);
+        dump($this->first_work_day);
         $end = $date->endOfMonth();
         if (!$start) return 0;
 
