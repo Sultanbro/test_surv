c2709f113e9bdae8a4fac43e7b9679adf9ea2d3c
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-12-28 12:55:05 +0400

    fix dates

--- app/Salary.php
+++ app/Salary.php
@@ -1189,7 +1189,7 @@ class Salary extends Model
                 $total_bonuses = (float)$awards;
                 $total_salary = 0;
 
-                if ($user->id == 27763) {
+                if ($user->id == 27766) {
                     dump($earnings);
                 }
 
