25839df6331cdb0828a5503fe6e7a4eeff0d3f55
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2023-12-29 11:56:52 +0500

    check kpi

--- app/Console/Commands/SaveUserKpi.php
+++ app/Console/Commands/SaveUserKpi.php
@@ -143,7 +143,7 @@ class SaveUserKpi extends Command
 
         foreach ($kpis as $key => $kpi) {
             if (!isset($kpi['users'][0])) continue;
-            dump($kpi['targetable_type'] . $kpi['targetable_id'], $kpi['children']);
+            dump($kpi['id'] . " " . $kpi['targetable_type'] . $kpi['targetable_id'], $kpi['children']);
 //            dd($kpi['users'][0]['items']);
 //             dd($kpi['users'][0]['items'][2]);
             foreach ($kpi['users'][0]['items'] as $item) {
