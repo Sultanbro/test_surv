bc39e6232f7dd0c83398170a6a67bb05f7e3a199
takodachi2501 <vrnehot@yandex.kz>
2024-01-05 14:44:24 +0600

    fix: useredit - iin -> uin

--- resources/js/components/pages/UserEdit/UserEditMain.vue
+++ resources/js/components/pages/UserEdit/UserEditMain.vue
@@ -95,6 +95,7 @@ export default {
 			userTimezone: null,
 			first_work_day: '',
 			timezones: [-11, -10, -9.5, -9, -8, -7, -6, -5, -4, -3.5, -3, -2, -1, 0, 1, 2, 3, 3.5, 4, 4.5, 5, 5.5, 5.75, 6, 6.5, 7, 8, 8.75, 9, 9.5, 10, 10.5, 11, 12, 12.75, 13, 14],
+			uin: this.user?.uin,
 		}
 	},
 	computed:{
@@ -124,6 +125,7 @@ export default {
 			this.position = user ? user.position_id : '';
 			this.workChartId = user ? user.work_chart_id : null;
 			this.userTimezone = user ? user.timezone : 6;
+			this.uin = user?.uin || ''
 			this.first_work_day = user?.first_work_day?.substring(0, 10) || ''
 		},
 		position(value){
