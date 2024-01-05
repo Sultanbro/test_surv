1bff553502647502f80767a492fb232f894b3c1c
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-01-05 10:19:24 +0500

    get deleted users only in current month

--- app/Service/KpiStatisticService.php
+++ app/Service/KpiStatisticService.php
@@ -1244,7 +1244,9 @@ class KpiStatisticService
         if ($type == 3) {
             $_user_ids = User::withTrashed()
                 ->where('position_id', $kpi->targetable_id)
-                ->whereDate('deleted_at', '>=', $date->startOfMonth()->format('Y-m-d'))
+                ->where(function ($q) use ($date) {
+                    $q->whereNull('deleted_at')->orWhereMonth('deleted_at', '>=', $date->month);
+                })
                 ->pluck('id')
                 ->toArray();
             if ($user_id != 0) $_user_ids = [$user_id];
