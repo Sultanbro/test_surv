d5734e5350deaa6cbdb261fb29125696165e3913
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2024-01-08 11:04:36 +0400

    fix stats

--- app/Service/KpiStatisticService.php
+++ app/Service/KpiStatisticService.php
@@ -1989,7 +1989,6 @@ class KpiStatisticService
             ->where('value', '>', 0)
             ->whereIn('activity_id', $activities)
             ->groupBy('user_id', 'activity_id');
-        dd($sum_and_counts->get());
         // query
         $users = User::withTrashed()
             ->select([
