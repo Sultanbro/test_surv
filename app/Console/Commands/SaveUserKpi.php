f5eec31d25865d97d2b1fc9a8e5371666a5baa8b
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-01-09 16:49:50 +0500

    check

--- app/Console/Commands/SaveUserKpi.php
+++ app/Console/Commands/SaveUserKpi.php
@@ -166,7 +166,7 @@ class SaveUserKpi extends Command
                 $off_limit = array_key_exists('off_limit', $payload) ? $payload['off_limit'] : false;
 
 //                dump("avg=" . $item['avg'] . " fact=" . $item['fact'] . " method=" . $item['method']);
-//                dump($kpi['histories_latest']['id'] . "payload=" . json_encode($payload) . " off_limit=" . $off_limit);
+                dump($kpi['histories_latest']['id'] . "payload=" . json_encode($payload) . " off_limit=" . $off_limit);
 //                dump($off_limit);
 
                 // off_limit -> check employee can get more kpi bonus with this kpi
@@ -182,7 +182,7 @@ class SaveUserKpi extends Command
                     (float)$item['full_time'] == 1 ? $kpi['completed_80'] : $kpi['completed_80'] / 2,
                     (float)$item['full_time'] == 1 ? $kpi['completed_100'] : $kpi['completed_100'] / 2,
                 );
-                dump('id=' . $item['id'] . ' ' . $item['name'] . ' fact=' . $item['fact'] . ' percent=' . $completed_percent . ' earned=' . $earnedActivity);
+                dump('id=' . $item['id'] . ' ' . $item['name'] . ' fact=' . $item['fact'] . ' percent=' . $completed_percent . ' share=' . $item['share'] . 'earned=' . $earnedActivity);
 
                 $earned += $earnedActivity;
 
