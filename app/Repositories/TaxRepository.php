40a0b12ed78faa2ac9c8b065e8d3be11d994edc8
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-01-10 15:35:05 +0500

    fix taxes is_percent

--- app/Repositories/TaxRepository.php
+++ app/Repositories/TaxRepository.php
@@ -79,9 +79,9 @@ class TaxRepository extends CoreRepository
     ): array
     {
         return Tax::query()
-            ->select('taxes.id', 'taxes.name',
+            ->select('taxes.id', 'taxes.name', 'user_tax.is_percent', 'taxes.end_subtraction',
                 DB::raw('CASE WHEN user_tax.value > 0 THEN user_tax.value ELSE taxes.value END AS value'),
-                'taxes.is_percent', 'taxes.end_subtraction', DB::raw('(user_tax.user_id IS NOT NULL) as isAssigned'))
+                DB::raw('(user_tax.user_id IS NOT NULL) as isAssigned'))
             ->leftJoin('user_tax', function ($join) use ($userId) {
                 $join->on('user_tax.tax_id', '=', 'taxes.id')
                     ->where('user_tax.user_id', '=', $userId);
