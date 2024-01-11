ccf2bc24c70dc6fd0559574f3626b347cdad6958
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-01-11 15:42:13 +0500

    check

--- app/Service/KpiStatisticService.php
+++ app/Service/KpiStatisticService.php
@@ -864,9 +864,9 @@ class KpiStatisticService
                     ELSE 4
                 END
             ")
-            ->limit(1)
+            ->limit(3)
             ->get();
-
+dd($kpis);
         $read = $kpis->contains(fn($k) => in_array($user_id, $k->read_by ?? []));
         $currency_rate = (float)(Currency::rates()[$currency] ?? 0.00001);
 
