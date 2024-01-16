6221ec7b385165dc398668a128b4bb0348b9b1fc
Vahagn <vahagn99ghukasyan@gmail.com>
2024-01-16 16:06:56 +0400

    fix nps

--- app/Http/Controllers/Analytics/NpsController.php
+++ app/Http/Controllers/Analytics/NpsController.php
@@ -50,11 +50,12 @@ class NpsController extends Controller
                 DB::raw('group_name'),
                 DB::raw('group_id'),
                 DB::raw('is_head'),
+                DB::raw('positions.position as position_name'),
             ])
             ->leftJoin('user_descriptions as ud', 'ud.user_id', '=', 'users.id')
+            ->join('positions', 'users.position_id', '=', 'positions.id')
             ->leftJoinSub($groupSubQuery, 'groups', 'groups.user_id', '=', 'users.id')
             ->whereIn('position_id', [45, 55])
-            ->with('position')
             ->where('is_trainee', 0)
             ->orderBy('group_id', 'desc')
             ->get();
@@ -63,7 +64,7 @@ class NpsController extends Controller
         foreach ($_users as $user) {
             $group = $user->group_name ?? '.Без группы';
 //            $position = $user->position_id == 45 ? 'Руковод' : 'Cт. спец';
-            $position = $user->position->position;
+            $position = $user->position_name;
 
             $arr = [
                 'id' => $user->id,
