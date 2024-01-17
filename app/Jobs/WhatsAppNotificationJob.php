e9002dddaf0aea1f6698f32fc8a7e0ffed100946
Vahagn <vahagn99ghukasyan@gmail.com>
2024-01-17 17:02:01 +0400

    fix notifications jobs

--- app/Jobs/WhatsAppNotificationJob.php
+++ app/Jobs/WhatsAppNotificationJob.php
@@ -4,19 +4,21 @@ namespace App\Jobs;
 
 use App\Classes\Helpers\Phone;
 use App\User;
+use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Http\Client\HttpClientException;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
 use stdClass;
 
 class WhatsAppNotificationJob implements ShouldQueue, ShouldBeUnique
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, Batchable;
 
     /**
      * Create a new job instance.
@@ -70,25 +72,33 @@ class WhatsAppNotificationJob implements ShouldQueue, ShouldBeUnique
         string $message
     ): void
     {
-        $channelId  = config('wazzup')['channel_id'];
-        $token  = config('wazzup')['token'];
+        $channelId = config('wazzup')['channel_id'];
+        $token = config('wazzup')['token'];
 
         $response = Http::withHeaders([
-            "Content-Type"  => "application/json",
+            "Content-Type" => "application/json",
             "Authorization" => "Bearer $token"
         ])
             ->timeout(100)
             ->post("https://api.wazzup24.com/v3/message", [
                 'channelId' => $channelId,
-                'chatId'    => $phone,
-                'text'      => $message,
-                'chatType'  => 'whatsapp',
+                'chatId' => $phone,
+                'text' => $message,
+                'chatType' => 'whatsapp',
             ]);
 
-        if (!$response->successful())
-        {
+        if (!$response->successful()) {
             throw new HttpClientException($response->body());
         }
     }
 
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array<int, object>
+     */
+    public function middleware(): array
+    {
+        return [new WithoutOverlapping($this->phone)];
+    }
 }
