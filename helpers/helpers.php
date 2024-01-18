53fa2193fd9bd660c8be18a546125c25cd1b789b
Vahagn <vahagn99ghukasyan@gmail.com>
2024-01-18 08:53:34 +0400

    fix referral

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -38,7 +38,7 @@ if (!function_exists('translit')) {
         } while ($st != $prev_st);
         $str = preg_replace("/_{2,}/", " ", $st);
         $arr = explode(' ', $str);
-        $str = \Illuminate\Support\Arr::map($arr, fn(string $item) => Str::ucfirst($item));
+        $str = \Illuminate\Support\Arr::map($arr, fn(string $item) => \Illuminate\Support\Str::ucfirst($item));
         return \Illuminate\Support\Arr::join($str, ' ');
     }
 }
