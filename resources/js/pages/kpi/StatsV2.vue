79c472414aca4730e804e28b615e4bb310f70255
takodachi2501 <vrnehot@yandex.kz>
2024-01-19 15:45:10 +0600

    fix: kpi stat - filters premium

--- resources/js/pages/kpi/StatsV2.vue
+++ resources/js/pages/kpi/StatsV2.vue
@@ -338,7 +338,12 @@ export default {
 				});
 			}
 			else if(this.s_type_main == 3){
-				this.axios.get('/statistics/quartal-premiums').then(response => {
+				this.axios.post('/statistics/quartal-premiums', {
+					filters: {
+						...filters,
+						query: this.searchText,
+					}
+				}).then(response => {
 					this.quartal_users = response.data[0].map(res=> ({...res, expanded: false}));
 					this.quartal_groups = response.data[1].map(res=> ({...res, expanded: false}));
 					loader.hide();
