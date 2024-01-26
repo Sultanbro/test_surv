7e150021452e5d8a0e25c515093a4032ad69366a
Vahagn <vahagn99ghukasyan@gmail.com>
2024-01-26 10:47:17 +0400

    delete top values

--- app/Http/Controllers/Analytics/TopController.php
+++ app/Http/Controllers/Analytics/TopController.php
@@ -435,7 +435,7 @@ class TopController extends Controller
 
     public function deleteGauge(Request $request): Response
     {
-        dd($request->get('gauge.id'));
+        dd($request->all());
         TopValue::query()
             ->find($request->get('gauge.id'));
 
