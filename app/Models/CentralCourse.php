c1251f0275606dbdd3e17a0bec025953cc4df275
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-01-29 17:19:07 +0500

    deletes

--- app/Models/CentralCourse.php
+++ app/Models/CentralCourse.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CentralCourse extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $connection = 'mysql';
     protected $table = 'central_courses';
