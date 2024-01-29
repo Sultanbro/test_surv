4f0d3833d55635f9e21599a7f7d8c1f21ffcd30d
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-01-29 14:08:28 +0500

    central course category

--- app/Http/Controllers/CentralCourseController.php
+++ app/Http/Controllers/CentralCourseController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\CentralCourse;
+use App\Service\CourseV2\CentralCourseService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CentralCourseController extends Controller
@@ -10,76 +12,15 @@ class CentralCourseController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param CentralCourseService $service
+     * @return JsonResponse
      */
-    public function getAll()
+    public function getAll(Request $request, CentralCourseService $service): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\CentralCourse  $centralCourse
-     * @return \Illuminate\Http\Response
-     */
-    public function show(CentralCourse $centralCourse)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\CentralCourse  $centralCourse
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(CentralCourse $centralCourse)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\CentralCourse  $centralCourse
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, CentralCourse $centralCourse)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\CentralCourse  $centralCourse
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(CentralCourse $centralCourse)
-    {
-        //
+        return $this->response(
+            message: self::SUCCESS_MESSAGE,
+            data: $service->getAll()
+        );
     }
 }
