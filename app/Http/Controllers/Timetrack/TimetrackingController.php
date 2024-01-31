595ced25ac4631b1f6189e738a85e65cad79cc99
Vahagn <vahagn99ghukasyan@gmail.com>
2024-01-31 15:26:15 +0400

    fix set day

--- app/Http/Controllers/Timetrack/TimetrackingController.php
+++ app/Http/Controllers/Timetrack/TimetrackingController.php
@@ -918,6 +918,7 @@ class TimetrackingController extends Controller
         /** @var User $user */
         $user = User::with(['workChart', 'groups.workChart'])->find($userId);
         $schedule = $user->scheduleFast();
+        dd($schedule);
         if ($schedule['start']) {
             $enter = $schedule['start']->setDate(intval($request->year), intval($request->month), intval($request->day))->subHours(6);
         } else {
