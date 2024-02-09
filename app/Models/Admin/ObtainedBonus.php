16fdddb98d09962d204dbd5af90857688a2bf820
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-02-09 10:32:35 +0500

    fix obtained bonus

--- app/Models/Admin/ObtainedBonus.php
+++ app/Models/Admin/ObtainedBonus.php
@@ -35,9 +35,9 @@ class ObtainedBonus extends Model
     /**
      * create or update bonus on day or month
      */
-    public static function createOrUpdate($data, $daypart = 0) : void
+    public static function createOrUpdate($data, $daypart = 0): void
     {
-        if($daypart == 2) {
+        if ($daypart == 2) {
             self::createOrUpdateForMonth($data);
         } else {
             self::createOrUpdateForDay($data);
@@ -47,86 +47,93 @@ class ObtainedBonus extends Model
     /**
      * create or update bonus on month
      */
-    public static function createOrUpdateForMonth($arr) : void
+    public static function createOrUpdateForMonth($arr): void
     {
         $date = Carbon::parse($arr['date'])->startOfMonth();
 
-        self::where('user_id', $arr['user_id'])
-                ->whereMonth('date', $date->month)
-                ->whereYear('date', $date->year)
-                ->where('bonus_id', $arr['bonus_id'])
-                ->delete();
+        self::query()
+            ->where('user_id', $arr['user_id'])
+            ->whereMonth('date', $date->month)
+            ->whereYear('date', $date->year)
+            ->where('bonus_id', $arr['bonus_id'])
+            ->delete();
 
-        self::create([
-            'user_id'  => $arr['user_id'],
-            'date'     => $date->format('Y-m-d'),
+        self::query()->create([
+            'user_id' => $arr['user_id'],
+            'date' => $date->format('Y-m-d'),
             'bonus_id' => $arr['bonus_id'],
-            'amount'   => $arr['amount'],
-            'comment'  => $arr['comment'],
+            'amount' => $arr['amount'],
+            'comment' => $arr['comment'],
         ]);
     }
 
     /**
      * create or update bonus on month
      */
-    public static function createOrUpdateForDay($arr) : void
+    public static function createOrUpdateForDay($arr): void
     {
-        $ob = self::where('user_id', $arr['user_id'])
-                    ->where('date', $arr['date'])
-                    ->where('bonus_id', $arr['bonus_id'])
-                    ->first();
-        if($ob) {
+        $ob = self::query()
+            ->where('user_id', $arr['user_id'])
+            ->where('date', $arr['date'])
+            ->where('bonus_id', $arr['bonus_id'])
+            ->first();
+        if ($ob) {
             $ob->amount = $arr['amount'];
             $ob->comment = $arr['comment'];
             $ob->save();
         } else {
-            self::create([
-                'user_id'  => $arr['user_id'],
-                'date'     => $arr['date'],
+            self::query()->create([
+                'user_id' => $arr['user_id'],
+                'date' => $arr['date'],
                 'bonus_id' => $arr['bonus_id'],
-                'amount'   => $arr['amount'],
-                'comment'  => $arr['comment'],
+                'amount' => $arr['amount'],
+                'comment' => $arr['comment'],
             ]);
         }
     }
 
-    public static function onDay($user_id, $date) {
-        return self::where('user_id', $user_id)
-                    ->where('date', $date)
-                    ->get()
-                    ->sum('amount');
+    public static function onDay($user_id, $date)
+    {
+        return self::query()
+            ->where('user_id', $user_id)
+            ->where('date', $date)
+            ->get()
+            ->sum('amount');
     }
 
-    public static function onMonth($user_id, $date) {
-        return self::where('user_id', $user_id)
-                    ->whereYear('date', Carbon::parse($date)->year)
-                    ->whereMonth('date', Carbon::parse($date)->month)
-                    ->get()
-                    ->sum('amount');
+    public static function onMonth($user_id, $date)
+    {
+        return self::query()
+            ->where('user_id', $user_id)
+            ->whereYear('date', Carbon::parse($date)->year)
+            ->whereMonth('date', Carbon::parse($date)->month)
+            ->get()
+            ->sum('amount');
     }
 
-    public static function getHistory($user_id, $date, $currency_rate = 1) {
+    public static function getHistory($user_id, $date, $currency_rate = 1)
+    {
         $bonusHistory = [];
 
         $month = Carbon::parse($date);
 
-        $bonuses_all = self::where('user_id', $user_id)
+        $bonuses_all = self::query()
+            ->where('user_id', $user_id)
             ->whereYear('date', $month->year)
             ->whereMonth('date', $month->month)
-                ->get();
+            ->get();
 
-        $test_bonuses_all = 0;
-//            TestBonus::where('user_id', $user_id)
-//            ->whereYear('date', $month->year)
-//            ->whereMonth('date', $month->month)
-//                ->get();
+        $test_bonuses_all = TestBonus::query()
+            ->where('user_id', $user_id)
+            ->whereYear('date', $month->year)
+            ->whereMonth('date', $month->month)
+            ->get();
 
         $manual_bonus_all = Salary::where('user_id', $user_id)
             ->selectRaw('date, bonus, comment_bonus, day(date) as day')
             ->whereYear('date', $month->year)
             ->whereMonth('date', $month->month)
             ->get();
-
 
 
         for ($i = $month->daysInMonth; $i > 0; $i--) {
@@ -141,17 +148,17 @@ class ObtainedBonus extends Model
             $comment = '';
 
 
-            if($manual_bonus) {
+            if ($manual_bonus) {
                 $item['date'] = $manual_bonus->date->format('Y-m-d') ?? '';
                 $sum += $manual_bonus->bonus * $currency_rate;
                 $comment .= $manual_bonus->comment_bonus;
             }
 
-            if($bonuses->count() > 0) {
-                foreach ($bonuses as $key => $bon) {
+            if ($bonuses->count() > 0) {
+                foreach ($bonuses as $bon) {
                     $item['date'] = $bon->date;
                     $sum += $bon->amount * $currency_rate;
-                    if(strlen($comment) > 0) {
+                    if (strlen($comment) > 0) {
                         $comment .= $bon->comment ? '<br>' . $bon->comment : '';
                     } else {
                         $comment = $bon->comment ? $bon->comment : '';
@@ -159,11 +166,11 @@ class ObtainedBonus extends Model
                 }
             }
 
-            if($test_bonuses->count() > 0) {
+            if ($test_bonuses->count() > 0) {
                 foreach ($test_bonuses as $key => $bon) {
                     $item['date'] = $bon->date;
                     $sum += $bon->amount * $currency_rate;
-                    if(strlen($comment) > 0) {
+                    if (strlen($comment) > 0) {
                         $comment .= $bon->comment;
                     } else {
                         $comment = $bon->comment;
@@ -174,8 +181,8 @@ class ObtainedBonus extends Model
             $item['sum'] = $sum;
             $item['comment'] = $comment;
 
-            if($sum > 0) {
-                array_push($bonusHistory, $item);
+            if ($sum > 0) {
+                $bonusHistory[] = $item;
             }
 
         }
@@ -184,22 +191,23 @@ class ObtainedBonus extends Model
         return $bonusHistory;
     }
 
-    public static function getHistoryJSON($user_id, $date, $currency_rate = 1){
+    public static function getHistoryJSON($user_id, $date, $currency_rate = 1)
+    {
         $bonusHistory = [];
 
         $month = Carbon::parse($date);
 
-        $bonuses_all = self::where('user_id', $user_id)
+        $bonuses_all = self::query()->where('user_id', $user_id)
             ->whereYear('date', $month->year)
             ->whereMonth('date', $month->month)
             ->get();
 
-        $test_bonuses_all = TestBonus::where('user_id', $user_id)
+        $test_bonuses_all = TestBonus::query()->where('user_id', $user_id)
             ->whereYear('date', $month->year)
             ->whereMonth('date', $month->month)
             ->get();
 
-        $manual_bonus_all = Salary::where('user_id', $user_id)
+        $manual_bonus_all = Salary::query()->where('user_id', $user_id)
             ->selectRaw('date, bonus, comment_bonus, day(date) as day')
             ->whereYear('date', $month->year)
             ->whereMonth('date', $month->month)
@@ -213,7 +221,7 @@ class ObtainedBonus extends Model
             $item = [];
             $sum = 0;
 
-            if($manual_bonus) {
+            if ($manual_bonus) {
                 $item['date'] = $manual_bonus->date->format('Y-m-d') ?? '';
                 $item['manual'] = [
                     'sum' => $manual_bonus->bonus * $currency_rate,
@@ -222,34 +230,34 @@ class ObtainedBonus extends Model
                 $sum += $manual_bonus->bonus * $currency_rate;
             }
 
-            if($bonuses->count() > 0) {
+            if ($bonuses->count() > 0) {
                 $item['bonuses'] = [];
-                foreach ($bonuses as $key => $bon) {
+                foreach ($bonuses as $bon) {
                     $item['date'] = $bon->date;
-                    array_push($item['bonuses'], [
+                    $item['bonuses'][] = [
                         'sum' => $bon->amount * $currency_rate,
                         'comment' => $bon->comment ? $bon->comment : '',
-                    ]);
+                    ];
                     $sum += $bon->amount * $currency_rate;
                 }
             }
 
-            if($test_bonuses->count() > 0) {
+            if ($test_bonuses->count() > 0) {
                 $item['test'] = [];
-                foreach ($test_bonuses as $key => $bon) {
+                foreach ($test_bonuses as $bon) {
                     $item['date'] = $bon->date;
                     $sum += $bon->amount * $currency_rate;
-                    array_push($item['test'], [
+                    $item['test'][] = [
                         'sum' => $bon->amount * $currency_rate,
                         'comment' => $bon->comment ? $bon->comment : '',
-                    ]);
+                    ];
                 }
             }
 
             $item['sum'] = $sum;
 
-            if($sum > 0) {
-                array_push($bonusHistory, $item);
+            if ($sum > 0) {
+                $bonusHistory[] = $item;
             }
         }
 
