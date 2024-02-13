d9b67fa52c1467860c2e99ee40d08f2749011d26
Vahagn <vahagn99ghukasyan@gmail.com>
2024-02-13 11:44:04 +0400

    add ucall sms integration

--- app/Providers/SmsServiceProvider.php
+++ app/Providers/SmsServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Service\Sms\ApiClientInterface;
-use App\Service\Sms\ClientFactory;
 use App\Service\Sms\SmsInterface;
 use App\Service\Sms\UCallApiClient;
 use App\Service\Sms\UCallSmsService;
@@ -14,7 +13,7 @@ class SmsServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ApiClientInterface::class, function () {
-            return ClientFactory::init(config('services.u-call.api_key'));
+            return new UCallApiClient(config('services.u-call.api_key'));
         });
         $this->app->bind(SmsInterface::class, UCallSmsService::class);
     }
