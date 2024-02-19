46f9985ef8e82b4ade99b03f1ed737741fe1502a
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-02-19 12:17:57 +0500

    seeder for pages and permissions

---
+++ database/seeds/PermissionSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::query()->firstOrCreate(['name' => 'taxes_view', 'guard_name' => 'web']);
+        Permission::query()->firstOrCreate(['name' => 'taxes_edit', 'guard_name' => 'web']);
+
+        Permission::query()->firstOrCreate(['name' => 'shifts_view', 'guard_name' => 'web']);
+        Permission::query()->firstOrCreate(['name' => 'shifts_edit', 'guard_name' => 'web']);
+
+        $settingsPage = Page::query()->where('key', 'settings')->first();
+        Page::query()->firstOrCreate([
+            'name' => 'Смены', 'parent_id' => $settingsPage->id, 'key' => 'shifts'
+        ]);
+        Page::query()->firstOrCreate([
+            'name' => 'Налоги', 'parent_id' => $settingsPage->id, 'key' => 'taxes'
+        ]);
+    }
+}
