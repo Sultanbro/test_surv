a7817c27a3d309fb9b910949737536e53c92f42c
Vahagn <vahagn99ghukasyan@gmail.com>
2024-02-22 12:41:52 +0400

    fix docs resource

--- app/Http/Resources/Files/FileResource.php
+++ app/Http/Resources/Files/FileResource.php
@@ -19,7 +19,7 @@ class FileResource extends JsonResource
             'local_name' => $this->resource->local_name,
             'extension' => $this->resource->extension,
             'url' => $this->resource->s3_url,
-            'signed_at' => $this->when(!is_null($this->resource->signed_at), $this->resource->signed_at),
+            'signed_at' => $this->resource->signed_at,
         ];
     }
 }
