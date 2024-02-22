c17c68966e5d13e08802252233b7ee13bd4e5fce
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-02-22 11:24:38 +0500

    checked

--- app/Salary.php
+++ app/Salary.php
@@ -957,6 +957,9 @@ class Salary extends Model
                 $x = $user->testBonuses->where('day', $d)->sum('amount');
                 if ($x > 0) {
                     $test_bonus[$i] = $x;
+                    if ($earnings[$i] && $user->salaries->where('day', $d)->exists()) {
+                        $allTotal += $x;
+                    }
                 } else {
                     $test_bonus[$i] = null;
                 }
@@ -1017,7 +1020,7 @@ class Salary extends Model
                 $user->kpi = Kpi::userKpi($user->id, $date);
             }
             $allTotal += $user->kpi;
-            $allTotal += array_sum($test_bonus);
+//            $allTotal += array_sum($test_bonus);
             $allTotal += array_sum($awards);
             $allTotal -= $fines_total;
             /**
