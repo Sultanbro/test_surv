978e76ff38ce6bd594fba9161c17bce9aaefd411
takodachi2501 <vrnehot@yandex.kz>
2024-02-28 17:45:24 +0600

    fix: bitrix token

--- config/bitrix.php
+++ config/bitrix.php
@@ -1,7 +1,7 @@
 <?php
 return [
-   'token' => 'lyg5ejdqbyjc1js8',
-   'host'  => 'https://infinitys.bitrix24.kz/rest/158504/',
+   'token' => 'esm1j37wdgx6xh4r',
+   'host'  => 'https://infinitys.bitrix24.kz/rest/66/',
     'notification' => [
         'token' => '8gvj9j84ne251tod',
         'host' => 'https://infinitys.bitrix24.kz/rest/94132/'
