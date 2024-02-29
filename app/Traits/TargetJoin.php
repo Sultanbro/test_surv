43a86955094088f425f8edb645c434053f7e5447
Vahagn <vahagn99ghukasyan@gmail.com>
2024-02-29 10:14:31 +0400

    fix kpi filter

--- app/Traits/TargetJoin.php
+++ app/Traits/TargetJoin.php
@@ -14,26 +14,26 @@ trait TargetJoin
         $table = $query->getModel()->getTable();
 
         return $query->select($table . '.*')
-            ->leftJoin('kpiables as morph', function (JoinClause $join) use ($table) {
-                $join->on('morph.kpi_id', '=', "$table.id");
-            })
+//            ->leftJoin('kpiables as morph', function (JoinClause $join) use ($table) {
+//                $join->on('morph.kpi_id', '=', "$table.id");
+//            })
             ->leftJoin('users as u', function (JoinClause $join) use ($table) {
                 $join->on('u.id', '=', "$table.targetable_id")
                     ->where("$table.targetable_type", '=', 'App\User');
-                $join->orOn('u.id', '=', "morph.kpiable_id")
-                    ->where("morph.kpiable_type", '=', 'App\User');
+//                $join->orOn('u.id', '=', "morph.kpiable_id")
+//                    ->where("morph.kpiable_type", '=', 'App\User');
             })
             ->leftJoin('profile_groups as pg', function (JoinClause $join) use ($table) {
                 $join->on('pg.id', '=', "$table.targetable_id")
                     ->where("$table.targetable_type", '=', 'App\ProfileGroup');
-                $join->orOn('pg.id', '=', "morph.kpiable_id")
-                    ->where("morph.kpiable_type", '=', 'App\ProfileGroup');
+//                $join->orOn('pg.id', '=', "morph.kpiable_id")
+//                    ->where("morph.kpiable_type", '=', 'App\ProfileGroup');
             })
             ->leftJoin('position as p', function (JoinClause $join) use ($table) {
                 $join->on('p.id', '=', "$table.targetable_id")
                     ->where("$table.targetable_type", '=', 'App\Position');
-                $join->orOn('p.id', '=', "morph.kpiable_id")
-                    ->where("morph.kpiable_type", '=', 'App\Position');
+//                $join->orOn('p.id', '=', "morph.kpiable_id")
+//                    ->where("morph.kpiable_type", '=', 'App\Position');
             })
             ->leftJoin('kpi_items as ki', 'ki.kpi_id', '=', 'kpis.id')
             ->leftJoin('users as updater', 'updater.id', '=', "$table.updated_by")
