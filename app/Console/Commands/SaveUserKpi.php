c438d9799b79351c66b67c8728832e6574884a7a
Vahagn <vahagn99ghukasyan@gmail.com>
2024-03-03 18:44:04 +0400

    fix: saved kpi

--- app/Console/Commands/SaveUserKpi.php
+++ app/Console/Commands/SaveUserKpi.php
@@ -113,7 +113,7 @@ class SaveUserKpi extends Command
 
                 foreach ($users as $user) {
                     $total = 0;
-                    dd_if($user['id'] == 4164, $user);
+                    dd_if($user['id'] == 4164, $user['name']);
                     foreach ($user['items'] as $item) {
                         $total += $this->calculator->calcSum($item, $kpi->toArray());
                     }
