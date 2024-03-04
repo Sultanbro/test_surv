b19e62a0cdf25f94210ca0abbab622b44ff17270
Vahagn <vahagn99ghukasyan@gmail.com>
2024-03-04 14:55:58 +0400

    fix saved kpi

--- app/Console/Commands/SaveUserKpi.php
+++ app/Console/Commands/SaveUserKpi.php
@@ -77,7 +77,6 @@ class SaveUserKpi extends Command
     private function calc($kpis, Carbon $date, $userId = null): void
     {
         $startOfMonth = $date->copy()->startOfMonth();
-        $endOfMonth = $date->copy()->endOfMonth();
         foreach ($kpis as $kpi) {
             if ($kpi->histories_latest) {
                 $payload = json_decode($kpi->histories_latest->payload, true);
@@ -86,27 +85,6 @@ class SaveUserKpi extends Command
                     $kpi->items = $kpi->items->whereIn('id', $payload['children']);
                 }
             }
-
-//            foreach ($kpi->items as $item) {
-//                $history = $item->histories->whereBetween('created_at', [$startOfMonth, $endOfMonth])->first();
-//                $has_edited_plan = $history ? json_decode($history->payload, true) : false;
-//                $item['daily_plan'] = (float)$item->plan;
-//                if ($has_edited_plan) {
-//                    if (array_key_exists('plan', $has_edited_plan)) $item['daily_plan'] = $has_edited_plan['plan'];
-//                    if (array_key_exists('name', $has_edited_plan)) $item['name'] = $has_edited_plan['name'];
-//                    if (array_key_exists('share', $has_edited_plan)) $item['share'] = $has_edited_plan['share'];
-//                    if (array_key_exists('method', $has_edited_plan)) $item['method'] = $has_edited_plan['method'];
-//                    if (array_key_exists('unit', $has_edited_plan)) $item['unit'] = $has_edited_plan['unit'];
-//                    if (array_key_exists('cell', $has_edited_plan)) $item['cell'] = $has_edited_plan['cell'];
-//                    if (array_key_exists('common', $has_edited_plan)) $item['common'] = $has_edited_plan['common'];
-//                    if (array_key_exists('percent', $has_edited_plan)) $item['percent'] = $has_edited_plan['percent'];
-//                    if (array_key_exists('sum', $has_edited_plan)) $item['sum'] = $has_edited_plan['sum'];
-//                    if (array_key_exists('group_id', $has_edited_plan)) $item['group_id'] = $has_edited_plan['group_id'];
-//                    if (array_key_exists('activity_id', $has_edited_plan)) $item['activity_id'] = $has_edited_plan['activity_id'];
-//                }
-//                $item['plan'] = $item['daily_plan'];
-//            }
-
             try {
                 $users = $this->statisticService->getUsersForKpi($kpi, $date);
                 if ($userId) {
@@ -116,6 +94,7 @@ class SaveUserKpi extends Command
                 foreach ($users as $user) {
                     $total = 0;
                     foreach ($user['items'] as $item) {
+                        if ($this->notPrioritize($user, $kpi, $startOfMonth)) continue;
                         if ($user['id'] == 3865) {
                             dump($item['kpi_id'], $this->calculator->calcSum($item, $kpi->toArray()));
                         }
@@ -132,6 +111,19 @@ class SaveUserKpi extends Command
                 continue;
             }
         }
+    }
+
+    function notPrioritize(User $user, Kpi $kpi, Carbon $date): bool
+    {
+        $withCurrency = $this->statisticService->fetchKpisWithCurrency([
+            'user_id' => $user->id,
+            'data_from' => [
+                'year' => $date->year,
+                'month' => $date->month
+            ]
+        ]);
+
+        dd($withCurrency);
     }
 
     private function updateSavedKpi(array $data): void
