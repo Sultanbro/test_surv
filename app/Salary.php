be1ec57e9afc089c6ca7d80cea20ff121c2dafc9
Vahagn <vahagn99ghukasyan@gmail.com>
2024-03-05 17:16:36 +0400

    add event analytics

--- app/Salary.php
+++ app/Salary.php
@@ -692,7 +692,7 @@ class Salary extends Model
 
                 $hourly_pay = $zarplata / $workdays / $working_hours;
 
-                dd_if($user->id === 4357, $zarplata . '/' . $workdays . '/' . $working_hours);
+                dump_if($user->id === 4357, $zarplata . '/' . $workdays . '/' . $working_hours);
 
                 $hourly_pays[$i] = round($hourly_pay, 2);
 
