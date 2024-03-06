ba7ac132e24381dcea75c7465ba9a3cd4398a3bf
Vahagn <vahagn99ghukasyan@gmail.com>
2024-03-06 10:41:56 +0400

    add event analytics

--- app/Facade/Referring.php
+++ app/Facade/Referring.php
@@ -127,14 +127,13 @@ class Referring extends Facade
 
         $workedWeeksCount = (int)floor($exists->timetracking_count / 6);
 
-
-        if ($workedWeeksCount < 1) return;
-
         dump_if($user->id == 31451, [
             'timetracking_count' => $workedWeeksCount,
             'date' => $date->format("Y-m-d"),
             'user_applied_at' => $userCurrentGroupStartingDate
         ]);
+
+        if ($workedWeeksCount < 1) return;
 
         if ($workedWeeksCount == 1) {
             $service->touch($exists, PaidType::FIRST_WORK);
