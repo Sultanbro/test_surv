8971ba2a2d61974bf2e3979d317c527ebe36622d
Vahagn <vahagn99ghukasyan@gmail.com>
2024-03-07 11:16:59 +0400

    add method for quartal premiums

--- app/Salary.php
+++ app/Salary.php
@@ -692,6 +692,7 @@ class Salary extends Model
 
 
                 $hourly_pay = $zarplata / $workdays / $working_hours;
+                dump_if($user->id == 25443, $hourly_pay);
 //                $hourly_pay = $user->full_time ? $hourly_pay : $hourly_pay / 2;
 
                 $hourly_pays[$i] = round($hourly_pay, 2);
