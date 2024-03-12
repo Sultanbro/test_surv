99b47450d84e3e365fc4c0ab4b388527fd937cd1
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-03-12 15:47:46 +0400

    fix rentablity

--- app/Http/Controllers/Analytics/NpsController.php
+++ app/Http/Controllers/Analytics/NpsController.php
@@ -33,13 +33,13 @@ class NpsController extends Controller
     public function fetch(Request $request): JsonResponse
     {
         $groupSubQuery = DB::table('profile_groups')
-            ->select(['user_id as user_id', 'group_id as group_id', 'name as group_name', 'work_start', 'work_end', 'has_analytics', 'is_head'])
+            ->select(['profile_groups.user_id as user_id', 'group_id as group_id', 'profile_groups.name as group_name', 'work_start', 'work_end', 'has_analytics', 'is_head'])
             ->join('group_user', 'group_user.group_id', '=', 'profile_groups.id')
             ->join('users', 'group_user.user_id', '=', 'users.id')
             ->where('status', 'active')
             ->whereNull('users.deleted_at')
             ->whereNull('to')
-            ->groupByRaw('group_id, user_id, name, work_start, work_end, has_analytics, is_head');
+            ->groupByRaw('group_id, user_id, profile_groups.name, work_start, work_end, has_analytics, is_head');
 
         $positionSubQuery = DB::table('position')
             ->select(['id', 'position']);
