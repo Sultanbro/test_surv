2438fbbb581c24f446ca447358c54aeb16ad9a9e
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-03-14 15:51:18 +0500

    checked analytics

--- app/Timetracking.php
+++ app/Timetracking.php
@@ -171,9 +171,9 @@ class Timetracking extends Model
         $users = User::withTrashed()
             ->when(empty($positions), fn($users) => $users->where('position_id', Position::OPERATOR_ID), fn($users) => $users->whereIn('position_id', $positions))
             ->whereIn('id', $users)->pluck('id')->toArray();
-        if (auth()->id() == 5) {
-            dd($users);
-        }
+//        if (auth()->id() == 5) {
+//            dd($users);
+//        }
         $total_hours = self::select(
             DB::raw('SUM(total_hours) as total_hours')
         )
