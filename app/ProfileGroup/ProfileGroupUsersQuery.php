7e17832fb6cf450e7dbc38d3bf0d44a1d0a72fb3
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-03-14 15:35:02 +0500

    check analytics

--- app/ProfileGroup/ProfileGroupUsersQuery.php
+++ app/ProfileGroup/ProfileGroupUsersQuery.php
@@ -63,14 +63,14 @@ final class ProfileGroupUsersQuery
                 }
                 $join->on('group_user.user_id', '=', 'users.id');
                 if ($date) {
-                    $join->whereDate('group_user.from', '>=', $date);
+                    $join->whereDate('group_user.from', '>=', $date->format('Y-m-d'));
                     $join->where(function ($query) use ($date) {
                         $query->whereNull('group_user.to');
-                        $query->orWhereDate('group_user.to', '<=', $date);
+                        $query->orWhereDate('group_user.to', '<=', $date->format('Y-m-d'));
                     });
                 }
             });
-dd($this->builder->toSql());
+//dd($this->builder->toSql());
         return $this;
     }
 
