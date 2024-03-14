8973dde15381d5f2394c55ee70fbc5abbd084786
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-03-14 15:38:41 +0500

    checked analytics

--- app/Timetracking.php
+++ app/Timetracking.php
@@ -165,9 +165,9 @@ class Timetracking extends Model
     ): float|int
     {
         $users = ProfileGroup::employees($group_id, $date);
-        if (auth()->id() == 5) {
-            dd($users);
-        }
+//        if (auth()->id() == 5) {
+//            dd($users);
+//        }
         $users = User::withTrashed()
             ->when(empty($positions), fn($users) => $users->where('position_id', Position::OPERATOR_ID), fn($users) => $users->whereIn('position_id', $positions))
             ->whereIn('id', $users)->pluck('id')->toArray();
