a9f71c36457a186a2bca9f309faec49a91bba08e
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-03-21 18:14:33 +0400

    fix subscription

--- app/Console/Commands/CountHours.php
+++ app/Console/Commands/CountHours.php
@@ -35,7 +35,6 @@ class CountHours extends Command
                 }
             )
             ->get();
-        dd($timeTrackRecords);
         foreach ($timeTrackRecords as $record) {
             /** @var User $user */
             $user = $record->user;
