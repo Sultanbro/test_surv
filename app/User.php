fca1f5524155f2c453a0a5e70971a8bb56c5a5c1
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-03-26 10:13:37 +0400

    fix: worse houly pay

--- app/User.php
+++ app/User.php
@@ -1792,12 +1792,12 @@ class User extends Authenticatable implements Authorizable, ReferrerInterface
             $date2 = date_create($firstWorkDay);
             $differBetweenFirstAndLastDay = date_diff($date1, $date2)->days;
 
-            $remains = $differBetweenFirstAndLastDay / $total;
-            dd_if($this->id == 18123, $remains);
+            $remains = $differBetweenFirstAndLastDay % $total;
 
             if ($remains < $workingDay && $dayInMonth != Carbon::parse($firstWorkDay)->subDay()->toDateString()) {
                 $workDayInMonth++;
             }
+
         }
 
 
