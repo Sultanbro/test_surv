b82c8c2d7cadc0dbe7644a6ff48b4fd53422cfed
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-03-27 11:17:35 +0500

    check central user data

--- app/Http/Requests/Tenant/RegisterRequest.php
+++ app/Http/Requests/Tenant/RegisterRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Tenant;
 
+use App\Rules\CheckCentralUsers;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -12,10 +13,14 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:190'],
             'last_name' => ['string', 'max:190', 'nullable'],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')],
+            'email' => ['required', 'string', 'email', 'max:255', new CheckCentralUsers('email')],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'phone' => ['required', 'string', 'min:11', 'max:30', Rule::unique('users', 'phone')],
-            'g-recaptcha-response' => [Rule::requiredIf(!app()->environment('testing')), 'recaptcha'],
+            'phone' => ['required', 'string', 'min:11', 'max:30', new CheckCentralUsers('phone')],
+            'g-recaptcha-response' => [
+                Rule::requiredIf(!app()->environment('local')),
+                Rule::excludeIf(app()->environment('local')),
+                'recaptcha',
+            ],
             'currency' => [
                 'required', Rule::in([
                     'kzt',
