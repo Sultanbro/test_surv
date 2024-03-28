39940ba27a944ee0748c8123642ef9abc6a69147
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-03-28 17:02:00 +0400

    integrate prodamus

--- database/migrations/2023_02_08_090200_create_tariff_payment_table.php
+++ database/migrations/2023_02_08_090200_create_tariff_payment_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,29 +12,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::connection('mysql')->create('tariff_payment', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('owner_id')
-                ->unsigned()
-                ->comment('User with is_admin=true');
-            $table->unsignedBigInteger('tariff_id')->unsigned();
-            $table->integer('extra_user_limit')
-                ->comment('Extra user amount added upon tariff user_limit')
-                ->nullable();
-            $table->date('expire_date')->comment('Срок истечения тарифа');
-            $table->boolean('auto_payment')->default(0)->comment('1 - Автоплатеж включен');
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        Schema::connection('mysql')
+            ->create('tariff_payment', function (Blueprint $table) {
+                $table->id();
+                $table->integer('extra_user_limit')
+                    ->comment('Extra user amount added upon tariff user_limit')
+                    ->nullable();
+                $table->date('expire_date')->comment('Срок истечения тарифа');
+                $table->boolean('auto_payment')->default(0)->comment('1 - Автоплатеж включен');
+                $table->timestamp('created_at')->useCurrent();
+                $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
-            $table->foreign('owner_id')
-                ->references('id')
-                ->on('users')
-                ->onUpdate('cascade');
-            $table->foreign('tariff_id')
-                ->references('id')
-                ->on('tariff')
-                ->onUpdate('cascade');
-        });
+                $table->foreignId('owner_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade');
+                $table->foreignId('tariff_id')
+                    ->references('id')
+                    ->on('tariff')
+                    ->onUpdate('cascade');
+            });
     }
 
     /**
