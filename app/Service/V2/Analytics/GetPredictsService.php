2bfdad4addee09ba24e6431d50474867885803e4
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-03-31 23:47:13 +0400

    fix mardja

--- app/Service/V2/Analytics/GetPredictsService.php
+++ app/Service/V2/Analytics/GetPredictsService.php
@@ -15,13 +15,15 @@ class GetPredictsService
     {
         $from = now()->firstOfMonth();
         $baseSubQuery = DB::table('users')
-            ->select(DB::raw('group_user.group_id as group_id'))
-            ->select(DB::raw('count(*) as count'))
+            ->select([
+                DB::raw('piv.group_id as group_id'),
+                DB::raw('count(*) as count')
+            ])
             ->join(DB::raw('user_descriptions as ud'), 'ud.user_id', '=', 'users.id')
             ->join(DB::raw('group_user as piv'), 'piv.user_id', '=', 'users.id')
             ->where('piv.status', 'active')
             ->where('users.created_at', '>=', $from)
-            ->groupBy('group_id');
+            ->groupBy('piv.group_id');
 
         $activeUsersSubQuery = $baseSubQuery;
 
@@ -31,10 +33,10 @@ class GetPredictsService
         return ProfileGroup::isActive()
             ->leftJoinSub($activeUsersSubQuery, 'active_users', 'profile_groups.id', 'active_users.group_id')
             ->leftJoinSub($activeTraineeSubQuery, 'active_trainees', 'profile_groups.id', 'active_trainees.group_id')
-//            ->leftJoinSub($activeEmployeeSubQuery, 'active_employees', 'profile_groups.id', 'active_employees.group_id')
+            ->leftJoinSub($activeEmployeeSubQuery, 'active_employees', 'profile_groups.id', 'active_employees.group_id')
             ->addSelect(DB::raw('active_users.count as users_total'))
             ->addSelect(DB::raw('active_trainees.count as users_trainees'))
-//            ->addSelect(DB::raw('active_employees.count as users_employees'))
+            ->addSelect(DB::raw('active_employees.count as users_employees'))
             ->hasAnalytics()
             ->get()
             ->map(function ($group) {
