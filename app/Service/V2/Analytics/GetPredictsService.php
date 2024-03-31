d4759f7d003f5066ee1b46cd494a1e274ef5399d
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-04-01 00:33:32 +0400

    fix mardja

--- app/Service/V2/Analytics/GetPredictsService.php
+++ app/Service/V2/Analytics/GetPredictsService.php
@@ -17,30 +17,23 @@ class GetPredictsService
     {
         $from = now()->firstOfMonth();
 
-        $activeUsersSubQuery = $this->baseSubQuery($from);
-        $activeTraineeSubQuery = $this->baseSubQuery($from)
-            ->where('ud.is_trainee', 1);
-        $activeEmployeeSubQuery = $this->baseSubQuery($from)
-            ->where('ud.is_trainee', 0);
-
-        return ProfileGroup::isActive()
-            ->hasAnalytics()
-            ->leftJoinSub($activeUsersSubQuery, 'active_users', 'active_users.group_id', 'profile_groups.id')
-            ->leftJoinSub($activeTraineeSubQuery, 'active_trainees', 'active_trainees.group_id', 'profile_groups.id')
-            ->leftJoinSub($activeEmployeeSubQuery, 'active_employees', 'active_employees.group_id', 'profile_groups.id')
-            ->select([
-                'id',
-                'name',
-                'required',
-                DB::raw('count(active_users.user_id) as users_total'),
-                DB::raw('count(active_trainees.user_id) as trainees_total'),
-                DB::raw('count(active_employees.user_id) as employees_total')
-            ])
-            ->groupBy([
-                'id',
-                'name',
-                'required'
-            ])
+        return ProfileGroup::query()
+            ->select(
+                'profile_groups.id',
+                'profile_groups.name',
+                'profile_groups.required',
+                DB::raw('count(users.id) as users_total'),
+                DB::raw('sum(case when ud.is_trainee = 0 then 1 else 0 end) as trainees_total'),
+                DB::raw('sum(case when ud.is_trainee = 1 then 1 else 0 end) as employees_total')
+            )
+            ->join('group_user as piv', 'piv.group_id', '=', 'profile_groups.id')
+            ->join('users', 'piv.user_id', '=', 'users.id')
+            ->join('user_descriptions as ud', 'ud.user_id', '=', 'users.id')
+            ->where('piv.status', 'active')
+            ->where('profile_groups.active', ProfileGroup::IS_ACTIVE)
+            ->whereIn('profile_groups.has_analytics', [ProfileGroup::HAS_ANALYTICS, ProfileGroup::ARCHIVED])
+            ->where('piv.from', '>=', $from)
+            ->groupBy('profile_groups.id', 'profile_groups.name', 'profile_groups.required')
             ->get()
             ->map(function ($group) {
                 return [
@@ -54,18 +47,5 @@ class GetPredictsService
                     'plan' => $group->required
                 ];
             });
-    }
-
-    private function baseSubQuery(Carbon $from): Builder
-    {
-        return DB::table('users')
-            ->select([
-                DB::raw('piv.group_id as group_id'),
-                DB::raw('piv.user_id as user_id'),
-            ])
-            ->join(DB::raw('user_descriptions as ud'), 'ud.user_id', '=', 'users.id')
-            ->join(DB::raw('group_user as piv'), 'piv.user_id', '=', 'users.id')
-            ->where('piv.status', 'active')
-            ->where('piv.from', '>=', $from);
     }
 }
