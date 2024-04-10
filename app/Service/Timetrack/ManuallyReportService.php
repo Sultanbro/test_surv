ce2b3ba5b97636f679ea47287257a5178f2f58a5
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-04-10 14:27:04 +0400

    fix: time enter

--- app/Service/Timetrack/ManuallyReportService.php
+++ app/Service/Timetrack/ManuallyReportService.php
@@ -85,7 +85,7 @@ final class ManuallyReportService
     {
         $user = User::query()->find($userId);
         /** @var User $user */
-        $time = Carbon::createFromTimeString($time, "UTC");
+        $time = Carbon::createFromTimeString($time, $user->timezone());
 //        $convertedTime = $time->setTimezone($user->timezone()); // Convert to user's timezone
         dd($time);
 
