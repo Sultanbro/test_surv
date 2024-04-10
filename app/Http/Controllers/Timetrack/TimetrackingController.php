71200083cdd3f7ac30a7cd7b58c37b5813863316
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-04-10 17:00:41 +0400

    fix: time enter

--- app/Http/Controllers/Timetrack/TimetrackingController.php
+++ app/Http/Controllers/Timetrack/TimetrackingController.php
@@ -1407,10 +1407,17 @@ class TimetrackingController extends Controller
 
 
                 foreach ($days as $day) {
+                    dd_if($userData->id == 5 && $day == 10,
+                        $userData->timetracking
+                            ->where('date', $day)
+                            ->min('enter')
+//                        ->setTimezone(Setting::TIMEZONES[5])
+                            ->format('H:i')
+                    );
                     $data[$userData->id][$day] = $userData->timetracking
                         ->where('date', $day)
                         ->min('enter')
-                        ->setTimezone(Setting::TIMEZONES[5])
+//                        ->setTimezone(Setting::TIMEZONES[5])
                         ->format('H:i');
                 }
 
