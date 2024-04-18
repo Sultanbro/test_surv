3680bb7125ea3925d5ea2a29edc511eb73d1d5ad
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-04-18 13:33:57 +0400

    fix:user start time fine

--- app/Service/Timetrack/UserLateService.php
+++ app/Service/Timetrack/UserLateService.php
@@ -41,7 +41,7 @@ class UserLateService
 
         if (!$actualTime) return;
         //Разница в минутах.
-        $diffInMinutes = $actualTime->diffInRealMinutes($shouldStartTime);
+        $diffInMinutes = $actualTime->diffInMinutes($shouldStartTime,false);
 
         dd($diffInMinutes);
         // Если минута 0 или меньше 0, то сотрудник пришел вовремя.
