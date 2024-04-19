c135f884051b94fb4451a59125ac1df2917e4af5
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-04-19 11:51:22 +0500

    zaybal eti

--- resources/js/pages/courses/CoursesCatalog.vue
+++ resources/js/pages/courses/CoursesCatalog.vue
@@ -1,13 +1,24 @@
 <template>
-	<div>
-		catalog
-	</div>
+	<div class="MyComp" />
 </template>
 
 <script>
 export default {
-
+	name: 'MyComp',
+	components: {},
+	props: {},
+	data(){
+		return {}
+	},
+	computed: {},
+	watch: {},
+	created(){},
+	mounted(){},
+	beforeDestroy(){},
+	methods: {},
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+//.MyComp{}
+</style>
--- resources/js/pages/courses/CoursesPage.vue
+++ resources/js/pages/courses/CoursesPage.vue
@@ -1,19 +1,66 @@
 <template>
-	<div>
-		<CourseFilter />
-		<CourseTable />
+	<div class="CoursesPage">
+		<b-row class="mt-4">
+			<b-col cols="4">
+				<CoursesFilter
+					v-model="filter"
+					:search="search"
+					@search="search = $event"
+				/>
+			</b-col>
+			<b-col cols="4">
+				<JobtronButton
+					small
+				>
+					Создать курс
+				</JobtronButton>
+			</b-col>
+			<b-col cols="4" />
+		</b-row>
 	</div>
 </template>
 
 <script>
-import CourseFilter from './components/CourseFilter.vue';
-import CourseTable from './components/CourseTable.vue';
+// import {
+// 	fetchV2Courses,
+// 	fetchV2Course,
+// 	createV2Course,
+// 	updateV2Course,
+// 	deleteV2Course,
+// } from '@/stores/api/courses.js'
+
+import CoursesFilter from '@/components/pages/Courses/CoursesFilter.vue'
+import JobtronButton from '../../components/ui/Button.vue'
+
 export default {
+	name: 'CoursesPage',
 	components: {
-		CourseFilter,
-		CourseTable
+		CoursesFilter,
+		JobtronButton,
 	},
-};
+	props: {},
+	data(){
+		return {
+			search: '',
+			filter: {
+				targets: [],
+				type: '',
+				sale: '',
+				created: ['', ''],
+			},
+		}
+	},
+	computed: {},
+	watch: {},
+	created(){},
+	mounted(){},
+	beforeDestroy(){},
+	methods: {},
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.CoursesPage{
+	min-height: calc(100vh - 80px); // для корректной работы клик оутсайд
+}
+</style>
