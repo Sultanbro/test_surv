523d23e85e7506a3334a0a3066218ba025fb998d
takodachi2501 <vrnehot@yandex.kz>
2024-04-22 09:53:39 +0500

    fix: ckp string

--- resources/js/pages/professions.vue
+++ resources/js/pages/professions.vue
@@ -379,8 +379,8 @@ export default {
 				this.isSpec = data[0].is_spec;
 				this.sum = data[0].sum;
 				this.ckp_status = data[0].ckp_status
-				this.ckp = data[0].ckp
-				this.ckp_link = data[0].ckp_link
+				this.ckp = data[0].ckp || ''
+				this.ckp_link = data[0].ckp_link || ''
 				this.desc = {
 					require: data[0].require,
 					actions: data[0].actions,
@@ -433,8 +433,8 @@ export default {
 					is_head: this.isHead,
 					is_spec: this.isSpec,
 					ckp_status: this.ckp_status,
-					ckp: this.ckp,
-					ckp_link: this.ckp_link,
+					ckp: this.ckp || '_',
+					ckp_link: this.ckp_link || '_',
 				})
 				if(responseSave.data.status !== 200) return this.$toast.error('Упс! Что-то пошло не так');
 				this.$toast.success(this.addNew ? 'Новая должность создана' : 'Изменения сохранены');
