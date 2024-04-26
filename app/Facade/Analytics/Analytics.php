bb9d5969d4e323c8d74f273487bbb3e7916bab05
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-04-26 13:47:44 +0500

    check distinct analytics

--- app/Facade/Analytics/Analytics.php
+++ app/Facade/Analytics/Analytics.php
@@ -446,7 +446,13 @@ final class Analytics
                     ->where('date', '<=', $dateFrom);
             })
             ->get();
-dd($users->only(['id', 'name', 'last_name'])->toArray());
+
+        $subset = $users->map(function ($user) {
+            return collect($user->toArray())
+                ->only(['id', 'name', 'last_name'])
+                ->all();
+        });
+        dd($subset->toArray());
         return $users->each(function ($employee) use ($firstOfMoth, $activity) {
             $workDay = isset($user->working_day_id) && $user->working_day_id == 1 ? WorkingDay::FIVE_DAYS : WorkingDay::SIX_DAYS;
             $appliedFrom = $employee->workdays_from_applied($firstOfMoth, $workDay);
