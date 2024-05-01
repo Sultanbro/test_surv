5f6018c5ecd8005d356a12fcf43fbb390fab4945
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-01 17:55:38 +0400

    fix analytics

--- app/Models/Analytics/AnalyticStat.php
+++ app/Models/Analytics/AnalyticStat.php
@@ -486,7 +486,7 @@ class AnalyticStat extends Model
     {
         $letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-        $sl_pos = -1;
+        $sl_pos = 0;
 
         $fl_pos = ($number + 1) % 26;
         if ($fl_pos == 0) $sl_pos++;
