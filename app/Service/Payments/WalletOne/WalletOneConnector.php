bcabe6a8519a60a3bf75498ef2300a3408570ba1
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-08 13:09:26 +0400

    fix: wallet one

--- app/Service/Payments/WalletOne/WalletOneConnector.php
+++ app/Service/Payments/WalletOne/WalletOneConnector.php
@@ -34,8 +34,8 @@ class WalletOneConnector implements PaymentConnector
         $idempotenceKey = $this->generateIdempotenceKey();
         $body = [
             "WMI_MERCHANT_ID" => $this->merchantId,
-            "WMI_PTENABLED" => 'W1KZT',
-            "WMI_PTDISABLED" => 'W1RUB',
+//            "WMI_PTENABLED" => 'W1KZT',
+//            "WMI_PTDISABLED" => 'W1RUB',
             "WMI_CUSTOMER_PHONE" => Phone::normalize($user->phone),
             "WMI_PAYMENT_NO" => $idempotenceKey,
             "WMI_CURRENCY_ID" => self::CURRENCY,
