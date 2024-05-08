0088005a7e9b8e720a8d667c0900749e7242d1e5
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-08 16:42:01 +0400

    fix: cells in analytics

--- app/Facade/Analytics/Analytics.php
+++ app/Facade/Analytics/Analytics.php
@@ -399,7 +399,7 @@ final class Analytics
 
         $stats = $stats->where('row_id', $rowId)->whereIn('column_id', $columns);
 
-        dd_if(auth()->id() == 5 && $stats->row_id = 15348 && $stats->column_id = 25987, $stats->pluck('show_value')->toArray());
+        dd_if(auth()->id() == 5 && $stats->row_id = 15348 && $stats->column_id = 25987, $stats->toArray());
 
         foreach ($stats as $stat) {
             if ($stat && is_numeric($stat->show_value)) {
