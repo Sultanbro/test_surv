3c7915129c90ff2bf846a458b67115ad9c4360b6
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-09 19:46:49 +0400

    fix adding new user witout user working country

--- database/migrations/tenant/2024_05_09_143431_set_user_working_country_field_nullable_in_users_table.php
+++ database/migrations/tenant/2024_05_09_143431_set_user_working_country_field_nullable_in_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('last_seen')->nullable();
+            $table->string('working_country')->nullable()->change();
         });
     }
 
@@ -26,7 +25,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('last_seen');
+            $table->string('working_country')->change();
         });
     }
 };
