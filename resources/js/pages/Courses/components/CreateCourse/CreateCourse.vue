14fd7c624f766c09aab6dd96e730d4bacf0eb33b
unknown <ruslanchik_nadyrov@mail.ru>
2024-05-10 15:53:01 +0600

    feat:settings page courses

--- resources/js/pages/Courses/components/CreateCourse/CreateCourse.vue
+++ resources/js/pages/Courses/components/CreateCourse/CreateCourse.vue
@@ -9,10 +9,11 @@
 					:active-link="activeLink"
 					@update:activeLink="activeLink = $event"
 				/>
-				<AppearanceCourse v-if="activeLink===1" />
-				<AppearanceUploadImage v-if="activeLink===1" />
-				<MaterialCourse v-if="activeLink===2" />
-				<CourseIsTaking v-if="activeLink===3" />
+				<AppearanceCourse v-if="activeLink === 1" />
+				<AppearanceUploadImage v-if="activeLink === 1" />
+				<MaterialCourse v-if="activeLink === 2" />
+				<CourseIsTaking v-if="activeLink === 3" />
+				<SettingsCourses v-if="activeLink === 4" />
 			</div>
 		</DefaultLayout>
 	</div>
@@ -21,6 +22,7 @@
 <script>
 import SideBarCourse from './Appearance/SideBarCourse.vue';
 import AppearanceCourse from './Appearance/AppearanceCourse.vue';
+import SettingsCourses from './Appearance/SettingsCourses.vue';
 import AppearanceUploadImage from './Appearance/AppearanceUploadImage.vue';
 import DefaultLayout from '../../../../layouts/DefaultLayout.vue';
 import MaterialCourse from './MaterialCourse/MaterialCourse.vue';
@@ -30,7 +32,13 @@ export default {
 	name: 'CreateCourse',
 	components: {
 		CourseIsTaking,
-		MaterialCourse, DefaultLayout, AppearanceUploadImage, AppearanceCourse, SideBarCourse },
+		MaterialCourse,
+		DefaultLayout,
+		AppearanceUploadImage,
+		AppearanceCourse,
+		SideBarCourse,
+		SettingsCourses
+	},
 	data() {
 		return {
 			isOpen: false,
@@ -44,7 +52,6 @@ export default {
 		closeModal() {
 			this.isOpen = false;
 		},
-
 	},
 };
 </script>
@@ -58,15 +65,11 @@ export default {
 	height: 100vh;
 	background-color: white;
 	z-index: 9999;
-
-
 }
 
-.create-course-content{
+.create-course-content {
 	display: flex;
-		justify-content: space-around;
+	justify-content: space-around;
 	background-color: #fafafa;
 }
-
-
-</style>
+</style>./Appearance/SettingsCourses.vue
