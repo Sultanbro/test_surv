75607cb83895f9db19ca1566b87d37e78b82a286
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-13 17:42:53 +0400

    fix table recration

--- database/migrations/2024_05_09_114528_create_tariff_table.php
+++ database/migrations/2024_05_09_114528_create_tariff_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
+        Schema::connection('mysql')->dropIfExists('tariff');
         Schema::connection('mysql')
             ->create('tariff', function (Blueprint $table) {
                 $table->id();
