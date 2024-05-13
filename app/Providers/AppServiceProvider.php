edf2855f217d677756df19d32ed1c28756036b95
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-05-13 13:34:55 +0500

    check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -100,7 +100,7 @@ class AppServiceProvider extends ServiceProvider
     private function dataToVue(): array
     {
         if (\Auth::guest()) return ['csrfToken' => csrf_token()];
-
+        dd(auth()->user());
         $permissions = auth()->user()->getAllPermissions()->pluck('name')->toArray();
 
         if (auth()->user()->program_id === 1 && tenant('id') == 'bp') {
