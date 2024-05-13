1b5cbbcb1d2f1cc3a79886092614208fda2943fb
unknown <ruslanchik_nadyrov@mail.ru>
2024-05-13 12:16:54 +0600

    feat: fix workshop page

--- resources/js/pages/workshop/WorkshopPage.vue
+++ resources/js/pages/workshop/WorkshopPage.vue
@@ -1,17 +1,9 @@
 <template>
 	<DefaultLayout>
 		<div class="workshop">
-			<div class="workshop-container">
-				<div>
-					<button>
-						<a
-							href="https://wl.walletone.com/checkout/form/119184"
-							target="_blank"
-							rel="noopener noreferrer"
-						>Оплата картой Казахстана</a>
-					</button>
-				</div>
-			</div>
+			<button @click="openPayForm">
+				Оплата картой Казахстана
+			</button>
 		</div>
 	</DefaultLayout>
 </template>
@@ -24,33 +16,27 @@ export default {
 	components: {
 		DefaultLayout,
 	},
+	methods: {
+		openPayForm() {
+			window.open(
+				'https://wl.walletone.com/checkout/refill/CreditCard/347428001/NewCard',
+				'_blank'
+			);
+		},
+	},
 };
 </script>
 
 <style scoped lang="scss">
-%center-content {
+.workshop {
+	font-size: 25px;
 	min-height: 100vh;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-}
-
-.workshop {
-	font-size: 25px;
-	@extend %center-content;
-	&-container {
-		@extend %center-content;
-		background-color: #ffffff;
-		border-radius: 15px;
-		padding: 1%;
-		min-height: 500px;
-		min-width: 500px;
-		box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px,
-			rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px;
-	}
 	button {
 		background-color: #33b4ff;
-		padding: 2%;
+		padding: 1%;
 		color: white;
 		border-radius: 10px;
 		transition: all ease 100ms;
