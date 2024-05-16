79a36b70e1ba8d9d890426d52053e0701026c91d
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-16 13:12:54 +0400

    add formated events api

--- app/Http/Controllers/Subscription/TrialSubscriptionController.php
+++ app/Http/Controllers/Subscription/TrialSubscriptionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Subscription;
 
+use App\DTO\Payment\CreateInvoiceDTO;
 use App\Http\Controllers\Controller;
+use App\Models\Tariff\PaymentToken;
+use App\Models\Tariff\Tariff;
+use App\Service\Subscription\SubscribeService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -12,7 +16,10 @@ class TrialSubscriptionController extends Controller
     public function enable(Request $request): JsonResponse
     {
         $tenant = $request->get('tenant_id') ?? tenant('id');
+        $tariff = Tariff::query()->find();
+        $service = new SubscribeService(new CreateInvoiceDTO(
 
+        ), new PaymentToken("trial"));
         return $this->response(
             message: 'Success',
             data: []
