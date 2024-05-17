a70474398f07e1c8c70a4fb41e8220b1ff373e70
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-17 12:07:27 +0400

    fast fix

--- database/migrations/2024_05_09_114526_create_tariff_table.php
+++ database/migrations/2024_05_09_114526_create_tariff_table.php
@@ -25,7 +25,6 @@ return new class extends Migration {
                 $table->string('validity')->default(TariffValidityEnum::Monthly)
                     ->comment('Период действия monthly-ежемесячно, annual-ежегодно');
                 $table->integer('users_limit')->comment('How many people can be assigned');
-                $table->decimal('price'); // TODO we should delete this and make another table named tariff_prices for kzt, usd, rub, ect..
                 $table->timestamp('created_at')->useCurrent();
                 $table->timestamp('updated_at')->useCurrentOnUpdate();
             });
