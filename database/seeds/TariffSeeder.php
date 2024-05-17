e17dd258fa7b403516897fe452a8713b145772c1
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-17 11:27:18 +0400

    fast fix

--- database/seeds/TariffSeeder.php
+++ database/seeds/TariffSeeder.php
@@ -35,7 +35,6 @@ class TariffSeeder extends Seeder
                 $tariffModel->validity = $validity;
                 $tariffModel->users_limit = $tariff['users_limit'];
                 $tariffModel->save();
-                dd($tariffModel);
                 $salePrice = 0;
                 foreach ($tariff['prices'] as $currency => $price) {
 
