5aa247b97e07ba0dc246bc6693fc9ce5ca1b0aa5
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-17 10:53:41 +0400

    fast fix

--- config/tariffs.php
+++ config/tariffs.php
@@ -2,39 +2,39 @@
 
 return [
     'free' => [
+        'sale_percent' => 0,
         'id' => 1,
         'users_limit' => 5,
         'prices' => [
             'kzt' => 0,
             'rub' => 0,
         ],
-        'sale_percent' => 0,
     ],
     'base' => [
+        'sale_percent' => 20,
         'id' => 2,
         'users_limit' => 20,
         'prices' => [
             'kzt' => 7158,
             'rub' => 1827,
         ],
-        'sale_percent' => 20,
     ],
     'standard' => [
+        'sale_percent' => 20,
         'id' => 3,
         'users_limit' => 50,
         'prices' => [
             'kzt' => 22966,
             'rub' => 5863,
         ],
-        'sale_percent' => 20,
     ],
     'pro' => [
+        'sale_percent' => 20, // per year
         'id' => 4,
         'users_limit' => 100,
         'prices' => [
             'kzt' => 85004,
             'rub' => 21073,
         ],
-        'sale_percent' => 20, // per year
     ],
 ];
