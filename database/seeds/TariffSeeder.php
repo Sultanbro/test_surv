b6ab72ddea39f8c625e861ccf3c7015f6bdb8faf
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-17 11:45:12 +0400

    fast fix

--- database/seeds/TariffSeeder.php
+++ database/seeds/TariffSeeder.php
@@ -21,34 +21,33 @@ class TariffSeeder extends Seeder
         DB::connection('mysql')->table('tariff_payment')->delete();
         DB::connection('mysql')->table('tariff')->delete();
         DB::connection('mysql')->statement('SET FOREIGN_KEY_CHECKS=1;');
-        $tariffs = config('tariffs');
-        ksort($tariffs);
-        $validates = [
-            'yearly' => 12,
-            '3_monthly' => 3,
-            'monthly' => 1,
-        ];
-
-        foreach ($validates as $validity => $monthsCount) {
-            foreach ($tariffs as $name => $tariff) {
-                $tariffModel = new Tariff();
-                $tariffModel->kind = $name;
-                $tariffModel->validity = $validity;
-                $tariffModel->users_limit = $tariff['users_limit'];
-                $tariffModel->save();
-                $salePrice = 0;
-                foreach ($tariff['prices'] as $currency => $price) {
-
-                    if ($monthsCount > 1) {
-                        $salePrice = ($price * $monthsCount) * $tariff['sale_percent'] / 100;
-                    }
-
-                    $tariffModel->prices()->create([
-                        'currency' => $currency,
-                        'value' => ($price * $monthsCount) - $salePrice,
-                    ]);
-                }
-            }
-        }
+//        $tariffs = config('tariffs');
+//        $validates = [
+//            'yearly' => 12,
+//            '3_monthly' => 3,
+//            'monthly' => 1,
+//        ];
+//
+//        foreach ($validates as $validity => $monthsCount) {
+//            foreach ($tariffs as $name => $tariff) {
+//                $tariffModel = new Tariff();
+//                $tariffModel->kind = $name;
+//                $tariffModel->validity = $validity;
+//                $tariffModel->users_limit = $tariff['users_limit'];
+//                $tariffModel->save();
+//                $salePrice = 0;
+//                foreach ($tariff['prices'] as $currency => $price) {
+//
+//                    if ($monthsCount > 1) {
+//                        $salePrice = ($price * $monthsCount) * $tariff['sale_percent'] / 100;
+//                    }
+//
+//                    $tariffModel->prices()->create([
+//                        'currency' => $currency,
+//                        'value' => ($price * $monthsCount) - $salePrice,
+//                    ]);
+//                }
+//            }
+//        }
     }
 }
