019905a54d0ac685220324d1c039b708695a0fb0
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-22 09:14:34 +0400

    payment relize 2.1

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Tariff\TariffController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tariffs/get', [TariffController::class, 'get'])->name('lending.tariffs.get');
+Route::get('/tariff/get', [TariffController::class, 'get'])->name('lending.tariffs.get');
