a1b8089af9785b12da30ed391f3cd136b68794dd
unknown <ruslanchik_nadyrov@mail.ru>
2024-05-23 16:12:04 +0600

    feat: fix getAmount

--- resources/js/pages/workshop/WorkshopPage.vue
+++ resources/js/pages/workshop/WorkshopPage.vue
@@ -3,22 +3,64 @@
 		<h1 class="workshop__title">
 			Оплата заказа
 		</h1>
-		<div
-			v-for="button in buttons"
-			:key="button.id"
-		>
-			<button
-				class="workshop__button"
-				@click="openPayForm(button.link)"
-			>
-				{{ button.title }}
-				<img
-					class="icon"
-					width="50"
-					height="50"
-					:src="button.img"
-					alt=""
+		<div class="wrapper">
+			<div style="margin-top: 5%">
+				<h2 style="font-size: 20px">
+					Оплатить картой
+				</h2>
+				<div style="display: flex; flex-direction: column">
+					<button>
+						<a
+							href="https://wl.walletone.com/checkout/refill/CreditCard/347770942/NewCard"
+						>
+							<img
+								width="40"
+								height="40"
+								src="https://cdn-icons-png.flaticon.com/512/5352/5352016.png"
+								alt=""
+							>
+							Картой банка не из Росиии
+						</a>
+					</button>
+					<button style="margin-top: 2%">
+						<a href="https://proeducation.kz/e44NQ/">
+							<img
+								width="40"
+								height="40"
+								src="https://gallery.yopriceville.com/downloadfullsize/send/10186"
+								alt=""
+							>
+							Картой Российского банка
+						</a>
+					</button>
+				</div>
+			</div>
+			<button style="margin-top: 2%">
+				<a href="https://pay.kaspi.kz/pay/jjtpkyxq">
+					<img
+						width="40"
+						height="40"
+						src="https://cdn-icons-png.flaticon.com/512/262/262042.png"
+						alt=""
+					>
+					Оплата Каспи Рассрочка
+				</a>
+			</button>
+			<button style="margin-top: 2%">
+				<a
+					href="https://wl.walletone.com/checkout/refill/CreditCard/347773203/NewCard"
 				>
+					<img
+						width="40"
+						height="40"
+						src="https://cdn-icons-png.flaticon.com/512/262/262042.png"
+						alt=""
+					>
+					Тест
+				</a>
+			</button>
+			<button @click="getAmount">
+				click
 			</button>
 		</div>
 	</div>
@@ -29,40 +71,42 @@ export default {
 	name: 'WorkshopPage',
 	data() {
 		return {
-			buttons: [
-				{
-					id: 1,
-					title: 'Картой банка не из Росиии',
-					link: 'https://wl.walletone.com/checkout/refill/CreditCard/347770942/NewCard',
-					img: 'https://cdn-icons-png.flaticon.com/512/5352/5352016.png',
-					dopImg:
-						'https://wl.walletone.com/Style/checkout/Areas/Cards/Shared/img/credit-card.svg',
-					desc: 'Оплачивая картой вы соглашаетесь с услагами оферты',
-				},
-				{
-					id: 2,
-					title: 'Картой Российского банка',
-					link: 'https://proeducation.kz/e44NQ/',
-					img: 'https://gallery.yopriceville.com/downloadfullsize/send/10186',
-				},
-				{
-					id: 3,
-					title: 'Оплата Каспи Рассрочка',
-					link: 'https://pay.kaspi.kz/pay/jjtpkyxq',
-					img: 'https://cdn-icons-png.flaticon.com/512/262/262042.png',
-				},
-				{
-					id: 3,
-					title: 'Тест',
-					link: 'https://wl.walletone.com/checkout/refill/CreditCard/347773203/NewCard',
-					img: 'https://cdn-icons-png.flaticon.com/512/262/262042.png',
-				},
-			],
+			amountApi: 'https://jobtron.org/api/payments/invoices/wallet-one',
+			data: [],
 		};
+	},
+	created() {
+		this.openPayForm();
+	},
+	mounted() {
+		this.openPayForm();
 	},
 	methods: {
 		openPayForm(link) {
 			window.open(link, '_blank');
+		},
+		createForm(res) {
+			const form = document.createElement('form');
+			form.method = 'post';
+			form.action = res.url;
+			Object.keys(res.params).forEach((key) => {
+				const inp = document.createElement('input');
+				inp.name = key;
+				inp.value = res.params[key];
+				form.appendChild(inp);
+			});
+			document.body.appendChild(form);
+			form.submit();
+		},
+		getAmount() {
+			return this.axios
+				.post(this.amountApi, {
+					amount: 100,
+				})
+				.then((res) => {
+          
+					this.createForm(res.data)
+				});
 		},
 	},
 };
