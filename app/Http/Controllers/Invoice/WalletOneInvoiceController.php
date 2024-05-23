6b5df84a140327f76a66c3f167595f008470bb3c
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-23 16:19:47 +0400

    add payment invoice api

--- app/Http/Controllers/Invoice/WalletOneInvoiceController.php
+++ app/Http/Controllers/Invoice/WalletOneInvoiceController.php
@@ -13,6 +13,7 @@ class WalletOneInvoiceController
 {
     public function __invoke(Request $request): JsonResponse
     {
+        //, 'customer@jobtron.com'
         $customer = new CustomerDto(0, 'kzt', 'unknown customer');
         $data = new CreateInvoiceDTO('kzt', $request->get('amount'));
         $invoice = Gateway::provider('kzt')->createInvoice($data, $customer);
