82602cee07060a4652330d8b7d327cb3c7e77863
unknown <ruslanchik_nadyrov@mail.ru>
2024-05-24 13:04:47 +0600

    feat: fix mobile design workshop

--- resources/js/pages/workshop/WorkshopPage.vue
+++ resources/js/pages/workshop/WorkshopPage.vue
@@ -1,60 +1,37 @@
 <template>
 	<div class="workshop">
 		<h1 class="workshop__title">
-			Оплата заказа
+			Оплата заказа: Практикум по созданию Базы Знаний
 		</h1>
-		<div class="wrapper">
-			<div style="margin-top: 5%">
-				<h2 style="font-size: 20px">
-					Оплатить картой
-				</h2>
-				<div style="display: flex; flex-direction: column">
-					<button @click="getAmount">
-						<a>
-							<img
-								width="40"
-								height="40"
-								src="https://cdn-icons-png.flaticon.com/512/5352/5352016.png"
-								alt=""
-							>
-							Картой банка не из Росиии
-						</a>
-					</button>
-					<button style="margin-top: 2%">
-						<a href="https://proeducation.kz/e44NQ/">
-							<img
-								width="40"
-								height="40"
-								src="https://gallery.yopriceville.com/downloadfullsize/send/10186"
-								alt=""
-							>
-							Картой Российского банка
-						</a>
-					</button>
-				</div>
-			</div>
-			<button style="margin-top: 2%">
+		<div class="workshop__wrapper">
+			<h2>Оплатить картой</h2>
+			<button @click="getAmount">
+				<a>
+					<img
+						src="https://cdn-icons-png.flaticon.com/512/5352/5352016.png"
+						alt=""
+					>
+					Картой банка не из Росиии
+				</a>
+			</button>
+			<p>Оплачивая картой вы соглашаетесь с условями <strong>оферты</strong></p>
+			<button>
+				<a href="https://proeducation.kz/e44NQ/">
+					<img
+						src="https://gallery.yopriceville.com/downloadfullsize/send/10186"
+						alt=""
+					>
+					Картой Российского банка
+				</a>
+			</button>
+			<h2>Купить в рассрочку или в кредит</h2>
+			<button>
 				<a href="https://pay.kaspi.kz/pay/jjtpkyxq">
 					<img
-						width="40"
-						height="40"
 						src="https://cdn-icons-png.flaticon.com/512/262/262042.png"
 						alt=""
 					>
 					Оплата Каспи Рассрочка
-				</a>
-			</button>
-			<button style="margin-top: 2%">
-				<a
-					href="https://wl.walletone.com/checkout/refill/CreditCard/347773203/NewCard"
-				>
-					<img
-						width="40"
-						height="40"
-						src="https://cdn-icons-png.flaticon.com/512/262/262042.png"
-						alt=""
-					>
-					Тест
 				</a>
 			</button>
 		</div>
@@ -78,6 +55,12 @@ export default {
 	},
 	mounted() {
 		this.openPayForm();
+		this.$nextTick(() => {
+			this.setMetaViewport();
+		});
+	},
+	updated() {
+		this.setMetaViewport();
 	},
 	methods: {
 		...mapActions(useAmountStore, ['getUsername']),
@@ -101,17 +84,121 @@ export default {
 		getAmount() {
 			return this.axios
 				.post(this.amountApi, {
-					amount:  sessionStorage.getItem('sumForm'),
+					amount: sessionStorage.getItem('sumForm'),
 					phone: sessionStorage.getItem('phoneForm'),
 				})
 				.then((res) => {
 					this.createForm(res.data);
 				});
 		},
+		setMetaViewport() {
+			const meta = document.createElement('meta');
+			meta.setAttribute('name', 'viewport');
+			meta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+			document.head.appendChild(meta);
+		},
 	},
 };
 </script>
 
 <style scoped lang="scss">
-@import url("./styles.scss");
+.workshop {
+	margin: 2% 4% 0% 4%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	font-size: 20px;
+	.workshop__title {
+		font-weight: bold;
+		font-size: 35px;
+	}
+	&__wrapper {
+		h2 {
+			margin-top: 1%;
+			font-size: 20px;
+			font-weight: bold;
+		}
+		button {
+			margin-top: 1%;
+			width: 100%;
+			font-weight: bold;
+			padding: 0.5%;
+			background-color: #fabf0f;
+			border-radius: 10px;
+			transition: all ease 100ms;
+			display: flex;
+			flex-wrap: wrap;
+      z-index: 1;
+			img {
+				width: 50px;
+				height: 50px;
+				border-radius: 50%;
+			}
+			&:hover {
+				background-color: #e8ae02;
+			}
+			&:focus {
+				outline: none;
+			}
+			a {
+				color: black;
+				display: flex;
+				align-items: center;
+				gap: 5px;
+			}
+		}
+		p {
+			font-size: 12px;
+			background-color: #f1d070f9;
+			padding: 0% 1%;
+			z-index: 0;
+			border-radius: 0px 0px 9px 8px;
+			-webkit-border-radius: 0px 0px 9px 8px;
+			-moz-border-radius: 0px 0px 9px 8px;
+			margin-top: -0.5%;
+		}
+	}
+}
+
+@media (max-width: 1000px) {
+	.workshop {
+		margin: 2% 4% 0% 4%;
+		font-size: 20px;
+		.workshop__title {
+			font-size: 35px;
+		}
+		&__wrapper {
+			h2 {
+				margin-top: 7%;
+				font-size: 20px;
+			}
+			button {
+				margin-top: 4%;
+				width: 100%;
+				font-weight: bold;
+				padding: 1.5%;
+				img {
+					width: 50px;
+					height: 50px;
+					border-radius: 50%;
+				}
+				&:hover {
+					background-color: #e8ae02;
+				}
+				&:focus {
+					outline: none;
+				}
+				a {
+					color: black;
+					display: flex;
+					align-items: center;
+					gap: 5px;
+				}
+			}
+		}
+    p {
+      margin-top: -2%;
+    }
+	}
+}
 </style>
