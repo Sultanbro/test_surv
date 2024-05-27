920820aa5dee594c8469ab343fd44a7d42cd135f
Vahagn99 <vahagn99ghukasyan@gmail.com>
2024-05-27 15:21:07 +0400

    refactoring payment

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/tariff/get', [TariffController::class, 'get'])->name('lending.tariffs.get');
 
-Route::prefix('invoices/v1')->group(function () {
+Route::prefix('/v1/invoices')->group(function () {
     Route::get('/', [InvoiceController::class, 'list']);
     Route::post('/', [InvoiceController::class, 'store']);
     Route::post('/{invoice}/status', [InvoiceController::class, 'update']);
