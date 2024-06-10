c7c86209fc38aca740e0814877aba5d6a6bb14e0
Vahagn <vahagn99ghukasyan@gmail.com>
2024-06-10 17:36:32 +0400

    fix: createing new cabinet

--- app/Http/Controllers/User/ProjectController.php
+++ app/Http/Controllers/User/ProjectController.php
@@ -65,12 +65,14 @@ class ProjectController extends Controller
     {
         $user = auth()->user();
 
-        if (!$user->working_country) {
-            // App/User cannot be null while creating new tenant
-            return response()->json([
-                'error' => 'Для создания нового кабинета заполните город в настройках профиля'
-            ], 400);
-        }
+        // deprecated
+//        if (!$user->working_country) {
+//            // App/User cannot be null while creating new tenant
+//            return response()->json([
+//                'error' => 'Для создания нового кабинета заполните город в настройках профиля'
+//            ], 400);
+//        }
+
         $centralUser = CentralUser::query()->where('email', $user->email)->first();
 
         $tenant = $this->createTenant($centralUser);
