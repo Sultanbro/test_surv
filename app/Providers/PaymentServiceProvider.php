f59a4dd2abad5789bf287644a886beb5a53e7eab
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-07-05 11:18:43 +0500

    fix

--- app/Providers/PaymentServiceProvider.php
+++ app/Providers/PaymentServiceProvider.php
@@ -25,24 +25,24 @@ class PaymentServiceProvider extends ServiceProvider
         /** @var Register $registry */
         $registry = $this->app->make(Register::class);
 
-//        $registry->register(["wallet1", "kzt"], function () {
-//            $connector = new WalletOneConnector(
-//                config('payment.providers.wallet1.payment_url'),
-//                config('payment.providers.wallet1.shop_key'),
-//                config('payment.providers.wallet1.merchant_id'),
-//                config('payment.providers.wallet1.success_url'),
-//                config('payment.providers.wallet1.failed_url')
-//            );
-//            return new WalletOneGateway($connector);
-//        });
-//        $registry->register(["prodamus", "rub"], function () {
-//            $connector = new ProdamusConnector(
-//                config('payment.providers.prodamus.payment_url'),
-//                config('payment.providers.prodamus.secret_key'),
-//                config('payment.providers.prodamus.success_url'),
-//                config('payment.providers.prodamus.failed_url')
-//            );
-//            return new ProdamusGateway($connector);
-//        });
+        $registry->register(["wallet1", "kzt"], function () {
+            $connector = new WalletOneConnector(
+                config('payment.providers.wallet1.payment_url'),
+                config('payment.providers.wallet1.shop_key'),
+                config('payment.providers.wallet1.merchant_id'),
+                config('payment.providers.wallet1.success_url'),
+                config('payment.providers.wallet1.failed_url')
+            );
+            return new WalletOneGateway($connector);
+        });
+        $registry->register(["prodamus", "rub"], function () {
+            $connector = new ProdamusConnector(
+                config('payment.providers.prodamus.payment_url'),
+                config('payment.providers.prodamus.secret_key'),
+                config('payment.providers.prodamus.success_url'),
+                config('payment.providers.prodamus.failed_url')
+            );
+            return new ProdamusGateway($connector);
+        });
     }
 }
