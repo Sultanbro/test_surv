eaaa6275e6e0301a81097108b2998f0edac41489
Vahagn <vahagn99ghukasyan@gmail.com>
2024-07-08 17:32:45 +0400

    add: index for invoices

--- database/migrations/2024_06_12_071521_add_indexes_to_invoices_table.php
+++ database/migrations/2024_06_12_071521_add_indexes_to_invoices_table.php
@@ -9,7 +9,6 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::connection('mysql')->table('invoices', function (Blueprint $table) {
-            $table->unique('transaction_id');
             $table->index(['transaction_id', 'status', 'provider']);
             $table->index('created_at');
         });
@@ -18,7 +17,6 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::connection('mysql')->table('invoices', function (Blueprint $table) {
-            $table->dropUnique(['transaction_id']);
             $table->dropIndex(['transaction_id', 'status', 'provider']);
             $table->dropIndex(['created_at']);
         });
