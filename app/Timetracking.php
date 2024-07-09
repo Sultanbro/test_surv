cd39d950b35bd2002050247a47daf50010bbe136
Vahagn <vahagn99ghukasyan@gmail.com>
2024-07-09 12:24:48 +0400

    fix: table users total hours

--- app/Timetracking.php
+++ app/Timetracking.php
@@ -148,7 +148,7 @@ class Timetracking extends Model
                 'full_time',
                 'weekdays',
                 'timezone',
-                DB::raw('SELECT SUM(TIMESTAMPDIFF(minute, `enter`, `exit`) / 60) FROM timetracking WHERE users.id = timetracking.user_id AND MONTH(enter) =' . $request->month . ' AND YEAR(enter)=' . $request->year . ') AS total_hourse')
+                DB::raw('SUM(TIMESTAMPDIFF(minute, `enter`, `exit`) / 60) FROM timetracking WHERE users.id = timetracking.user_id AND MONTH(enter) =' . $request->month . ' AND YEAR(enter)=' . $request->year . ') AS total_hourse')
             ])
             ->with([
                 'group_users',
